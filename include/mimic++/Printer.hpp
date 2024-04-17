034904d80de498df56bd8d330bae1f5223ba5384
DNKpp <dnkpp2011@gmail.com>
2024-04-17 21:47:11 +0200

    feat: add Printer.hpp

---
+++ include/mimic++/Printer.hpp
@@ -0,0 +1,180 @@
+// //          Copyright Dominic (DNKpp) Koepke 2024 - 2024.
+// // Distributed under the Boost Software License, Version 1.0.
+// //    (See accompanying file LICENSE_1_0.txt or copy at
+// //          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MIMICPP_PRINTER_HPP
+#define MIMICPP_PRINTER_HPP
+
+#pragma once
+
+#include <format>
+
+namespace mimicpp
+{
+	using CharT = char;
+
+	template <typename T>
+	concept print_iterator = std::output_iterator<T, CharT>;
+}
+
+namespace mimicpp::custom
+{
+	template <typename>
+	class Printer;
+}
+
+namespace mimicpp::detail
+{
+	template <std::size_t priority>
+	struct priority_tag
+		: public priority_tag<priority - 1>
+	{
+	};
+
+	template <>
+	struct priority_tag<0>
+	{
+	};
+
+	template <print_iterator OutIter, typename T, typename Printer = custom::Printer<std::remove_cvref_t<T>>>
+	constexpr OutIter print(
+		OutIter out,
+		T&& value,
+		const priority_tag<3>
+	)
+		requires requires
+		{
+			{ Printer::print(out, std::forward<T>(value)) } -> std::convertible_to<OutIter>;
+		}
+	{
+		return Printer::print(out, std::forward<T>(value));
+	}
+
+	template <typename Char>
+	struct format_context;
+
+	template <typename Char>
+	using format_context_t = typename format_context<Char>::type;
+
+	template <>
+	struct format_context<char>
+	{
+		using type = std::format_context;
+	};
+
+	template <>
+	struct format_context<wchar_t>
+	{
+		using type = std::wformat_context;
+	};
+
+	/**
+	 * \brief Determines, whether a complete specialization of ``std::formatter`` for the given (possibly cv-ref qualified) type exists.
+	 * \tparam T Type to check.
+	 * \tparam Char Used character type.
+	 * \details This is an adapted implementation of the ``std::formattable`` concept, which is added c++23.
+	 * \note This implementation takes a simple but reasonable shortcut in assuming, that ```Char`` is either ``char`` or ``wchar_t``,
+	 * which must not necessarily true.
+	 * \see Adapted from here: https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2022/p2286r8.html#concept-formattable
+	 * \see https://en.cppreference.com/w/cpp/utility/format/formattable
+	 */
+	template <class T, class Char>
+	concept formattable =
+		std::semiregular<std::formatter<std::remove_cvref_t<T>, Char>>
+		&& requires(
+		std::formatter<std::remove_cvref_t<T>, Char> formatter,
+		T t,
+		format_context_t<Char> formatContext,
+		std::basic_format_parse_context<Char> parseContext
+	)
+		{
+			{ formatter.parse(parseContext) } -> std::same_as<typename std::basic_format_parse_context<Char>::iterator>;
+			{
+				std::as_const(formatter).format(t, formatContext)
+			} -> std::same_as<typename std::remove_reference_t<decltype(formatContext)>::iterator>;
+		};
+
+	template <typename OutIter, formattable<CharT> T>
+	constexpr OutIter print(
+		OutIter out,
+		T& value,
+		const priority_tag<2>
+	)
+	{
+		return std::format_to(out, "{}", value);
+	}
+
+	template <typename OutIter, std::ranges::forward_range Range>
+	OutIter print(
+		OutIter out,
+		Range&& range,
+		priority_tag<1>
+	);
+
+	template <typename OutIter>
+	constexpr OutIter print(
+		OutIter out,
+		auto&,
+		const priority_tag<0>
+	)
+	{
+		return std::format_to(out, "{{?}}");
+	}
+
+	class PrintFn
+	{
+	public:
+		template <print_iterator OutIter, typename T>
+		constexpr OutIter operator ()(
+			OutIter out,
+			T&& value
+		) const
+		{
+			static_assert(
+				requires(const priority_tag<3> tag)
+				{
+					{ print(out, std::forward<T>(value), tag) } -> std::convertible_to<OutIter>;
+				},
+				"The given type is not printable. ");
+
+			return print(
+				out,
+				std::forward<T>(value),
+				priority_tag<3>{});
+		}
+	};
+
+	template <typename OutIter, std::ranges::forward_range Range>
+	OutIter print(
+		OutIter out,
+		Range&& range,
+		const priority_tag<1>
+	)
+	{
+		out = std::format_to(out, "{{ ");
+		auto iter = std::ranges::begin(range);
+		if (const auto end = std::ranges::end(range);
+			iter != end)
+		{
+			constexpr PrintFn print{};
+			out = print(out, *iter++);
+
+			for (; iter != end; ++iter)
+			{
+				out = print(
+					std::format_to(out, ", "),
+					*iter);
+			}
+		}
+
+		return std::format_to(out, " }}");
+	}
+}
+
+namespace mimicpp
+{
+	inline constexpr detail::PrintFn print{};
+}
+
+#endif
---
+++ test/Printer.cpp
@@ -0,0 +1,210 @@
+// //          Copyright Dominic (DNKpp) Koepke 2024 - 2024.
+// // Distributed under the Boost Software License, Version 1.0.
+// //    (See accompanying file LICENSE_1_0.txt or copy at
+// //          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "mimic++/Printer.hpp"
+
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/matchers/catch_matchers.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
+
+#include <sstream>
+
+using namespace mimicpp;
+
+namespace
+{
+	class NonPrintable
+	{
+	};
+}
+
+TEMPLATE_TEST_CASE_SIG(
+	"detail::formattable determines whether the given type has a std::formatter specialization.",
+	"[print]",
+	((bool expected, typename T, typename Char), expected, T, Char),
+	(true, int, char),
+	(true, const int, char),
+	(true, int&, char),
+	(true, const int&, char),
+	(true, int, wchar_t),
+	(false, NonPrintable, char)
+)
+{
+	STATIC_REQUIRE(expected == detail::formattable<T, Char>);
+}
+
+namespace
+{
+	class CustomPrintable
+	{
+	};
+
+	class StdFormatPrintable
+	{
+	};
+
+	class StdFormatAndCustomPrintable
+	{
+	};
+}
+
+template <>
+class custom::Printer<CustomPrintable>
+{
+public:
+	inline static int printCallCounter{0};
+
+	static auto print(print_iterator auto out, const CustomPrintable&)
+	{
+		++printCallCounter;
+		return std::format_to(out, "CustomPrintable");
+	}
+};
+
+template <typename Char>
+struct std::formatter<StdFormatPrintable, Char>
+{
+	inline static int printCallCounter{0};
+
+	static constexpr auto parse(std::format_parse_context& ctx)
+	{
+		return ctx.begin();
+	}
+
+	static auto format(const StdFormatPrintable&, std::format_context& ctx)
+	{
+		++printCallCounter;
+		return std::format_to(ctx.out(), "StdFormatPrintable");
+	}
+};
+
+template <>
+class custom::Printer<StdFormatAndCustomPrintable>
+{
+public:
+	inline static int printCallCounter{0};
+
+	static auto print(print_iterator auto out, const StdFormatAndCustomPrintable&)
+	{
+		++printCallCounter;
+		return std::format_to(out, "StdFormatAndCustomPrintable");
+	}
+};
+
+template <typename Char>
+struct std::formatter<StdFormatAndCustomPrintable, Char>
+{
+	inline static int printCallCounter{0};
+
+	static constexpr auto parse(std::format_parse_context& ctx)
+	{
+		return ctx.begin();
+	}
+
+	static auto format(const StdFormatAndCustomPrintable&, std::format_context& ctx)
+	{
+		++printCallCounter;
+		return std::format_to(ctx.out(), "StdFormatAndCustomPrintable");
+	}
+};
+
+TEST_CASE("print selects the best option to print a given value.", "[print]")
+{
+	std::basic_stringstream<CharT> stream{};
+
+	SECTION("custom::Printer specialization is supported.")
+	{
+		using PrinterT = custom::Printer<CustomPrintable>;
+		PrinterT::printCallCounter = 0;
+
+		constexpr CustomPrintable value{};
+
+		print(std::ostreambuf_iterator{stream}, value);
+		REQUIRE(PrinterT::printCallCounter == 1);
+		REQUIRE_THAT(
+			stream.str(),
+			Catch::Matchers::Equals("CustomPrintable"));
+	}
+
+	SECTION("std::formatter specialization is supported.")
+	{
+		using PrinterT = std::formatter<StdFormatPrintable, CharT>;
+		PrinterT::printCallCounter = 0;
+
+		constexpr StdFormatPrintable value{};
+
+		print(std::ostreambuf_iterator{stream}, value);
+		REQUIRE(PrinterT::printCallCounter == 1);
+		REQUIRE_THAT(
+			stream.str(),
+			Catch::Matchers::Equals("StdFormatPrintable"));
+	}
+
+	SECTION("custom::Printer specialization is prefered.")
+	{
+		using PrinterT = custom::Printer<StdFormatAndCustomPrintable>;
+		PrinterT::printCallCounter = 0;
+
+		constexpr StdFormatAndCustomPrintable value{};
+
+		print(std::ostreambuf_iterator{stream}, value);
+		REQUIRE(PrinterT::printCallCounter == 1);
+		REQUIRE_THAT(
+			stream.str(),
+			Catch::Matchers::Equals("StdFormatAndCustomPrintable"));
+	}
+
+	SECTION("Ranges have special treatment.")
+	{
+		SECTION("Empty ranges are supported.")
+		{
+			const std::vector<int> vec{};
+
+			print(std::ostreambuf_iterator{stream}, vec);
+			REQUIRE_THAT(
+				stream.str(),
+				Catch::Matchers::Equals("{  }"));
+		}
+
+		SECTION("Ranges with single element are supported.")
+		{
+			const std::vector vec{42};
+
+			print(std::ostreambuf_iterator{stream}, vec);
+			REQUIRE_THAT(
+				stream.str(),
+				Catch::Matchers::Equals("{ 42 }"));
+		}
+
+		SECTION("Ranges with multiple elements are supported.")
+		{
+			const std::vector vec{42, 1337};
+
+			print(std::ostreambuf_iterator{stream}, vec);
+			REQUIRE_THAT(
+				stream.str(),
+				Catch::Matchers::Equals("{ 42, 1337 }"));
+		}
+
+		SECTION("Range elements are forwarded to the print function..")
+		{
+			const std::vector vec{NonPrintable{}, NonPrintable{}, NonPrintable{}};
+
+			print(std::ostreambuf_iterator{stream}, vec);
+			REQUIRE_THAT(
+				stream.str(),
+				Catch::Matchers::Equals("{ {?}, {?}, {?} }"));
+		}
+	}
+
+	SECTION("When nothing matches, a default token is inserted.")
+	{
+		constexpr NonPrintable value{};
+		print(std::ostreambuf_iterator{stream}, value);
+		REQUIRE_THAT(
+			stream.str(),
+			Catch::Matchers::Equals("{?}"));
+	}
+}
