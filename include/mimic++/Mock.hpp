878ddda2172bcfdc26a95e76fe993bc339c2f7cf
DNKpp <dnkpp2011@gmail.com>
2024-04-21 16:54:57 +0200

    feat: add Mock

---
+++ include/mimic++/Mock.hpp
@@ -0,0 +1,693 @@
+// //          Copyright Dominic (DNKpp) Koepke 2024 - 2024.
+// // Distributed under the Boost Software License, Version 1.0.
+// //    (See accompanying file LICENSE_1_0.txt or copy at
+// //          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MIMICPP_MOCK_HPP
+#define MIMICPP_MOCK_HPP
+
+#pragma once
+
+#include "mimic++/Expectation.hpp"
+#include "mimic++/ExpectationBuilder.hpp"
+#include "mimic++/ExpectationPolicies.hpp"
+
+namespace mimicpp::detail
+{
+	template <typename Signature>
+	class MockBase;
+
+	template <typename Return, typename... Params>
+	class MockBase<Return(Params...)>
+	{
+	protected:
+		using SignatureT = Return(Params...);
+		static constexpr auto category{ValueCategory::any};
+		static constexpr auto constness{Constness::non_const};
+
+	public:
+		MockBase(const MockBase&) = delete;
+		MockBase& operator =(const MockBase&) = delete;
+
+		template <typename... Args, source_location_data from = source_location_data{std::source_location::current()}>
+			requires (sizeof...(Params) == sizeof...(Args))
+		[[nodiscard]]
+		constexpr auto expect_call(Args&&... args)
+		{
+			return detail::make_expectation_builder(
+						m_Expectations,
+						from,
+						std::forward<Args>(args)...)
+					| expectation_policies::Category<category>{}
+					| expectation_policies::Constness<constness>{};
+		}
+
+	protected:
+		~MockBase() = default;
+
+		[[nodiscard]]
+		MockBase() = default;
+
+		[[nodiscard]]
+		MockBase(MockBase&&) = default;
+		MockBase& operator =(MockBase&&) = default;
+
+		[[nodiscard]]
+		constexpr Return handle_call(const source_location_data& from, Params... params)
+		{
+			using CallInfoT = call::Info<Return, Params...>;
+
+			return m_Expectations->handle_call(
+				CallInfoT{
+					.params = {std::ref(params)...},
+					.fromCategory = category,
+					.fromConstness = constness,
+					.fromSourceLocation = from
+				});
+		}
+
+	private:
+		std::shared_ptr<ExpectationCollection<SignatureT>> m_Expectations{
+			std::make_shared<ExpectationCollection<SignatureT>>()
+		};
+	};
+
+	template <typename Return, typename... Params>
+	class MockBase<Return(Params...) const>
+	{
+	protected:
+		using SignatureT = Return(Params...) const;
+		static constexpr auto category{ValueCategory::any};
+		static constexpr auto constness{Constness::as_const};
+
+	public:
+		MockBase(const MockBase&) = delete;
+		MockBase& operator =(const MockBase&) = delete;
+
+		template <typename... Args, source_location_data from = source_location_data{std::source_location::current()}>
+			requires (sizeof...(Params) == sizeof...(Args))
+		[[nodiscard]]
+		constexpr auto expect_call(Args&&... args) const
+		{
+			return detail::make_expectation_builder(
+						m_Expectations,
+						from,
+						std::forward<Args>(args)...)
+					| expectation_policies::Category<category>{}
+					| expectation_policies::Constness<constness>{};
+		}
+
+	protected:
+		~MockBase() = default;
+
+		[[nodiscard]]
+		MockBase() = default;
+
+		[[nodiscard]]
+		MockBase(MockBase&&) = default;
+		MockBase& operator =(MockBase&&) = default;
+
+		[[nodiscard]]
+		constexpr Return handle_call(const source_location_data& from, Params... params) const
+		{
+			using CallInfoT = call::Info<Return, Params...>;
+
+			return m_Expectations->handle_call(
+				CallInfoT{
+					.params = {std::ref(params)...},
+					.fromCategory = category,
+					.fromConstness = constness,
+					.fromSourceLocation = from
+				});
+		}
+
+	private:
+		std::shared_ptr<ExpectationCollection<SignatureT>> m_Expectations{
+			std::make_shared<ExpectationCollection<SignatureT>>()
+		};
+	};
+
+	template <typename Return, typename... Params>
+	class MockBase<Return(Params...) &>
+	{
+	protected:
+		using SignatureT = Return(Params...) &;
+		static constexpr auto category{ValueCategory::lvalue};
+		static constexpr auto constness{Constness::non_const};
+
+	public:
+		MockBase(const MockBase&) = delete;
+		MockBase& operator =(const MockBase&) = delete;
+
+		template <typename... Args, source_location_data from = source_location_data{std::source_location::current()}>
+			requires (sizeof...(Params) == sizeof...(Args))
+		[[nodiscard]]
+		constexpr auto expect_call(Args&&... args) &
+		{
+			return detail::make_expectation_builder(
+						m_Expectations,
+						from,
+						std::forward<Args>(args)...)
+					| expectation_policies::Category<category>{}
+					| expectation_policies::Constness<constness>{};
+		}
+
+	protected:
+		~MockBase() = default;
+
+		[[nodiscard]]
+		MockBase() = default;
+
+		[[nodiscard]]
+		MockBase(MockBase&&) = default;
+		MockBase& operator =(MockBase&&) = default;
+
+		[[nodiscard]]
+		constexpr Return handle_call(const source_location_data& from, Params... params) &
+		{
+			using CallInfoT = call::Info<Return, Params...>;
+
+			return m_Expectations->handle_call(
+				CallInfoT{
+					.params = {std::ref(params)...},
+					.fromCategory = category,
+					.fromConstness = constness,
+					.fromSourceLocation = from
+				});
+		}
+
+	private:
+		std::shared_ptr<ExpectationCollection<SignatureT>> m_Expectations{
+			std::make_shared<ExpectationCollection<SignatureT>>()
+		};
+	};
+
+	template <typename Return, typename... Params>
+	class MockBase<Return(Params...) const &>
+	{
+	protected:
+		using SignatureT = Return(Params...) const &;
+		static constexpr auto category{ValueCategory::lvalue};
+		static constexpr auto constness{Constness::as_const};
+
+	public:
+		MockBase(const MockBase&) = delete;
+		MockBase& operator =(const MockBase&) = delete;
+
+		template <typename... Args, source_location_data from = source_location_data{std::source_location::current()}>
+			requires (sizeof...(Params) == sizeof...(Args))
+		[[nodiscard]]
+		constexpr auto expect_call(Args&&... args) const &
+		{
+			return detail::make_expectation_builder(
+						m_Expectations,
+						from,
+						std::forward<Args>(args)...)
+					| expectation_policies::Category<category>{}
+					| expectation_policies::Constness<constness>{};
+		}
+
+	protected:
+		~MockBase() = default;
+
+		[[nodiscard]]
+		MockBase() = default;
+
+		[[nodiscard]]
+		MockBase(MockBase&&) = default;
+		MockBase& operator =(MockBase&&) = default;
+
+		[[nodiscard]]
+		constexpr Return handle_call(const source_location_data& from, Params... params) const &
+		{
+			using CallInfoT = call::Info<Return, Params...>;
+
+			return m_Expectations->handle_call(
+				CallInfoT{
+					.params = {std::ref(params)...},
+					.fromCategory = category,
+					.fromConstness = constness,
+					.fromSourceLocation = from
+				});
+		}
+
+	private:
+		std::shared_ptr<ExpectationCollection<SignatureT>> m_Expectations{
+			std::make_shared<ExpectationCollection<SignatureT>>()
+		};
+	};
+
+	template <typename Return, typename... Params>
+	class MockBase<Return(Params...) &&>
+	{
+	protected:
+		using SignatureT = Return(Params...) &&;
+		static constexpr auto category{ValueCategory::rvalue};
+		static constexpr auto constness{Constness::non_const};
+
+	public:
+		MockBase(const MockBase&) = delete;
+		MockBase& operator =(const MockBase&) = delete;
+
+		template <typename... Args, source_location_data from = source_location_data{std::source_location::current()}>
+			requires (sizeof...(Params) == sizeof...(Args))
+		[[nodiscard]]
+		constexpr auto expect_call(Args&&... args) &&
+		{
+			return detail::make_expectation_builder(
+						m_Expectations,
+						from,
+						std::forward<Args>(args)...)
+					| expectation_policies::Category<category>{}
+					| expectation_policies::Constness<constness>{};
+		}
+
+	protected:
+		~MockBase() = default;
+
+		[[nodiscard]]
+		MockBase() = default;
+
+		[[nodiscard]]
+		MockBase(MockBase&&) = default;
+		MockBase& operator =(MockBase&&) = default;
+
+		[[nodiscard]]
+		constexpr Return handle_call(const source_location_data& from, Params... params) &&
+		{
+			using CallInfoT = call::Info<Return, Params...>;
+
+			return m_Expectations->handle_call(
+				CallInfoT{
+					.params = {std::ref(params)...},
+					.fromCategory = category,
+					.fromConstness = constness,
+					.fromSourceLocation = from
+				});
+		}
+
+	private:
+		std::shared_ptr<ExpectationCollection<SignatureT>> m_Expectations{
+			std::make_shared<ExpectationCollection<SignatureT>>()
+		};
+	};
+
+	template <typename Return, typename... Params>
+	class MockBase<Return(Params...) const &&>
+	{
+	protected:
+		using SignatureT = Return(Params...) const &&;
+		static constexpr auto category{ValueCategory::rvalue};
+		static constexpr auto constness{Constness::as_const};
+
+	public:
+		MockBase(const MockBase&) = delete;
+		MockBase& operator =(const MockBase&) = delete;
+
+		template <typename... Args, source_location_data from = source_location_data{std::source_location::current()}>
+			requires (sizeof...(Params) == sizeof...(Args))
+		[[nodiscard]]
+		constexpr auto expect_call(Args&&... args) const &&
+		{
+			return detail::make_expectation_builder(
+						m_Expectations,
+						from,
+						std::forward<Args>(args)...)
+					| expectation_policies::Category<category>{}
+					| expectation_policies::Constness<constness>{};
+		}
+
+	protected:
+		~MockBase() = default;
+
+		[[nodiscard]]
+		MockBase() = default;
+
+		[[nodiscard]]
+		MockBase(MockBase&&) = default;
+		MockBase& operator =(MockBase&&) = default;
+
+		[[nodiscard]]
+		constexpr Return handle_call(const source_location_data& from, Params... params) const &&
+		{
+			using CallInfoT = call::Info<Return, Params...>;
+
+			return m_Expectations->handle_call(
+				CallInfoT{
+					.params = {std::ref(params)...},
+					.fromCategory = category,
+					.fromConstness = constness,
+					.fromSourceLocation = from
+				});
+		}
+
+	private:
+		std::shared_ptr<ExpectationCollection<SignatureT>> m_Expectations{
+			std::make_shared<ExpectationCollection<SignatureT>>()
+		};
+	};
+}
+
+namespace mimicpp
+{
+	template <typename Signature>
+	class Mock;
+
+	template <typename Return, typename... Params>
+	class Mock<Return(Params...)>
+		: public detail::MockBase<Return(Params...)>
+	{
+		using SuperT = detail::MockBase<Return(Params...)>;
+
+	public:
+		~Mock() = default;
+
+		[[nodiscard]]
+		Mock() = default;
+
+		Mock(const Mock&) = delete;
+		Mock& operator =(const Mock&) = delete;
+
+		[[nodiscard]]
+		Mock(Mock&&) = default;
+		Mock& operator =(Mock&&) = default;
+
+		template <source_location_data from = source_location_data{std::source_location::current()}>
+		constexpr Return operator ()(Params... params)
+		{
+			return SuperT::handle_call(
+				from,
+				std::forward<Params>(params)...);
+		}
+	};
+
+	template <typename Return, typename... Params>
+	class Mock<Return(Params...) const>
+		: public detail::MockBase<Return(Params...) const>
+	{
+		using SuperT = detail::MockBase<Return(Params...) const>;
+
+	public:
+		~Mock() = default;
+
+		[[nodiscard]]
+		Mock() = default;
+
+		Mock(const Mock&) = delete;
+		Mock& operator =(const Mock&) = delete;
+
+		[[nodiscard]]
+		Mock(Mock&&) = default;
+		Mock& operator =(Mock&&) = default;
+
+		template <source_location_data from = source_location_data{std::source_location::current()}>
+		constexpr Return operator ()(Params... params) const
+		{
+			return SuperT::handle_call(
+				from,
+				std::forward<Params>(params)...);
+		}
+	};
+
+	template <typename Return, typename... Params>
+	class Mock<Return(Params...) &>
+		: public detail::MockBase<Return(Params...) &>
+	{
+		using SuperT = detail::MockBase<Return(Params...) &>;
+
+	public:
+		~Mock() = default;
+
+		[[nodiscard]]
+		Mock() = default;
+
+		Mock(const Mock&) = delete;
+		Mock& operator =(const Mock&) = delete;
+
+		[[nodiscard]]
+		Mock(Mock&&) = default;
+		Mock& operator =(Mock&&) = default;
+
+		template <source_location_data from = source_location_data{std::source_location::current()}>
+		constexpr Return operator ()(Params... params) &
+		{
+			return SuperT::handle_call(
+				from,
+				std::forward<Params>(params)...);
+		}
+	};
+
+	template <typename Return, typename... Params>
+	class Mock<Return(Params...) const &>
+		: public detail::MockBase<Return(Params...) const &>
+	{
+		using SuperT = detail::MockBase<Return(Params...) const &>;
+
+	public:
+		~Mock() = default;
+
+		[[nodiscard]]
+		Mock() = default;
+
+		Mock(const Mock&) = delete;
+		Mock& operator =(const Mock&) = delete;
+
+		[[nodiscard]]
+		Mock(Mock&&) = default;
+		Mock& operator =(Mock&&) = default;
+
+		template <source_location_data from = source_location_data{std::source_location::current()}>
+		constexpr Return operator ()(Params... params) const &
+		{
+			return SuperT::handle_call(
+				from,
+				std::forward<Params>(params)...);
+		}
+	};
+
+	template <typename Return, typename... Params>
+	class Mock<Return(Params...) &&>
+		: public detail::MockBase<Return(Params...) &&>
+	{
+		using SuperT = detail::MockBase<Return(Params...) &&>;
+
+	public:
+		~Mock() = default;
+
+		[[nodiscard]]
+		Mock() = default;
+
+		Mock(const Mock&) = delete;
+		Mock& operator =(const Mock&) = delete;
+
+		[[nodiscard]]
+		Mock(Mock&&) = default;
+		Mock& operator =(Mock&&) = default;
+
+		template <source_location_data from = source_location_data{std::source_location::current()}>
+		constexpr Return operator ()(Params... params) &&
+		{
+			return std::move(*this).SuperT::handle_call(
+				from,
+				std::forward<Params>(params)...);
+		}
+	};
+
+	template <typename Return, typename... Params>
+	class Mock<Return(Params...) const &&>
+		: public detail::MockBase<Return(Params...) const &&>
+	{
+		using SuperT = detail::MockBase<Return(Params...) const &&>;
+
+	public:
+		~Mock() = default;
+
+		[[nodiscard]]
+		Mock() = default;
+
+		Mock(const Mock&) = delete;
+		Mock& operator =(const Mock&) = delete;
+
+		[[nodiscard]]
+		Mock(Mock&&) = default;
+		Mock& operator =(Mock&&) = default;
+
+		template <source_location_data from = source_location_data{std::source_location::current()}>
+		constexpr Return operator ()(Params... params) const &&
+		{
+			return std::move(*this).handle_call(
+				from,
+				std::forward<Params>(params)...);
+		}
+	};
+
+	template <typename Return, typename... Params>
+	class Mock<Return(Params...) noexcept>
+		: public detail::MockBase<Return(Params...)>
+	{
+		using SuperT = detail::MockBase<Return(Params...)>;
+
+	public:
+		~Mock() = default;
+
+		[[nodiscard]]
+		Mock() = default;
+
+		Mock(const Mock&) = delete;
+		Mock& operator =(const Mock&) = delete;
+
+		[[nodiscard]]
+		Mock(Mock&&) = default;
+		Mock& operator =(Mock&&) = default;
+
+		template <source_location_data from = source_location_data{std::source_location::current()}>
+		constexpr Return operator ()(Params... params) noexcept
+		{
+			return SuperT::handle_call(
+				from,
+				std::forward<Params>(params)...);
+		}
+	};
+
+	template <typename Return, typename... Params>
+	class Mock<Return(Params...) const noexcept>
+		: public detail::MockBase<Return(Params...) const>
+	{
+		using SuperT = detail::MockBase<Return(Params...) const>;
+
+	public:
+		~Mock() = default;
+
+		[[nodiscard]]
+		Mock() = default;
+
+		Mock(const Mock&) = delete;
+		Mock& operator =(const Mock&) = delete;
+
+		[[nodiscard]]
+		Mock(Mock&&) = default;
+		Mock& operator =(Mock&&) = default;
+
+		template <source_location_data from = source_location_data{std::source_location::current()}>
+		constexpr Return operator ()(Params... params) const noexcept
+		{
+			return SuperT::handle_call(
+				from,
+				std::forward<Params>(params)...);
+		}
+	};
+
+	template <typename Return, typename... Params>
+	class Mock<Return(Params...) & noexcept>
+		: public detail::MockBase<Return(Params...) &>
+	{
+		using SuperT = detail::MockBase<Return(Params...) &>;
+
+	public:
+		~Mock() = default;
+
+		[[nodiscard]]
+		Mock() = default;
+
+		Mock(const Mock&) = delete;
+		Mock& operator =(const Mock&) = delete;
+
+		[[nodiscard]]
+		Mock(Mock&&) = default;
+		Mock& operator =(Mock&&) = default;
+
+		template <source_location_data from = source_location_data{std::source_location::current()}>
+		constexpr Return operator ()(Params... params) & noexcept
+		{
+			return SuperT::handle_call(
+				from,
+				std::forward<Params>(params)...);
+		}
+	};
+
+	template <typename Return, typename... Params>
+	class Mock<Return(Params...) const & noexcept>
+		: public detail::MockBase<Return(Params...) const &>
+	{
+		using SuperT = detail::MockBase<Return(Params...) const &>;
+
+	public:
+		~Mock() = default;
+
+		[[nodiscard]]
+		Mock() = default;
+
+		Mock(const Mock&) = delete;
+		Mock& operator =(const Mock&) = delete;
+
+		[[nodiscard]]
+		Mock(Mock&&) = default;
+		Mock& operator =(Mock&&) = default;
+
+		template <source_location_data from = source_location_data{std::source_location::current()}>
+		constexpr Return operator ()(Params... params) const & noexcept
+		{
+			return SuperT::handle_call(
+				from,
+				std::forward<Params>(params)...);
+		}
+	};
+
+	template <typename Return, typename... Params>
+	class Mock<Return(Params...) && noexcept>
+		: public detail::MockBase<Return(Params...) &&>
+	{
+		using SuperT = detail::MockBase<Return(Params...) &&>;
+
+	public:
+		~Mock() = default;
+
+		[[nodiscard]]
+		Mock() = default;
+
+		Mock(const Mock&) = delete;
+		Mock& operator =(const Mock&) = delete;
+
+		[[nodiscard]]
+		Mock(Mock&&) = default;
+		Mock& operator =(Mock&&) = default;
+
+		template <source_location_data from = source_location_data{std::source_location::current()}>
+		constexpr Return operator ()(Params... params) && noexcept
+		{
+			return std::move(*this).SuperT::handle_call(
+				from,
+				std::forward<Params>(params)...);
+		}
+	};
+
+	template <typename Return, typename... Params>
+	class Mock<Return(Params...) const && noexcept>
+		: public detail::MockBase<Return(Params...) const &&>
+	{
+		using SuperT = detail::MockBase<Return(Params...) const &&>;
+
+	public:
+		~Mock() = default;
+
+		[[nodiscard]]
+		Mock() = default;
+
+		Mock(const Mock&) = delete;
+		Mock& operator =(const Mock&) = delete;
+
+		[[nodiscard]]
+		Mock(Mock&&) = default;
+		Mock& operator =(Mock&&) = default;
+
+		template <source_location_data from = source_location_data{std::source_location::current()}>
+		constexpr Return operator ()(Params... params) const && noexcept
+		{
+			return std::move(*this).SuperT::handle_call(
+				from,
+				std::forward<Params>(params)...);
+		}
+	};
+}
+
+#endif
---
+++ test/Mock.cpp
@@ -0,0 +1,432 @@
+// //          Copyright Dominic (DNKpp) Koepke 2024 - 2024.
+// // Distributed under the Boost Software License, Version 1.0.
+// //    (See accompanying file LICENSE_1_0.txt or copy at
+// //          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "mimic++/Mock.hpp"
+#include "TestReporter.hpp"
+#include "TestTypes.hpp"
+
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/trompeloeil.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/generators/catch_generators_range.hpp>
+#include <catch2/matchers/catch_matchers_container_properties.hpp>
+#include <catch2/matchers/catch_matchers_templated.hpp>
+
+using namespace mimicpp;
+
+TEMPLATE_TEST_CASE(
+	"Mock is a non-copyable, but movable and default-constructible type.",
+	"[mock]",
+	void(),
+	void() const,
+	void() &,
+	void() const &,
+	void() &&,
+	void() const &&,
+	void() noexcept,
+	void() const noexcept,
+	void() & noexcept,
+	void() const & noexcept,
+	void() && noexcept,
+	void() const && noexcept
+)
+{
+	using MockT = Mock<TestType>;
+
+	STATIC_REQUIRE(!std::is_copy_constructible_v<MockT>);
+	STATIC_REQUIRE(!std::is_copy_assignable_v<MockT>);
+
+	STATIC_REQUIRE(std::is_move_constructible_v<MockT>);
+	STATIC_REQUIRE(std::is_move_assignable_v<MockT>);
+	STATIC_REQUIRE(std::is_default_constructible_v<MockT>);
+
+	STATIC_REQUIRE(std::is_nothrow_move_constructible_v<MockT>);
+	STATIC_REQUIRE(std::is_nothrow_move_assignable_v<MockT>);
+}
+
+TEST_CASE(
+	"Mutable Mock specialization supports expectations.",
+	"[mock]"
+)
+{
+	ScopedReporter reporter{};
+
+	SECTION("With void() signature.")
+	{
+		Mock<void()> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call();
+		CHECK(!expectation.is_satisfied());
+
+		mock();
+
+		REQUIRE(expectation.is_satisfied());
+	}
+
+	SECTION("With int() signature.")
+	{
+		Mock<int()> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call()
+											| expect::returns(42);
+		CHECK(!expectation.is_satisfied());
+
+		REQUIRE(42 == mock());
+		REQUIRE(expectation.is_satisfied());
+	}
+}
+
+TEST_CASE(
+	"Const Mock specialization supports expectations.",
+	"[mock]"
+)
+{
+	ScopedReporter reporter{};
+
+	SECTION("With void() signature.")
+	{
+		Mock<void() const> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call();
+		CHECK(!expectation.is_satisfied());
+
+		mock();
+
+		REQUIRE(expectation.is_satisfied());
+	}
+
+	SECTION("With int() signature.")
+	{
+		Mock<int() const> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call()
+											| expect::returns(42);
+		CHECK(!expectation.is_satisfied());
+
+		REQUIRE(42 == mock());
+		REQUIRE(expectation.is_satisfied());
+	}
+}
+
+TEST_CASE(
+	"Lvalue Mock specialization supports expectations.",
+	"[mock]"
+)
+{
+	ScopedReporter reporter{};
+
+	SECTION("With void() signature.")
+	{
+		Mock<void() &> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call();
+		CHECK(!expectation.is_satisfied());
+
+		mock();
+
+		REQUIRE(expectation.is_satisfied());
+	}
+
+	SECTION("With int() signature.")
+	{
+		Mock<int() &> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call()
+											| expect::returns(42);
+		CHECK(!expectation.is_satisfied());
+
+		REQUIRE(42 == mock());
+		REQUIRE(expectation.is_satisfied());
+	}
+}
+
+TEST_CASE(
+	"Const lvalue Mock specialization supports expectations.",
+	"[mock]"
+)
+{
+	ScopedReporter reporter{};
+
+	SECTION("With void() signature.")
+	{
+		Mock<void() const &> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call();
+		CHECK(!expectation.is_satisfied());
+
+		mock();
+
+		REQUIRE(expectation.is_satisfied());
+	}
+
+	SECTION("With int() signature.")
+	{
+		Mock<int() const &> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call()
+											| expect::returns(42);
+		CHECK(!expectation.is_satisfied());
+
+		REQUIRE(42 == mock());
+		REQUIRE(expectation.is_satisfied());
+	}
+}
+
+TEST_CASE(
+	"Rvalue Mock specialization supports expectations.",
+	"[mock]"
+)
+{
+	ScopedReporter reporter{};
+
+	SECTION("With void() signature.")
+	{
+		Mock<void() &&> mock{};
+
+		const ScopedExpectation expectation = std::move(mock).expect_call();
+		CHECK(!expectation.is_satisfied());
+
+		std::move(mock)();
+
+		REQUIRE(expectation.is_satisfied());
+	}
+
+	SECTION("With int() signature.")
+	{
+		Mock<int() &&> mock{};
+
+		const ScopedExpectation expectation = std::move(mock).expect_call()
+											| expect::returns(42);
+		CHECK(!expectation.is_satisfied());
+
+		REQUIRE(42 == std::move(mock)());
+		REQUIRE(expectation.is_satisfied());
+	}
+}
+
+TEST_CASE(
+	"Const rvalue Mock specialization supports expectations.",
+	"[mock]"
+)
+{
+	ScopedReporter reporter{};
+
+	SECTION("With void() signature.")
+	{
+		Mock<void() const &&> mock{};
+
+		const ScopedExpectation expectation = std::move(mock).expect_call();
+		CHECK(!expectation.is_satisfied());
+
+		std::move(mock)();
+
+		REQUIRE(expectation.is_satisfied());
+	}
+
+	SECTION("With int() signature.")
+	{
+		Mock<int() const &&> mock{};
+
+		const ScopedExpectation expectation = std::move(mock).expect_call()
+											| expect::returns(42);
+		CHECK(!expectation.is_satisfied());
+
+		REQUIRE(42 == std::move(mock)());
+		REQUIRE(expectation.is_satisfied());
+	}
+}
+
+TEST_CASE(
+	"Mutable noexcept Mock specialization supports expectations.",
+	"[mock]"
+)
+{
+	ScopedReporter reporter{};
+
+	SECTION("With void() signature.")
+	{
+		Mock<void() noexcept> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call();
+		CHECK(!expectation.is_satisfied());
+
+		mock();
+
+		REQUIRE(expectation.is_satisfied());
+	}
+
+	SECTION("With int() signature.")
+	{
+		Mock<int() noexcept> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call()
+											| expect::returns(42);
+		CHECK(!expectation.is_satisfied());
+
+		REQUIRE(42 == mock());
+		REQUIRE(expectation.is_satisfied());
+	}
+}
+
+TEST_CASE(
+	"Const noexcept Mock specialization supports expectations.",
+	"[mock]"
+)
+{
+	ScopedReporter reporter{};
+
+	SECTION("With void() signature.")
+	{
+		Mock<void() const noexcept> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call();
+		CHECK(!expectation.is_satisfied());
+
+		mock();
+
+		REQUIRE(expectation.is_satisfied());
+	}
+
+	SECTION("With int() signature.")
+	{
+		Mock<int() const noexcept> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call()
+											| expect::returns(42);
+		CHECK(!expectation.is_satisfied());
+
+		REQUIRE(42 == mock());
+		REQUIRE(expectation.is_satisfied());
+	}
+}
+
+TEST_CASE(
+	"Lvalue noexcept Mock specialization supports expectations.",
+	"[mock]"
+)
+{
+	ScopedReporter reporter{};
+
+	SECTION("With void() signature.")
+	{
+		Mock<void() & noexcept> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call();
+		CHECK(!expectation.is_satisfied());
+
+		mock();
+
+		REQUIRE(expectation.is_satisfied());
+	}
+
+	SECTION("With int() signature.")
+	{
+		Mock<int() & noexcept> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call()
+											| expect::returns(42);
+		CHECK(!expectation.is_satisfied());
+
+		REQUIRE(42 == mock());
+		REQUIRE(expectation.is_satisfied());
+	}
+}
+
+TEST_CASE(
+	"Const lvalue noexcept Mock specialization supports expectations.",
+	"[mock]"
+)
+{
+	ScopedReporter reporter{};
+
+	SECTION("With void() signature.")
+	{
+		Mock<void() const & noexcept> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call();
+		CHECK(!expectation.is_satisfied());
+
+		mock();
+
+		REQUIRE(expectation.is_satisfied());
+	}
+
+	SECTION("With int() signature.")
+	{
+		Mock<int() const & noexcept> mock{};
+
+		const ScopedExpectation expectation = mock.expect_call()
+											| expect::returns(42);
+		CHECK(!expectation.is_satisfied());
+
+		REQUIRE(42 == mock());
+		REQUIRE(expectation.is_satisfied());
+	}
+}
+
+TEST_CASE(
+	"Rvalue noexcept Mock specialization supports expectations.",
+	"[mock]"
+)
+{
+	ScopedReporter reporter{};
+
+	SECTION("With void() signature.")
+	{
+		Mock<void() && noexcept> mock{};
+
+		const ScopedExpectation expectation = std::move(mock).expect_call();
+		CHECK(!expectation.is_satisfied());
+
+		std::move(mock)();
+
+		REQUIRE(expectation.is_satisfied());
+	}
+
+	SECTION("With int() signature.")
+	{
+		Mock<int() && noexcept> mock{};
+
+		const ScopedExpectation expectation = std::move(mock).expect_call()
+											| expect::returns(42);
+		CHECK(!expectation.is_satisfied());
+
+		REQUIRE(42 == std::move(mock)());
+		REQUIRE(expectation.is_satisfied());
+	}
+}
+
+TEST_CASE(
+	"Const rvalue noexcept Mock specialization supports expectations.",
+	"[mock]"
+)
+{
+	ScopedReporter reporter{};
+
+	SECTION("With void() signature.")
+	{
+		Mock<void() const && noexcept> mock{};
+
+		const ScopedExpectation expectation = std::move(mock).expect_call();
+		CHECK(!expectation.is_satisfied());
+
+		std::move(mock)();
+
+		REQUIRE(expectation.is_satisfied());
+	}
+
+	SECTION("With int() signature.")
+	{
+		Mock<int() const && noexcept> mock{};
+
+		const ScopedExpectation expectation = std::move(mock).expect_call()
+											| expect::returns(42);
+		CHECK(!expectation.is_satisfied());
+
+		REQUIRE(42 == std::move(mock)());
+		REQUIRE(expectation.is_satisfied());
+	}
+}
