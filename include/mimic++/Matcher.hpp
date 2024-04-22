f33283b7e1ecd9cdac71a7414d48a085c9f57823
DNKpp <dnkpp2011@gmail.com>
2024-04-22 16:56:10 +0200

    feat: extend matcher

--- include/mimic++/Matcher.hpp
+++ include/mimic++/Matcher.hpp
@@ -20,9 +20,9 @@ namespace mimicpp
 {
 	template <typename T, typename Target>
 	concept matcher_for = std::same_as<T, std::remove_cvref_t<T>>
-						&& std::movable<T>
+						&& std::is_move_constructible_v<T>
 						&& std::destructible<T>
-						&& requires(const T& matcher, Target&& target)
+						&& requires(const T& matcher, Target& target)
 						{
 							{ matcher.matches(target) } -> std::convertible_to<bool>;
 							{ matcher.describe(target) } -> std::convertible_to<StringT>;
@@ -31,10 +31,38 @@ namespace mimicpp
 
 namespace mimicpp::matcher
 {
-	template <std::movable Predicate, std::movable Describer>
-	class PredicateMatcher
+	class TargetPredicateDescriber
 	{
 	public:
+		[[nodiscard]]
+		explicit constexpr TargetPredicateDescriber(StringT&& predicateDescription) noexcept
+			: m_PredicateDescription{std::move(predicateDescription)}
+		{
+		}
+
+		template <typename T>
+		[[nodiscard]]
+		StringT operator()(T& target) const
+		{
+			return std::vformat(
+				m_PredicateDescription,
+				std::make_format_args(mimicpp::print(target)));
+		}
+
+	private:
+		StringT m_PredicateDescription;
+	};
+
+	template <typename Predicate, typename Describer, template <typename Derived> typename... Policies>
+		requires std::is_move_constructible_v<Predicate>
+				&& std::is_move_constructible_v<Describer>
+	class PredicateMatcher
+		: public Policies<PredicateMatcher<Predicate, Describer, Policies...>>...
+	{
+	public:
+		template <typename OtherPredicate, typename OtherDescriber>
+		using RebindT = PredicateMatcher<OtherPredicate, OtherDescriber, Policies...>;
+
 		[[nodiscard]]
 		explicit constexpr PredicateMatcher(
 			Predicate predicate,
@@ -47,9 +75,11 @@ namespace mimicpp::matcher
 		}
 
 		template <typename T>
-			requires std::predicate<const Predicate, T>
+			requires std::predicate<const Predicate, T&>
 		[[nodiscard]]
-		constexpr bool matches(T& target) const
+		constexpr bool matches(
+			T& target
+		) const noexcept(std::is_nothrow_invocable_r_v<bool, const Predicate, T&>)
 		{
 			return std::invoke(
 				m_Predicate,
@@ -57,13 +87,26 @@ namespace mimicpp::matcher
 		}
 
 		template <typename T>
-			requires std::convertible_to<std::invoke_result_t<const Describer, T>, StringT>
+			requires std::regular_invocable<const Describer, T&>
+					&& std::convertible_to<std::invoke_result_t<const Describer, T&>, StringT>
 		[[nodiscard]]
-		constexpr StringT matches(T& target) const
+		constexpr StringT describe(T& target) const
 		{
 			return std::invoke(
 				m_Describer,
 				target);
+		}
+
+		[[nodiscard]]
+		constexpr Predicate&& predicate() && noexcept
+		{
+			return std::move(m_Predicate);
+		}
+
+		[[nodiscard]]
+		constexpr Describer&& describer() && noexcept
+		{
+			return std::move(m_Describer);
 		}
 
 	private:
@@ -71,27 +114,49 @@ namespace mimicpp::matcher
 		[[no_unique_address]] Describer m_Describer;
 	};
 
-	class TargetPredicateDescriber
+	template <template <typename> typename... Policies, typename Predicate, typename Describer>
+	[[nodiscard]]
+	constexpr PredicateMatcher<Predicate, Describer, Policies...> make_predicate_matcher(
+		Predicate predicate,
+		Describer describer
+	) noexcept(std::is_nothrow_move_constructible_v<Predicate>
+				&& std::is_nothrow_move_constructible_v<Describer>)
+	{
+		return PredicateMatcher<Predicate, Describer, Policies...>{
+			std::move(predicate),
+			std::move(describer)
+		};
+	}
+
+	template <typename Derived>
+	class InvertiblePolicy
 	{
 	public:
 		[[nodiscard]]
-		explicit constexpr TargetPredicateDescriber(StringT&& predicateDescription) noexcept
-			: m_PredicateDescription{std::move(predicateDescription)}
+		constexpr auto operator !() &&
 		{
-		}
+			auto& self = static_cast<Derived&>(*this);
 
-		template <typename T>
-		[[nodiscard]]
-		constexpr StringT operator()(T& target)
-		{
-			return std::format(
-				"{} {}",
-				mimicpp::print(target),
-				m_PredicateDescription);
-		}
+			auto notPredicate = [predicate = std::move(self).predicate()](auto& target)
+			{
+				return !std::invoke(predicate, target);
+			};
 
-	private:
-		StringT m_PredicateDescription;
+			auto invertedDescriber = [describer = std::move(self).describer()](
+				auto& target
+			) -> StringT  // NOLINT(bugprone-use-after-move)
+			{
+				return std::format(
+					"!({})",
+					std::invoke(describer, target));
+			};
+
+			using InvertedMatcherT = typename Derived::template RebindT<decltype(notPredicate), decltype(invertedDescriber)>;
+			return InvertedMatcherT{
+				std::move(notPredicate),
+				std::move(invertedDescriber)
+			};
+		}
 	};
 }
 
@@ -99,19 +164,18 @@ namespace mimicpp::matches
 {
 	inline static const matcher::PredicateMatcher _{
 		AlwaysTruePredicate{},
-		matcher::TargetPredicateDescriber{"== _"}
+		matcher::TargetPredicateDescriber{"{} without constraints"}
 	};
 
 	template <typename T>
 	[[nodiscard]]
 	constexpr auto eq(T&& value)
 	{
-		return matcher::PredicateMatcher{
-			std::bind_front(std::equal_to{}, std::forward<T>(value)),
+		return matcher::make_predicate_matcher<matcher::InvertiblePolicy>(
+			std::bind_front(std::ranges::equal_to{}, std::forward<T>(value)),
 			matcher::TargetPredicateDescriber{
-				std::format("== {}", mimicpp::print(value))
-			}
-		};
+				std::format("{{}} == {}", mimicpp::print(value))
+			});
 	}
 }
 
---
+++ test/Matcher.cpp
@@ -0,0 +1,214 @@
+// //          Copyright Dominic (DNKpp) Koepke 2024 - 2024.
+// // Distributed under the Boost Software License, Version 1.0.
+// //    (See accompanying file LICENSE_1_0.txt or copy at
+// //          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "mimic++/Matcher.hpp"
+#include "mimic++/Printer.hpp"
+
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/trompeloeil.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/generators/catch_generators_range.hpp>
+#include <catch2/matchers/catch_matchers_container_properties.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
+
+using namespace mimicpp;
+
+TEMPLATE_TEST_CASE(
+	"Given types satisfy mimicpp::matcher_for concept.",
+	"[matcher]",
+	std::remove_const_t<decltype(matches::_)>,
+	std::remove_const_t<decltype(matches::eq(42))>,
+	std::remove_const_t<decltype(!matches::eq(42))>
+)
+{
+	STATIC_REQUIRE(matcher_for<TestType, int>);
+	STATIC_REQUIRE(matcher_for<TestType, const int>);
+	STATIC_REQUIRE(matcher_for<TestType, int&>);
+	STATIC_REQUIRE(matcher_for<TestType, const int&>);
+	STATIC_REQUIRE(matcher_for<TestType, int&&>);
+	STATIC_REQUIRE(matcher_for<TestType, const int&&>);
+}
+
+namespace
+{
+	template <typename T>
+	class MatcherPredicateMock
+	{
+	public:
+		MAKE_CONST_MOCK1(check, bool(T));
+
+		[[nodiscard]]
+		constexpr bool operator ()(T value) const
+		{
+			return check(value);
+		}
+	};
+
+	template <typename T>
+	class MatcherDescriberMock
+	{
+	public:
+		MAKE_CONST_MOCK1(describe, StringT(T));
+
+		[[nodiscard]]
+		constexpr StringT operator ()(T value) const
+		{
+			return describe(value);
+		}
+	};
+}
+
+TEST_CASE(
+	"matcher::PredicateMatcher is a generic matcher.",
+	"[matcher]")
+{
+	MatcherPredicateMock<int> predicate{};
+	MatcherDescriberMock<int> describer{};
+	matcher::PredicateMatcher matcher{
+		std::ref(predicate),
+		std::ref(describer)
+	};
+
+	SECTION("When matches() is called, argument is forwarded to the predicate.")
+	{
+		const bool result = GENERATE(true, false);
+
+		REQUIRE_CALL(predicate, check(42))
+			.RETURN(result);
+
+		constexpr int value{42};
+		REQUIRE(result == matcher.matches(value));
+	}
+
+	SECTION("When describe() is called, argument is forwarded to the functional.")
+	{
+		REQUIRE_CALL(describer, describe(42))
+			.RETURN("Hello, World!");
+
+		constexpr int value{42};
+		REQUIRE("Hello, World!" == matcher.describe(value));
+	}
+}
+
+TEST_CASE(
+	"matcher::PredicateMatcher can be extended with matcher::InvertiblePolicy.",
+	"[matcher]")
+{
+	MatcherPredicateMock<int> predicate{};
+	MatcherDescriberMock<int> describer{};
+	matcher::PredicateMatcher<
+			std::reference_wrapper<MatcherPredicateMock<int>>,
+			std::reference_wrapper<MatcherDescriberMock<int>>,
+			matcher::InvertiblePolicy
+		>
+		matcher{
+			std::ref(predicate),
+			std::ref(describer)
+		};
+
+	matcher::PredicateMatcher negatedMatcher = !std::move(matcher);
+	STATIC_REQUIRE(matcher_for<decltype(negatedMatcher), int>);
+
+	SECTION("When matches() is called, argument is forwarded to the predicate.")
+	{
+		const bool result = GENERATE(true, false);
+
+		REQUIRE_CALL(predicate, check(42))
+			.RETURN(result);
+
+		constexpr int value{42};
+		REQUIRE(result == !negatedMatcher.matches(value));
+	}
+
+	SECTION("When describe() is called, argument is forwarded to the functional.")
+	{
+		REQUIRE_CALL(describer, describe(42))
+				.RETURN("Hello, World!");
+
+		constexpr int value{42};
+		REQUIRE("!(Hello, World!)" == negatedMatcher.describe(value));
+	}
+}
+
+TEST_CASE(
+	"matcher::TargetPredicateDescriber serves as a helper for pre-defined formatting descriptions.",
+	"[matcher]"
+)
+{
+	const matcher::TargetPredicateDescriber describer{"Hello, {} World!"};
+
+	constexpr int value{42};
+	REQUIRE("Hello, 42 World!" == describer(value));
+
+	const StringT str{" test "};
+	REQUIRE("Hello,  test  World!" == describer(str));
+}
+
+TEST_CASE(
+	"matches::_ matches always.",
+	"[matcher]"
+)
+{
+	constexpr int value{42};
+	REQUIRE(matches::_.matches(value));
+	REQUIRE_THAT(
+		matches::_.describe(value),
+		Catch::Matchers::EndsWith(" without constraints")
+		&& Catch::Matchers::StartsWith("42"));
+}
+
+TEST_CASE(
+	"matches::eq matches when target value compares equal to the stored one.",
+	"[matcher]"
+)
+{
+	const auto matcher = matches::eq(42);
+
+	SECTION("When target is equal.")
+	{
+		constexpr int target{42};
+		REQUIRE(matcher.matches(target));
+		REQUIRE_THAT(
+			matcher.describe(target),
+			Catch::Matchers::EndsWith(" == 42")
+			&& Catch::Matchers::StartsWith("42"));
+	}
+
+	SECTION("When target is not equal.")
+	{
+		constexpr int target{1337};
+		REQUIRE(!matcher.matches(target));
+		REQUIRE_THAT(
+			matcher.describe(target),
+			Catch::Matchers::EndsWith(" == 42")
+			&& Catch::Matchers::StartsWith("1337"));
+	}
+
+	SECTION("Matcher can be inverted.")
+	{
+		const auto invertedMatcher = !matches::eq(42);
+
+		SECTION("When target is equal.")
+		{
+			constexpr int target{42};
+			REQUIRE(!invertedMatcher.matches(target));
+			REQUIRE_THAT(
+				invertedMatcher.describe(target),
+				Catch::Matchers::EndsWith(" == 42)")
+				&& Catch::Matchers::StartsWith("!(42"));
+		}
+
+		SECTION("When target is not equal.")
+		{
+			constexpr int target{1337};
+			REQUIRE(invertedMatcher.matches(target));
+			REQUIRE_THAT(
+				invertedMatcher.describe(target),
+				Catch::Matchers::EndsWith(" == 42)")
+				&& Catch::Matchers::StartsWith("!(1337"));
+		}
+	}
+}
