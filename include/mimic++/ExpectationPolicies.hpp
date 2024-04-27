ef4f8e55350f1ca97db71ce29e5479171b023013
DNKpp <dnkpp2011@gmail.com>
2024-04-27 22:01:32 +0200

    docs: add finalizer documentation

--- include/mimic++/ExpectationPolicies.hpp
+++ include/mimic++/ExpectationPolicies.hpp
@@ -527,26 +527,87 @@ namespace mimicpp::expect
 	}
 }
 
+namespace mimicpp::finally::detail
+{
+	struct forward_fn
+	{
+		template <typename T>
+		[[nodiscard]]
+		constexpr T&& operator ()(T&& obj) const noexcept
+		{
+			return std::forward<T>(obj);
+		}
+	};
+}
+
 namespace mimicpp::finally
 {
-	template <typename Action>
-		requires std::invocable<std::remove_cvref_t<Action>&>
-				&& (!std::is_void_v<std::invoke_result_t<std::remove_cvref_t<Action>&>>)
+	// ReSharper disable CppDoxygenUnresolvedReference
+
+	/**
+	 * \defgroup EXPECTATION_FINALIZER finalizer
+	 * \ingroup EXPECTATION
+	 * \brief Finalizers are the last step of a matching expectation.
+	 * \details Finalizers are executed for calls, which have a matching expectation. They are responsible for either returning an
+	 * appropriate return value or leaving the call by throwing an exception.
+	 *
+	 * An expectation must have exactly one finalizer applied. For mocks returning void, an appropriate finalizer is attached by default.
+	 * This default finalizer can be exchanged once. If an expectation setup contains multiple finalize statements, a compile error is
+	 * triggered.
+	 *
+	 *# Custom Finalizers
+	 * There are several provided finalizers, but user may create their own as desired.
+	 * A valid finalizer has to satisfy the ``finalize_policy_for`` concept, which in fact requires the existence of a ``finalize_call``
+	 * member function.
+	 * \snippet Finalizers.cpp custom finalizer
+	 *\{
+	 */
+
+	/**
+	 * \brief During the finalization step, the invocation result of the given function is returned.
+	 * \snippet Finalizers.cpp finally::returns_result_of
+	 * \tparam Fun The function type.
+	 * \param fun The function to be invoked.
+	 * \return Forward returns the invocation result of ``fun``.
+	 *
+	 * \details The provided functions must be invocable without arguments, but may return any type, which is explicitly convertible to
+	 * the mocks return type.
+	 */
+	template <typename Fun>
+		requires std::invocable<std::remove_cvref_t<Fun>&>
+				&& (!std::is_void_v<std::invoke_result_t<std::remove_cvref_t<Fun>&>>)
 	[[nodiscard]]
 	constexpr auto returns_result_of(
-		Action&& action  // NOLINT(cppcoreguidelines-missing-std-forward)
-	) noexcept(std::is_nothrow_constructible_v<std::remove_cvref_t<Action>, Action>)
+		Fun&& fun  // NOLINT(cppcoreguidelines-missing-std-forward)
+	) noexcept(std::is_nothrow_constructible_v<std::remove_cvref_t<Fun>, Fun>)
 	{
 		return expectation_policies::ReturnsResultOf{
 			[
-				action = std::forward<Action>(action)
-			]([[maybe_unused]] const auto& call) mutable noexcept(std::is_nothrow_invocable_v<decltype(action)>) -> decltype(auto)
+				fun = std::forward<Fun>(fun)
+			]([[maybe_unused]] const auto& call) mutable noexcept(std::is_nothrow_invocable_v<decltype(fun)>) -> decltype(auto)
 			{
-				return std::invoke(action);
+				return std::invoke(fun);
 			}
 		};
 	}
 
+	/**
+	 * \brief During the finalization step, the stored value is returned.
+	 * \tparam T The value type.
+	 * \param value The value to be returned.
+	 * \return Returns a copy of ``value``.
+	 *
+	 * \details The provided value must be copyable, because multiple invocations must be possible.
+	 * \snippet Finalizers.cpp finally::returns
+	 *
+	 * This finalizer is aware of ``std::reference_wrapper``, which can be used to return values from the outer scope. Such values are
+	 * explicitly unwrapped, before they are returned.
+	 * \snippet Finalizers.cpp finally::returns std::ref
+	 *
+	 * In fact any value category can be returned; it is the users responsibility to make sure, not to use any dangling references.
+	 * If an actual value is stored and a reference is returned, this is fine until the expectation goes out of scope.
+	 * \snippet Finalizers.cpp finally::returns ref
+	 */
 	template <typename T>
 		requires std::copyable<std::remove_cvref_t<T>>
 	[[nodiscard]]
@@ -562,10 +623,22 @@ namespace mimicpp::finally
 		};
 	}
 
+	/**
+	 * \brief During the finalization step, the selected call arguments are applied on the given action.
+	 * \tparam index The first selected argument index.
+	 * \tparam otherIndices Addition selected arguments.
+	 * \tparam Action The action type.
+	 * \param action The action to be applied to.
+	 * \return Returns the invocation result of the given action.
+	 *
+	 * \details The selected call arguments are applied as (possibly const qualified) lvalue-references. The action may proceed with them
+	 * as desired, but be aware that this may actually affect objects outside the call (e.g. if call params are lvalues.).
+	 * \snippet Finalizers.cpp finally::returns_apply_result_of
+	 */
 	template <std::size_t index, std::size_t... otherIndices, typename Action>
 	[[nodiscard]]
 	constexpr auto returns_apply_result_of(
-		Action&& action  // NOLINT(cppcoreguidelines-missing-std-forward)
+		Action&& action
 	) noexcept(std::is_nothrow_constructible_v<std::remove_cvref_t<Action>, Action>)
 	{
 		return expectation_policies::ReturnsResultOf{
@@ -579,19 +652,13 @@ namespace mimicpp::finally
 		};
 	}
 
-	namespace detail
-	{
-		struct forward_fn
-		{
-			template <typename T>
-			[[nodiscard]]
-			constexpr T&& operator ()(T&& obj) const noexcept
-			{
-				return std::forward<T>(obj);
-			}
-		};
-	}
-
+	/**
+	 * \brief During the finalization step, the selected call argument is returned.
+	 * \return Returns the forwarded and explicitly converted argument.
+	 *
+	 * \details The selected call argument is forwarded and explicitly converted to the mocks return type.
+	 * \snippet Finalizers.cpp finally::returns_param
+	 */
 	template <std::size_t index>
 	[[nodiscard]]
 	constexpr auto returns_param() noexcept
@@ -606,6 +673,15 @@ namespace mimicpp::finally
 		};
 	}
 
+	/**
+	 * \brief During the finalization step, the given exception is thrown.
+	 * \tparam T The value type.
+	 * \param value The value to be returned.
+	 * \return Returns a copy of ``value``.
+	 *
+	 * \details The provided exception must be copyable, because multiple invocations must be possible.
+	 * \snippet Finalizers.cpp finally::throws
+	 */
 	template <typename T>
 		requires std::copyable<std::remove_cvref_t<T>>
 	[[nodiscard]]
@@ -617,6 +693,12 @@ namespace mimicpp::finally
 			std::forward<T>(exception)
 		};
 	}
+
+	/**
+	 * \}
+	 */
+
+	// ReSharper restore CppDoxygenUnresolvedReference
 }
 
 namespace mimicpp::then
