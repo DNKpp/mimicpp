713a577ea7c0e13b57f6abdbcbc5a8c01510db15
DNKpp <dnkpp2011@gmail.com>
2024-04-27 22:01:21 +0200

    feat: add finalizer examples

---
+++ examples/Finalizers.cpp
@@ -0,0 +1,164 @@
+// //          Copyright Dominic (DNKpp) Koepke 2024 - 2024.
+// // Distributed under the Boost Software License, Version 1.0.
+// //    (See accompanying file LICENSE_1_0.txt or copy at
+// //          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "mimic++/Mock.hpp"
+
+#include "../test/TestReporter.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <stdexcept>
+
+TEST_CASE(
+	"Finalizers can return pre-defined values.",
+	"[example][example::finalizer]"
+)
+{
+	//! [finally::returns]
+	namespace finally = mimicpp::finally;
+
+	mimicpp::Mock<int()> mock{};
+
+	SCOPED_EXP mock.expect_call()
+				| finally::returns(42);
+
+	REQUIRE(42 == mock());
+	//! [finally::returns]
+}
+
+TEST_CASE(
+	"Returning finalizers are aware of std::reference_wrapper.",
+	"[example][example::finalizer]"
+)
+{
+	//! [finally::returns std::ref]
+	namespace finally = mimicpp::finally;
+
+	mimicpp::Mock<int&()> mock{};
+
+	int myReturn{42};
+	SCOPED_EXP mock.expect_call()
+				| finally::returns(std::ref(myReturn));
+
+	REQUIRE(&myReturn == &mock());
+	//! [finally::returns std::ref]
+}
+
+TEST_CASE(
+	"Storing returned reference is fine, as long as the expectation is alive.",
+	"[example][example::finalizer]"
+)
+{
+	//! [finally::returns ref]
+	namespace finally = mimicpp::finally;
+	namespace expect = mimicpp::expect;
+
+	mimicpp::Mock<int&()> mock{};
+
+	SCOPED_EXP mock.expect_call()
+				| expect::twice()	// we call the mock two times
+				| finally::returns(42);
+
+	int& result = mock();
+	REQUIRE(42 == result);	// fine
+
+	result = 1337;
+	REQUIRE(1337 == mock());
+	//! [finally::returns std::ref]
+}
+
+TEST_CASE(
+	"Finalizers can throw exceptions.",
+	"[example][example::finalizer]"
+)
+{
+	//! [finally::throws]
+	namespace finally = mimicpp::finally;
+
+	mimicpp::Mock<int()> mock{};
+
+	SCOPED_EXP mock.expect_call()
+				| finally::throws(std::runtime_error{"Something happened."});
+
+	REQUIRE_THROWS_AS(
+		mock(),
+		std::runtime_error);
+	//! [finally::throws]
+}
+
+TEST_CASE(
+	"Finalizers can return the result of a function call.",
+	"[example][example::finalizer]"
+)
+{
+	//! [finally::returns_result_of]
+	namespace finally = mimicpp::finally;
+
+	mimicpp::Mock<std::string()> mock{};
+
+	SCOPED_EXP mock.expect_call()
+				| finally::returns_result_of([] { return "Hello, World!"; });
+
+	REQUIRE("Hello, World!" == mock());
+	//! [finally::returns_result_of]
+}
+
+TEST_CASE(
+	"Finalizers can return any given argument.",
+	"[example][example::finalizer]"
+)
+{
+	//! [finally::returns_param]
+	namespace finally = mimicpp::finally;
+
+	mimicpp::Mock<int(int, int)> mock{};
+
+	SCOPED_EXP mock.expect_call(1337, 42)
+				| finally::returns_param<1>();
+
+	REQUIRE(42 == mock(1337, 42));
+	//! [finally::returns_param]
+}
+
+TEST_CASE(
+	"Finalizers can apply any argument to a function and then return the invocation result.",
+	"[example][example::finalizer]"
+)
+{
+	//! [finally::returns_apply_result_of]
+	namespace finally = mimicpp::finally;
+
+	mimicpp::Mock<int(int, int)> mock{};
+
+	SCOPED_EXP mock.expect_call(1337, 42)
+				| finally::returns_apply_result_of<0, 1>(std::plus{});
+
+	REQUIRE(1379 == mock(1337, 42));
+	//! [finally::returns_apply_result_of]
+}
+
+TEST_CASE(
+	"Custom finalizers are supported.",
+	"[example][example::finalizer]"
+)
+{
+	//! [custom finalizer]
+	class MyFinalizer
+	{
+	public:
+		int finalize_call(const mimicpp::call::Info<int>& callInfo)
+		{
+			return 1337;
+		}
+	};
+
+	mimicpp::Mock<int()> mock{};
+
+	SCOPED_EXP mock.expect_call()
+				| MyFinalizer{};
+
+	REQUIRE(1379 == mock());
+	//! [custom finalizer]
+}
