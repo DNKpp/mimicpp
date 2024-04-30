a15389990d34ab738ad01abc4a00c14c65f6dc95
DNKpp <dnkpp2011@gmail.com>
2024-04-30 23:04:12 +0200

    docs: add sequence documentation

--- include/mimic++/Sequence.hpp
+++ include/mimic++/Sequence.hpp
@@ -20,10 +20,65 @@ namespace mimicpp::expectation_policies
 
 namespace mimicpp
 {
+	/**
+	 * \defgroup EXPECTATION_SEQUENCE sequence
+	 * \ingroup EXPECTATION
+	 * \ingroup EXPECTATION_TIMES
+	 * \brief Sequences enable deterministic ordering between multiple expectations.
+	 * \details Their aim is to provide a convenient way for users, to circumvent the rather loosely ordering of expectations,
+	 * which is by design. By default, if two or more expectations would match a call, the last created one is used.
+	 * If multiple expectations are built one after another, that may be rather unintuitive design, but when it comes to
+	 * scoping, it's usually preferable to match the expectations from within the current scope, even if there exist
+	 * another similar expectation from the outside scope.
+	 *
+	 * As I have no idea how to use one strategy for one case and the other for the other case, I decided to take the same
+	 * route as ``trompeloeil``.
+	 * \see https://github.com/rollbear/trompeloeil/blob/main/docs/CookBook.md#sequences
+	 *
+	 * Either way, this is sometimes not enough, when we want to enforce deterministic ordering between two or more expectations.
+	 * That's where sequences come into play. The expectation, which gets attached first must match before all subsequent
+	 * expectations. If that one is fulfilled, the next one in the row must be matched; and so on.
+	 * \snippet Sequences.cpp sequence
+	 *
+	 * Sequences can also enforce orders on expectations, which refer to different mocks.
+	 * \snippet Sequences.cpp sequence multiple mocks
+	 *
+	 * Sequenced and non-sequenced expectations may be arbitrarily mixed; even if this can be very difficult to trace, by
+	 * simply reviewing the code.
+	 * \snippet Sequences.cpp sequence mixed
+	 * 
+	 * It's totally fine to attach expectations to sequences, which are already queried for matches. Sequences do not have
+	 * to be setup in one go.
+	 *
+	 * # Thread-Safety
+	 * Sequences are not thread-safe and are never intended to be. If one attempts to enforce a strong ordering between
+	 * multiple threads without any explicit synchronisation, that attempt is doomed to fail.
+	 *
+	 * # Afterthoughts
+	 * Sequence policies are treated as ``times_policy`` and therefore can not be mixed with e.g. ``Times``.
+	 * At a first glance this seems rather unintuitive, as a sequence does not actually mind how often an expectation
+	 * is matched, but rather if one expectation has been matched before another. This being true, there is still a
+	 * huge intersection between those cases, because a sequence still must know how often an expectation is expected
+	 * to match. This would lead to higher coupling between two rather unrelated domains, but isn't the actual deal-breaker.
+	 * The actual reason is, that both, ``Times`` and ``RuntimeTimes`` are rather permissive designed and thus allow for
+	 * a wide range of valid configurations. Especially the cases, where a range of possible calls is configured (like
+	 * ``at_least``), makes it very hard for a sequence to reliably determine, whether an expectation shall match or not.
+	 * When users define expectations not precise enough, this quickly leads to ambiguities between multiple expectations
+	 * which may and will result in surprising outcomes.
+	 *\{
+	 */
+
+	/**
+	 * \brief Strong type for internally used sequence ids.
+	 */
 	enum class SequenceId
 		: std::size_t
 	{
 	};
+
+	/**
+	 * \}
+	 */
 
 	namespace detail
 	{
@@ -102,6 +157,13 @@ namespace mimicpp
 		};
 	}
 
+	/**
+	 * \brief The user level sequence object.
+	 * \ingroup EXPECTATION_SEQUENCE
+	 * \details This class is just a very thin wrapper and does nothing by its own. It just exists, so that users
+	 * have something they can attach expectations to. In fact, objects of this type may even go out of scope before
+	 * the attached expectations are destroyed.
+	 */
 	class Sequence
 	{
 		friend class expectation_policies::Sequence;
@@ -172,10 +234,13 @@ namespace mimicpp::expectation_policies
 namespace mimicpp::expect
 {
 	/**
-	 * \brief 
-	 * \param sequence 
-	 * \param times 
-	 * \return 
+	 * \brief Attaches the expectation onto a sequence.
+	 * \ingroup EXPECTATION_REQUIREMENT_TIMES
+	 * \ingroup EXPECTATION_SEQUENCE
+	 * \param sequence The sequence to be attached to.
+	 * \param times The expected times.
+	 * \snippet Sequences.cpp sequence
+	 * \snippet Sequences.cpp sequence mixed
 	 */
 	[[nodiscard]]
 	inline auto in_sequence(Sequence& sequence, const std::size_t times = 1u)
