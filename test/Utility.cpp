761b53b1dde8bbdc6d1607aab69a6f0cbc772d3c
DNKpp <dnkpp2011@gmail.com>
2024-05-01 20:55:16 +0200

    trst: add to_underlying test cases

---
+++ test/Utility.cpp
@@ -0,0 +1,63 @@
+// //          Copyright Dominic (DNKpp) Koepke 2024 - 2024.
+// // Distributed under the Boost Software License, Version 1.0.
+// //    (See accompanying file LICENSE_1_0.txt or copy at
+// //          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "mimic++/Utility.hpp"
+
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+#include <cstdint>
+
+using namespace mimicpp;
+
+TEMPLATE_TEST_CASE(
+	"to_underlying converts the enum value to its underlying representation.",
+	"[utility]",
+	std::int8_t,
+	std::uint8_t,
+	std::int16_t,
+	std::uint16_t,
+	std::int32_t,
+	std::uint32_t,
+	std::int64_t,
+	std::uint64_t
+)
+{
+	using UnderlyingT = TestType;
+
+	SECTION("When an enum value is given.")
+	{
+		enum Test
+			: UnderlyingT
+		{
+		};
+
+		const UnderlyingT value = GENERATE(
+			std::numeric_limits<UnderlyingT>::min(),
+			0,
+			1,
+			std::numeric_limits<UnderlyingT>::max());
+
+		STATIC_REQUIRE(std::same_as<UnderlyingT, decltype(to_underlying(Test{value}))>);
+		REQUIRE(value == to_underlying(Test{value}));
+	}
+
+	SECTION("When an class enum value is given.")
+	{
+		enum class Test
+			: UnderlyingT
+		{
+		};
+
+		const UnderlyingT value = GENERATE(
+			std::numeric_limits<UnderlyingT>::min(),
+			0,
+			1,
+			std::numeric_limits<UnderlyingT>::max());
+
+		STATIC_REQUIRE(std::same_as<UnderlyingT, decltype(to_underlying(Test{value}))>);
+		REQUIRE(value == to_underlying(Test{value}));
+	}
+}
