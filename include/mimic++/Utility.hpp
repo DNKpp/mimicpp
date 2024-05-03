5062cea6d87c5aaebaf9477d088e4045527e9179
DNKpp <dnkpp2011@gmail.com>
2024-05-03 23:58:06 +0200

    feat: unreachable helper function

--- include/mimic++/Utility.hpp
+++ include/mimic++/Utility.hpp
@@ -142,6 +142,25 @@ namespace mimicpp
 	{
 		return static_cast<std::underlying_type_t<T>>(value);
 	}
+
+#ifdef __cpp_lib_unreachable
+	using std::unreachable;
+#else
+
+	// taken from: https://en.cppreference.com/w/cpp/utility/unreachable
+	[[noreturn]]
+	inline void unreachable()
+	{
+	    // Uses compiler specific extensions if possible.
+	    // Even if no extension is used, undefined behavior is still raised by
+	    // an empty function body and the noreturn attribute.
+	#if defined(_MSC_VER) && !defined(__clang__) // MSVC
+	    __assume(false);
+	#else // GCC, Clang
+	    __builtin_unreachable();
+	#endif
+	}
+#endif
 }
 
 #endif
