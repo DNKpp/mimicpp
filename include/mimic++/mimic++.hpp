8d08b7e78110e7d92aa4379686c6959916f57daf
DNKpp <dnkpp2011@gmail.com>
2024-06-17 16:23:33 +0200

    feat: add mimic++.hpp all-in-one header

---
+++ include/mimic++/mimic++.hpp
@@ -0,0 +1,28 @@
+// //          Copyright Dominic (DNKpp) Koepke 2024 - 2024.
+// // Distributed under the Boost Software License, Version 1.0.
+// //    (See accompanying file LICENSE_1_0.txt or copy at
+// //          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MIMICPP_MIMICPP_HPP
+#define MIMICPP_MIMICPP_HPP
+
+#pragma once
+
+#include "mimic++/Fwd.hpp"
+
+#include "mimic++/Utility.hpp"
+#include "mimic++/TypeTraits.hpp"
+
+#include "mimic++/Printer.hpp"
+#include "mimic++/Reporter.hpp"
+
+#include "mimic++/Sequence.hpp"
+#include "mimic++/Matcher.hpp"
+#include "mimic++/ExpectationPolicies.hpp"
+#include "mimic++/ExpectationBuilder.hpp"
+
+#include "mimic++/Call.hpp"
+#include "mimic++/Expectation.hpp"
+#include "mimic++/Mock.hpp"
+
+#endif
