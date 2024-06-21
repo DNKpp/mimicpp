895d41fd6855655a2b3f5fceae08fc00058b1bc9
DNKpp <dnkpp2011@gmail.com>
2024-06-21 21:27:28 +0200

    feat: wip ControlPolicy and ControlPolicyConfig

---
+++ include/mimic++/ControlPolicy.hpp
@@ -0,0 +1,201 @@
+// //          Copyright Dominic (DNKpp) Koepke 2024 - 2024.
+// // Distributed under the Boost Software License, Version 1.0.
+// //    (See accompanying file LICENSE_1_0.txt or copy at
+// //          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MIMICPP_CONTROL_POLICY_HPP
+#define MIMICPP_CONTROL_POLICY_HPP
+
+#pragma once
+
+#include "mimic++/Printer.hpp"
+#include "mimic++/Reporter.hpp"
+#include "mimic++/Utility.hpp"
+
+#include <cassert>
+#include <limits>
+#include <stdexcept>
+
+namespace mimicpp
+{
+	class ControlPolicyConfig
+	{
+	public:
+		constexpr void set_limits(const int min, const int max)
+		{
+			if (min < 0
+				|| max < 0
+				|| max < min)
+			{
+				throw std::invalid_argument{
+					"min must be less or equal to max and both must not be less than zero."
+				};
+			}
+
+			m_Min = min;
+			m_Max = max;
+		}
+
+		[[nodiscard]]
+		constexpr int min() const noexcept
+		{
+			return m_Min;
+		}
+
+		[[nodiscard]]
+		constexpr int max() const noexcept
+		{
+			return m_Max;
+		}
+
+	private:
+		int m_Min{1};
+		int m_Max{1};
+	};
+
+	class ControlPolicy
+	{
+	public:
+		[[nodiscard]]
+		explicit constexpr ControlPolicy(const ControlPolicyConfig& config) noexcept
+			: m_Min{config.min()},
+			m_Max{config.max()}
+		{
+		}
+
+		[[nodiscard]]
+		constexpr bool is_satisfied() const noexcept
+		{
+			return m_Min <= m_Count
+					&& m_Count <= m_Max;
+		}
+
+		[[nodiscard]]
+		constexpr bool is_applicable() const noexcept
+		{
+			return m_Count < m_Max;
+		}
+
+		constexpr void consume() noexcept
+		{
+			assert(m_Count < m_Max);
+
+			++m_Count;
+		}
+
+		[[nodiscard]]
+		StringT describe_state() const
+		{
+			return {};
+		}
+
+	private:
+		int m_Min{};
+		int m_Max{};
+		int m_Count{};
+	};
+}
+
+namespace mimicpp::expect
+{
+	/**
+	 * \defgroup EXPECTATION_TIMES times
+	 * \ingroup EXPECTATION
+	 * \brief Times indicates, how often an expectation must be matched.
+	 * \details During each expectation building users can specify a times policy once. If not specified, that policy defaults to ``once``.
+	 * If users attempt to specify a times policy more than once for a single expectation, a compile-error will occur.
+	 *
+	 * Times in general have both, a lower and an upper limit. Both limits are treated as inclusive.
+	 *
+	 *\{
+	 */
+
+	/**
+	 * \brief Specifies a times policy with a limit range.
+	 * \param min The lower limit.
+	 * \param max The upper limit.
+	 * \return The newly created policy.
+	 *
+	 * \snippet Times.cpp times rt
+	 */
+	[[nodiscard]]
+	constexpr auto times(const int min, const int max)
+	{
+		ControlPolicyConfig config{};
+		config.set_limits(min, max);
+		return config;
+	}
+
+	/**
+	 * \brief Specifies a times policy with an exact limit.
+	 * \param exactly The limit.
+	 * \return The newly created policy.
+	 * \details This requires the expectation to be matched exactly the specified times.
+	 * \snippet Times.cpp times single rt
+	 */
+	[[nodiscard]]
+	constexpr auto times(const int exactly) noexcept
+	{
+		return times(exactly, exactly);
+	}
+
+	/**
+	 * \brief Specifies a times policy with just a lower limit.
+	 * \tparam min The lower limit.
+	 * \return The newly created policy.
+	 * \details This requires the expectation to be matched at least ``min`` times or more.
+	 * \snippet Times.cpp at_least compile-time
+	 */
+	[[nodiscard]]
+	constexpr auto at_least(const int min) noexcept
+	{
+		return times(
+			min,
+			std::numeric_limits<int>::max());
+	}
+
+	/**
+	 * \brief Specifies a times policy with just an upper limit.
+	 * \param max The upper limit.
+	 * \return The newly created policy.
+	 * \details This requires the expectation to be matched up to ``max`` times.
+	 */
+	[[nodiscard]]
+	constexpr auto at_most(const int max) noexcept
+	{
+		return times(
+			0,
+			max);
+	}
+
+	/**
+	 * \brief Specifies a times policy with both limits set to 1.
+	 * \return The newly created policy.
+	 * \details This requires the expectation to be matched exactly once.
+	 * \snippet Times.cpp once
+	 */
+	[[nodiscard]]
+	consteval auto once() noexcept
+	{
+		return times(1);
+	}
+
+	/**
+	 * \brief Specifies a times policy with both limits set to 2.
+	 * \return The newly created policy.
+	 * \details This requires the expectation to be matched exactly twice.
+	 * \snippet Times.cpp twice
+	 */
+	[[nodiscard]]
+	consteval auto twice() noexcept
+	{
+		return times(2);
+	}
+
+	/**
+	 * \}
+	 */
+
+}
+
+#endif
