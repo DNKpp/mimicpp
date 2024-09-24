b38b58334ee83bf44835803983b06a2730b46c5a
DNKpp <dnkpp2011@gmail.com>
2024-09-24 22:26:43 +0200

    feat: add InterfaceMock.hpp

---
+++ include/mimic++/InterfaceMock.hpp
@@ -0,0 +1,23 @@
+// //          Copyright Dominic (DNKpp) Koepke 2024 - 2024.
+// // Distributed under the Boost Software License, Version 1.0.
+// //    (See accompanying file LICENSE_1_0.txt or copy at
+// //          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MIMICPP_INTERFACE_MOCK_HPP
+#define MIMICPP_INTERFACE_MOCK_HPP
+
+#pragma once
+
+#include "mimic++/Fwd.hpp"
+#include "mimic++/Mock.hpp"
+
+#endif
+
+namespace mimicpp
+{
+	/**
+	 * \defgroup INTERFACES interfaces
+	 * \ingroup MOCK
+	 * \brief Contains utility to simplify interface mocking.
+	 */
+}
---
+++ test/unit-tests/InterfaceMock.cpp
@@ -0,0 +1,19 @@
+ //          Copyright Dominic (DNKpp) Koepke 2024 - 2024.
+ // Distributed under the Boost Software License, Version 1.0.
+ //    (See accompanying file LICENSE_1_0.txt or copy at
+ //          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "TestReporter.hpp"
+#include "TestTypes.hpp"
+
+#include "mimic++/InterfaceMock.hpp"
+
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/generators/catch_generators_range.hpp>
+#include <catch2/matchers/catch_matchers_container_properties.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
+#include <catch2/matchers/catch_matchers_templated.hpp>
+
+
