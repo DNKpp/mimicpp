0e16d3e8d87996b5205d292809ba71da4ec90629
DNKpp <dnkpp2011@gmail.com>
2024-09-26 21:26:02 +0200

    feat: interface mocks example

---
+++ examples/InterfaceMock.cpp
@@ -0,0 +1,130 @@
+// //          Copyright Dominic (DNKpp) Koepke 2024 - 2024.
+// // Distributed under the Boost Software License, Version 1.0.
+// //    (See accompanying file LICENSE_1_0.txt or copy at
+// //          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "mimic++/InterfaceMock.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE(
+	"Mocking interface methods by hand.",
+	"[example][example::mock][example::mock::interface]"
+)
+{
+	//! [interface mock manual]
+	class Interface
+	{
+	public:
+		virtual ~Interface() = default;
+		virtual void foo() = 0;
+	};
+
+	class Derived
+		: public Interface
+	{
+	public:
+		~Derived() override = default;
+
+		// Begin boilerplate
+
+		// we build our mock object by hand
+		mimicpp::Mock<void()> foo_{};
+
+		// and forward the incoming call from the interface function to the mock object.
+		void foo() override
+		{
+			return foo_();
+		}
+
+		// End boilerplate
+	};
+
+	// this may be a function from somewhere else, working with an interface.
+	constexpr auto my_function = [](Interface& obj)
+	{
+		obj.foo();
+	};
+
+	Derived mock{};
+	SCOPED_EXP mock.foo_.expect_call();
+
+	my_function(mock);
+	//! [interface mock manual]
+}
+
+TEST_CASE(
+	"Interfaces methods can be mocked.",
+	"[example][example::mock][example::mock::interface]"
+)
+{
+	//! [interface mock simple]
+	class Interface
+	{
+	public:
+		virtual ~Interface() = default;
+		virtual void foo() = 0;
+	};
+
+	class Derived
+		: public Interface
+	{
+	public:
+		~Derived() override = default;
+
+		// this generates the override method and a mock object named foo_
+		MOCK_METHOD(foo, void, ());
+	};
+
+	// this may be a function from somewhere else, working with an interface.
+	constexpr auto my_function = [](Interface& obj)
+	{
+		obj.foo();
+	};
+
+	Derived mock{};
+	SCOPED_EXP mock.foo_.expect_call();	// note the _ suffix. That's the name of the mock object.
+
+	my_function(mock);
+	//! [interface mock simple]
+}
+
+TEST_CASE(
+	"Overloaded interface methods can be mocked.",
+	"[example][example::mock][example::mock::interface]"
+)
+{
+	//! [interface mock overloaded]
+	class Interface
+	{
+	public:
+		virtual ~Interface() = default;
+		virtual void foo() = 0;
+		virtual void foo() const noexcept = 0;
+	};
+
+	class Derived
+		: public Interface
+	{
+	public:
+		~Derived() override = default;
+
+		// this generates both overrides and a mock object named foo_
+		MOCK_OVERLOADED_METHOD(
+			foo,
+			ADD_OVERLOAD(void, ()),
+			ADD_OVERLOAD(void, (), const noexcept));	// note the ``const noexcept`` specifications as third argument
+	};
+
+	// this may be a function from somewhere else, working with an immutable interface.
+	constexpr auto my_function = [](const Interface& obj)
+	{
+		obj.foo();
+	};
+
+	Derived mock{};
+	SCOPED_EXP std::as_const(mock).foo_.expect_call();	// We explicitly require the const overload to be called.
+
+	my_function(mock);
+	//! [interface mock overloaded]
+}
