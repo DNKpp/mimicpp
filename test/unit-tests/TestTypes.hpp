64a97a2d82d0e727f645fae8b26f2fe554b20e1c
DNKpp <dnkpp2011@gmail.com>
2024-10-08 17:01:09 +0200

    fix: disable -Wself-move suppression on gcc-12

--- test/unit-tests/TestTypes.hpp
+++ test/unit-tests/TestTypes.hpp
@@ -31,7 +31,15 @@
 #define STOP_WARNING_SUPPRESSION	_Pragma("GCC diagnostic pop")
 
 #define SUPPRESS_UNREACHABLE_CODE	_Pragma("GCC diagnostic ignored \"-Wunreachable-code\"")
+
+// gcc 12 doesn't know -Wself-move option
+#if !defined(__clang__) \
+	&& defined(__GNUC__) \
+	&& 12 >= defined(__GNUC__)
+#define SUPPRESS_SELF_MOVE
+#else
 #define SUPPRESS_SELF_MOVE			_Pragma("GCC diagnostic ignored \"-Wself-move\"")
+#endif
 
 #endif
 
