4b416c706266b04235a699298fd52cb4be355275
DNKpp <dnkpp2011@gmail.com>
2024-10-08 16:42:43 +0200

    fix: treat warning flags for clang-cl special

--- cmake/SetupTestTarget.cmake
+++ cmake/SetupTestTarget.cmake
@@ -6,6 +6,8 @@
 
 function(setup_test_target TARGET_NAME)
 
+	find_package(sanitizers-cmake)
+
 	if (SANITIZE_ADDRESS)
 
 		# workaround linker errors on msvc
@@ -21,11 +23,22 @@ function(setup_test_target TARGET_NAME)
 
 	add_sanitizers(${TARGET_NAME})
 
-	set(WARNING_FLAGS
-		$<IF:$<CXX_COMPILER_ID:MSVC>,
-			/W4 /WX,
-			-Wall -Wextra -Wpedantic -Werror>
-	)
+	# We need to circumvent the huge nonsense warnings from clang-cl
+	# see: https://discourse.cmake.org/t/wall-with-visual-studio-clang-toolchain-results-in-way-too-many-warnings/7927
+	if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang"
+		AND CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
+
+		set(WARNING_FLAGS /W4 -Wextra -Wpedantic -Werror -Wno-unknown-attributes)
+
+	else()
+
+		set(WARNING_FLAGS
+			$<IF:$<CXX_COMPILER_ID:MSVC>,
+				/W4 /WX,
+				-Wall -Wextra -Wpedantic -Werror>
+		)
+
+	endif()
 
 	target_compile_options(
 		${TARGET_NAME}
