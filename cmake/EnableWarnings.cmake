0bb97d6e361865706544b7c27cd673d7f915666e
Tim Ebbeke <Tim06TR@gmail.com>
2024-10-08 21:18:41 +0200

    chore: split warnings from setup function

---
+++ cmake/EnableWarnings.cmake
@@ -0,0 +1,21 @@
+add_library(enable-warnings INTERFACE)
+
+# We need to circumvent the huge nonsense warnings from clang-cl
+# see: https://discourse.cmake.org/t/wall-with-visual-studio-clang-toolchain-results-in-way-too-many-warnings/7927
+if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang"
+	AND CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
+
+	set(WARNING_FLAGS /W4 -Wextra -Wpedantic -Werror -Wno-unknown-attributes)
+else()
+	set(WARNING_FLAGS
+		$<IF:$<CXX_COMPILER_ID:MSVC>,
+		/W4 /WX,
+		-Wall -Wextra -Wpedantic -Werror>
+	)
+endif()
+
+target_compile_options(
+	enable-warnings
+	INTERFACE
+	${WARNING_FLAGS}
+)
