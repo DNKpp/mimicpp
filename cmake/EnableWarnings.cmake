c807091185708f25e00690d81e61b1c5d5cc749f
DNKpp <dnkpp2011@gmail.com>
2024-10-09 15:03:16 +0200

    fix: include EnableWarnings.cmake only when necessary

--- cmake/EnableWarnings.cmake
+++ cmake/EnableWarnings.cmake
@@ -1,22 +1,27 @@
-add_library(enable-warnings INTERFACE)
-add_library(mimicpp::internal::warnings ALIAS enable-warnings)
 
-# We need to circumvent the huge nonsense warnings from clang-cl
-# see: https://discourse.cmake.org/t/wall-with-visual-studio-clang-toolchain-results-in-way-too-many-warnings/7927
-if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang"
-	AND CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
+if (NOT TARGET enable-warnings)
 
-	set(WARNING_FLAGS /W4 -Wextra -Wpedantic -Werror -Wno-unknown-attributes)
-else()
-	set(WARNING_FLAGS
-		$<IF:$<CXX_COMPILER_ID:MSVC>,
-		/W4 /WX,
-		-Wall -Wextra -Wpedantic -Werror>
+	add_library(enable-warnings INTERFACE)
+	add_library(mimicpp::internal::warnings ALIAS enable-warnings)
+
+	# We need to circumvent the huge nonsense warnings from clang-cl
+	# see: https://discourse.cmake.org/t/wall-with-visual-studio-clang-toolchain-results-in-way-too-many-warnings/7927
+	if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang"
+		AND CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
+
+		set(WARNING_FLAGS /W4 -Wextra -Wpedantic -Werror -Wno-unknown-attributes)
+	else()
+		set(WARNING_FLAGS
+			$<IF:$<CXX_COMPILER_ID:MSVC>,
+			/W4 /WX,
+			-Wall -Wextra -Wpedantic -Werror>
+		)
+	endif()
+
+	target_compile_options(
+		enable-warnings
+		INTERFACE
+		${WARNING_FLAGS}
 	)
-endif()
 
-target_compile_options(
-	enable-warnings
-	INTERFACE
-	${WARNING_FLAGS}
-)
+endif()
