de64fef5aacae2b738a66b91324f834f7645296b
DNKpp <dnkpp2011@gmail.com>
2024-12-11 17:10:15 +0100

    chore: update formatting for GTest-Adapter related files

--- include/mimic++/adapters/gtest.hpp
+++ include/mimic++/adapters/gtest.hpp
@@ -11,61 +11,59 @@
 #include "mimic++/Reporter.hpp"
 
 #if __has_include(<gtest/gtest.h>)
-#include <gtest/gtest.h>
+    #include <gtest/gtest.h>
 #else
-	#error "Unable to find gtest includes."
+    #error "Unable to find gtest includes."
 #endif
 
 namespace mimicpp::detail::gtest
 {
-	struct failure
-	{
-	};
+    struct failure
+    {
+    };
 
-	[[noreturn]]
-	inline void send_fail(const StringViewT& msg)
-	{
-		// GTEST_FAIL has an immediate return
-		std::invoke(
-			[&]
-			{
-				GTEST_FAIL() << msg;
-			});
+    [[noreturn]]
+    inline void send_fail(const StringViewT& msg)
+    {
+        // GTEST_FAIL has an immediate return
+        std::invoke(
+            [&] {
+                GTEST_FAIL() << msg;
+            });
 
-		throw failure{};
-	}
+        throw failure{};
+    }
 
-	inline void send_success(const StringViewT& msg)
-	{
-		GTEST_SUCCEED() << msg;
-	}
+    inline void send_success(const StringViewT& msg)
+    {
+        GTEST_SUCCEED() << msg;
+    }
 
-	inline void send_warning([[maybe_unused]] const StringViewT& msg)
-	{
-		// seems unsupported
-	}
+    inline void send_warning([[maybe_unused]] const StringViewT& msg)
+    {
+        // seems unsupported
+    }
 }
 
 namespace mimicpp
 {
-	/**
-	 * \brief Reporter for the integration into gtest.
-	 * \ingroup REPORTING_ADAPTERS
-	 * \details This reporter enables the integration of ``mimic++`` into ``gtest`` and prefixes the headers
-	 * of ``gtest`` with ``gtest/``.
-	 *
-	 * This reporter installs itself by simply including this header file into any source file of the test executable.
-	 */
-	using GTestReporterT = BasicReporter<
-		&detail::gtest::send_success,
-		&detail::gtest::send_warning,
-		&detail::gtest::send_fail
-	>;
+    /**
+     * \brief Reporter for the integration into gtest.
+     * \ingroup REPORTING_ADAPTERS
+     * \details This reporter enables the integration of ``mimic++`` into ``gtest`` and prefixes the headers
+     * of ``gtest`` with ``gtest/``.
+     *
+     * This reporter installs itself by simply including this header file into any source file of the test executable.
+     */
+    using GTestReporterT = BasicReporter<
+        &detail::gtest::send_success,
+        &detail::gtest::send_warning,
+        &detail::gtest::send_fail>;
 }
 
 namespace mimicpp::detail::gtest
 {
-	inline const ReporterInstaller<GTestReporterT> installer{};
+    inline const ReporterInstaller<GTestReporterT> installer{};
 }
 
 #endif
--- test/adapter-tests/gtest/main.cpp
+++ test/adapter-tests/gtest/main.cpp
@@ -10,105 +10,100 @@
 
 namespace
 {
-	inline int g_SuccessCounter{0};
+    inline int g_SuccessCounter{0};
 
-	class SuccessListener final
-		: public testing::EmptyTestEventListener
-	{
-	public:
-		void OnTestPartResult(const testing::TestPartResult& result) override
-		{
-			if (result.passed())
-			{
-				++g_SuccessCounter;
-			}
-		}
-	};
+    class SuccessListener final
+        : public testing::EmptyTestEventListener
+    {
+    public:
+        void OnTestPartResult(const testing::TestPartResult& result) override
+        {
+            if (result.passed())
+            {
+                ++g_SuccessCounter;
+            }
+        }
+    };
 
-	template <typename Listener>
-	class ScopedListener
-	{
-	public:
-		~ScopedListener() noexcept
-		{
-			delete testing::UnitTest::GetInstance()
-					->listeners()
-					.Release(m_Ptr);
-		}
+    template <typename Listener>
+    class ScopedListener
+    {
+    public:
+        ~ScopedListener() noexcept
+        {
+            delete testing::UnitTest::GetInstance()
+                ->listeners()
+                .Release(m_Ptr);
+        }
 
-		[[nodiscard]]
-		ScopedListener()
-			: m_Ptr{new Listener{}}
-		{
-			testing::UnitTest::GetInstance()
-				->listeners()
-				.Append(m_Ptr);
-		}
+        [[nodiscard]]
+        ScopedListener()
+            : m_Ptr{new Listener{}}
+        {
+            testing::UnitTest::GetInstance()
+                ->listeners()
+                .Append(m_Ptr);
+        }
 
-	private:
-		Listener* m_Ptr{};
-	};
+    private:
+        Listener* m_Ptr{};
+    };
 }
 
 TEST(
-	GTestReporter,
-	MatchReport
-)
+    GTestReporter,
+    MatchReport)
 {
-	const ScopedListener<SuccessListener> listener{};
+    const ScopedListener<SuccessListener> listener{};
 
-	g_SuccessCounter = 0;
-	mimicpp::Mock<void(int)> mock{};
+    g_SuccessCounter = 0;
+    mimicpp::Mock<void(int)> mock{};
 
-	SCOPED_EXP mock.expect_call(42);
+    SCOPED_EXP mock.expect_call(42);
 
-	ASSERT_EQ(g_SuccessCounter, 0);
-	ASSERT_EQ(g_SuccessCounter, 0);	// check, that ASSERT_EQ isn't reported
-	mock(42);
-	EXPECT_EQ(g_SuccessCounter, 1);
+    ASSERT_EQ(g_SuccessCounter, 0);
+    ASSERT_EQ(g_SuccessCounter, 0); // check, that ASSERT_EQ isn't reported
+    mock(42);
+    EXPECT_EQ(g_SuccessCounter, 1);
 }
 
 TEST(
-	GTestReporter,
-	NoMatchReport
-)
+    GTestReporter,
+    NoMatchReport)
 {
-	EXPECT_FATAL_FAILURE(
-		mimicpp::Mock<void(int)> mock{};
-		EXPECT_ANY_THROW(mock(1337)),
-		"No match for");
+    EXPECT_FATAL_FAILURE(
+        mimicpp::Mock<void(int)> mock{};
+        EXPECT_ANY_THROW(mock(1337)),
+        "No match for");
 }
 
 TEST(
-	GTestReporter,
-	ReportSuccess
-)
+    GTestReporter,
+    ReportSuccess)
 {
-	const ScopedListener<SuccessListener> listener{};
+    const ScopedListener<SuccessListener> listener{};
 
-	g_SuccessCounter = 0;
+    g_SuccessCounter = 0;
 
-	mimicpp::detail::gtest::send_success("Test");
+    mimicpp::detail::gtest::send_success("Test");
 
-	EXPECT_EQ(g_SuccessCounter, 1);
+    EXPECT_EQ(g_SuccessCounter, 1);
 }
 
 TEST(
-	GTestReporter,
-	ReportWarning
-)
+    GTestReporter,
+    ReportWarning)
 {
-	mimicpp::detail::gtest::send_warning("Test");
+    mimicpp::detail::gtest::send_warning("Test");
 
-	// nothing to do
+    // nothing to do
 }
 
 TEST(
-	GTestReporter,
-	ReportFail
-)
+    GTestReporter,
+    ReportFail)
 {
-	EXPECT_FATAL_FAILURE(
-		EXPECT_ANY_THROW(mimicpp::detail::gtest::send_fail("Test")),
-		"Test");
+    EXPECT_FATAL_FAILURE(
+        EXPECT_ANY_THROW(mimicpp::detail::gtest::send_fail("Test")),
+        "Test");
 }
