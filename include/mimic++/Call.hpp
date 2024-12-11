a7019f1bb22d4ff209cb7cfb19dd203f758d5f14
DNKpp <dnkpp2011@gmail.com>
2024-12-11 16:04:37 +0100

    chore: apply formatting on Call.hpp and unit-tests/Call.cpp

--- include/mimic++/Call.hpp
+++ include/mimic++/Call.hpp
@@ -18,62 +18,61 @@
 
 namespace mimicpp::call::detail
 {
-	template <typename... Args, std::size_t... indices>
-	[[nodiscard]]
-	constexpr bool is_equal_param_list(
-		const std::tuple<std::reference_wrapper<Args>...>& lhs,
-		const std::tuple<std::reference_wrapper<Args>...>& rhs,
-		const std::index_sequence<indices...>
-	) noexcept
-	{
-		return (
-			...
-			&& (std::addressof(
-					std::get<indices>(lhs).get())
-				== std::addressof(
-					std::get<indices>(rhs).get())));
-	}
+    template <typename... Args, std::size_t... indices>
+    [[nodiscard]]
+    constexpr bool is_equal_param_list(
+        const std::tuple<std::reference_wrapper<Args>...>& lhs,
+        const std::tuple<std::reference_wrapper<Args>...>& rhs,
+        const std::index_sequence<indices...>) noexcept
+    {
+        return (
+            ...
+            && (std::addressof(
+                    std::get<indices>(lhs).get())
+                == std::addressof(
+                    std::get<indices>(rhs).get())));
+    }
 }
 
 namespace mimicpp::call
 {
-	template <typename Return, typename... Args>
-	class Info
-	{
-	public:
-		using ArgListT = std::tuple<std::reference_wrapper<std::remove_reference_t<Args>>...>;
+    template <typename Return, typename... Args>
+    class Info
+    {
+    public:
+        using ArgListT = std::tuple<std::reference_wrapper<std::remove_reference_t<Args>>...>;
 
-		ArgListT args;
-		ValueCategory fromCategory{};
-		Constness fromConstness{};
-		std::source_location fromSourceLocation{};
+        ArgListT args;
+        ValueCategory fromCategory{};
+        Constness fromConstness{};
+        std::source_location fromSourceLocation{};
 
-		[[nodiscard]]
-		friend bool operator ==(const Info& lhs, const Info& rhs)
-		{
-			return lhs.fromCategory == rhs.fromCategory
-					&& lhs.fromConstness == rhs.fromConstness
-					&& detail::is_equal_param_list(lhs.args, rhs.args, std::index_sequence_for<Args...>{})
-					&& is_same_source_location(lhs.fromSourceLocation, rhs.fromSourceLocation);
-		}
-	};
+        [[nodiscard]]
+        friend bool operator==(const Info& lhs, const Info& rhs)
+        {
+            return lhs.fromCategory == rhs.fromCategory
+                && lhs.fromConstness == rhs.fromConstness
+                && detail::is_equal_param_list(lhs.args, rhs.args, std::index_sequence_for<Args...>{})
+                && is_same_source_location(lhs.fromSourceLocation, rhs.fromSourceLocation);
+        }
+    };
 
-	template <typename Signature>
-	struct info_for_signature
-		/** \cond Help doxygen with recursion.*/
-		: public info_for_signature<signature_decay_t<Signature>>
-		/** \endcond */
-	{
-	};
+    template <typename Signature>
+    struct info_for_signature
+        /** \cond Help doxygen with recursion.*/
+        : public info_for_signature<signature_decay_t<Signature>>
+    /** \endcond */
+    {
+    };
 
-	template <typename Signature>
-	using info_for_signature_t = typename info_for_signature<Signature>::type;
+    template <typename Signature>
+    using info_for_signature_t = typename info_for_signature<Signature>::type;
 
-	template <typename Return, typename... Args>
-	struct info_for_signature<Return(Args...)>
-	{
-		using type = Info<Return, Args...>;
-	};
+    template <typename Return, typename... Args>
+    struct info_for_signature<Return(Args...)>
+    {
+        using type = Info<Return, Args...>;
+    };
 }
 
 #endif
