455a5904d9ee9080b47d449b62e753de16e18718
DNKpp <dnkpp2011@gmail.com>
2024-12-11 17:04:24 +0100

    chore: apply formatting on unit-tests headers

--- test/unit-tests/TestReporter.hpp
+++ test/unit-tests/TestReporter.hpp
@@ -22,158 +22,153 @@ class NonApplicableMatchError
 };
 
 class TestReporter final
-	: public mimicpp::IReporter
+    : public mimicpp::IReporter
 {
 public:
-	using call_report_t = mimicpp::CallReport;
-	using expectation_report_t = mimicpp::ExpectationReport;
-	using match_report_t = mimicpp::MatchReport;
+    using call_report_t = mimicpp::CallReport;
+    using expectation_report_t = mimicpp::ExpectationReport;
+    using match_report_t = mimicpp::MatchReport;
 
-	std::vector<std::tuple<call_report_t, match_report_t>> noMatchResults{};
+    std::vector<std::tuple<call_report_t, match_report_t>> noMatchResults{};
 
-	[[noreturn]]
-	void report_no_matches(
-		call_report_t call,
-		std::vector<mimicpp::MatchReport> matchReports
-	) override
-	{
-		for (auto& exp : matchReports)
-		{
-			noMatchResults.emplace_back(
-				call,
-				std::move(exp));
-		}
+    [[noreturn]]
+    void report_no_matches(
+        call_report_t call,
+        std::vector<mimicpp::MatchReport> matchReports) override
+    {
+        for (auto& exp : matchReports)
+        {
+            noMatchResults.emplace_back(
+                call,
+                std::move(exp));
+        }
 
-		throw NoMatchError{};
-	}
+        throw NoMatchError{};
+    }
 
-	std::vector<std::tuple<call_report_t, match_report_t>> inapplicableMatchResults{};
+    std::vector<std::tuple<call_report_t, match_report_t>> inapplicableMatchResults{};
 
-	[[noreturn]]
-	void report_inapplicable_matches(
-		call_report_t call,
-		std::vector<mimicpp::MatchReport> matchReports
-	) override
-	{
-		for (auto& exp : matchReports)
-		{
-			inapplicableMatchResults.emplace_back(
-				call,
-				std::move(exp));
-		}
+    [[noreturn]]
+    void report_inapplicable_matches(
+        call_report_t call,
+        std::vector<mimicpp::MatchReport> matchReports) override
+    {
+        for (auto& exp : matchReports)
+        {
+            inapplicableMatchResults.emplace_back(
+                call,
+                std::move(exp));
+        }
 
-		throw NonApplicableMatchError{};
-	}
+        throw NonApplicableMatchError{};
+    }
 
-	std::vector<std::tuple<call_report_t, match_report_t>> fullMatchResults{};
+    std::vector<std::tuple<call_report_t, match_report_t>> fullMatchResults{};
 
-	void report_full_match(
-		call_report_t call,
-		mimicpp::MatchReport matchReport
-	) noexcept override
-	{
-		fullMatchResults.emplace_back(
-			std::move(call),
-			std::move(matchReport));
-	}
+    void report_full_match(
+        call_report_t call,
+        mimicpp::MatchReport matchReport) noexcept override
+    {
+        fullMatchResults.emplace_back(
+            std::move(call),
+            std::move(matchReport));
+    }
 
-	std::vector<expectation_report_t> unfulfilledExpectations{};
+    std::vector<expectation_report_t> unfulfilledExpectations{};
 
-	void report_unfulfilled_expectation(
-		expectation_report_t expectationReport
-	) override
-	{
-		unfulfilledExpectations.emplace_back(std::move(expectationReport));
-	}
+    void report_unfulfilled_expectation(
+        expectation_report_t expectationReport) override
+    {
+        unfulfilledExpectations.emplace_back(std::move(expectationReport));
+    }
 
-	std::vector<mimicpp::StringT> errors{};
+    std::vector<mimicpp::StringT> errors{};
 
-	void report_error(mimicpp::StringT message) override
-	{
-		errors.emplace_back(std::move(message));
-	}
+    void report_error(mimicpp::StringT message) override
+    {
+        errors.emplace_back(std::move(message));
+    }
 
-	struct unhandled_exception_info
-	{
-		call_report_t call;
-		expectation_report_t expectation{};
-		std::exception_ptr exception;
-	};
+    struct unhandled_exception_info
+    {
+        call_report_t call;
+        expectation_report_t expectation{};
+        std::exception_ptr exception;
+    };
 
-	std::vector<unhandled_exception_info> unhandledExceptions{};
+    std::vector<unhandled_exception_info> unhandledExceptions{};
 
-	void report_unhandled_exception(
-		call_report_t call,
-		expectation_report_t expectationReport,
-		std::exception_ptr exception
-	) override
-	{
-		unhandledExceptions.emplace_back(
-			std::move(call),
-			std::move(expectationReport),
-			std::move(exception));
-	}
+    void report_unhandled_exception(
+        call_report_t call,
+        expectation_report_t expectationReport,
+        std::exception_ptr exception) override
+    {
+        unhandledExceptions.emplace_back(
+            std::move(call),
+            std::move(expectationReport),
+            std::move(exception));
+    }
 };
 
 class ScopedReporter
 {
 public:
-	~ScopedReporter() noexcept
-	{
-		mimicpp::install_reporter<mimicpp::DefaultReporter>();
-	}
+    ~ScopedReporter() noexcept
+    {
+        mimicpp::install_reporter<mimicpp::DefaultReporter>();
+    }
 
-	ScopedReporter() noexcept
-	{
-		mimicpp::install_reporter<TestReporter>();
-	}
+    ScopedReporter() noexcept
+    {
+        mimicpp::install_reporter<TestReporter>();
+    }
 
-	ScopedReporter(const ScopedReporter&) = delete;
-	ScopedReporter& operator =(const ScopedReporter&) = delete;
-	ScopedReporter(ScopedReporter&&) = delete;
-	ScopedReporter& operator =(ScopedReporter&&) = delete;
+    ScopedReporter(const ScopedReporter&) = delete;
+    ScopedReporter& operator=(const ScopedReporter&) = delete;
+    ScopedReporter(ScopedReporter&&) = delete;
+    ScopedReporter& operator=(ScopedReporter&&) = delete;
 
-	auto& no_match_reports() noexcept
-	{
-		return reporter()
-			.noMatchResults;
-	}
+    auto& no_match_reports() noexcept
+    {
+        return reporter()
+            .noMatchResults;
+    }
 
-	auto& inapplicable_match_reports() noexcept
-	{
-		return reporter()
-			.inapplicableMatchResults;
-	}
+    auto& inapplicable_match_reports() noexcept
+    {
+        return reporter()
+            .inapplicableMatchResults;
+    }
 
-	auto& full_match_reports() noexcept
-	{
-		return reporter()
-			.fullMatchResults;
-	}
+    auto& full_match_reports() noexcept
+    {
+        return reporter()
+            .fullMatchResults;
+    }
 
-	auto& errors() noexcept
-	{
-		return reporter()
-			.errors;
-	}
+    auto& errors() noexcept
+    {
+        return reporter()
+            .errors;
+    }
 
-	auto& unhandled_exceptions() noexcept
-	{
-		return reporter()
-			.unhandledExceptions;
-	}
+    auto& unhandled_exceptions() noexcept
+    {
+        return reporter()
+            .unhandledExceptions;
+    }
 
-	auto& unfulfilled_expectations() noexcept
-	{
-		return reporter()
-			.unfulfilledExpectations;
-	}
+    auto& unfulfilled_expectations() noexcept
+    {
+        return reporter()
+            .unfulfilledExpectations;
+    }
 
 private:
-	[[nodiscard]]
-	static const TestReporter& reporter()
-	{
-		return dynamic_cast<const TestReporter&>(
-			*mimicpp::detail::get_reporter());
-	}
+    [[nodiscard]]
+    static const TestReporter& reporter()
+    {
+        return dynamic_cast<const TestReporter&>(
+            *mimicpp::detail::get_reporter());
+    }
 };
--- test/unit-tests/TestTypes.hpp
+++ test/unit-tests/TestTypes.hpp
@@ -10,471 +10,470 @@
 #include "mimic++/Reports.hpp"
 
 #include "catch2/catch_test_macros.hpp"
-#include "catch2/trompeloeil.hpp"
 #include "catch2/matchers/catch_matchers_templated.hpp"
+#include "catch2/trompeloeil.hpp"
 
 #include <variant>
 
 class UnwrapReferenceWrapper
 {
 public:
-	template <typename T>
-	constexpr T& operator ()(const std::reference_wrapper<T> ref) const noexcept
-	{
-		return ref.get();
-	}
+    template <typename T>
+    constexpr T& operator()(const std::reference_wrapper<T> ref) const noexcept
+    {
+        return ref.get();
+    }
 };
 
 template <typename Signature>
 class PolicyFake
 {
 public:
-	using CallInfoT = mimicpp::call::info_for_signature_t<Signature>;
+    using CallInfoT = mimicpp::call::info_for_signature_t<Signature>;
 
-	bool isSatisfied{};
+    bool isSatisfied{};
 
-	[[nodiscard]]
-	constexpr bool is_satisfied() const noexcept
-	{
-		return isSatisfied;
-	}
+    [[nodiscard]]
+    constexpr bool is_satisfied() const noexcept
+    {
+        return isSatisfied;
+    }
 
-	bool matchResult{};
+    bool matchResult{};
 
-	[[nodiscard]]
-	constexpr bool matches([[maybe_unused]] const CallInfoT& call) const noexcept
-	{
-		return matchResult;
-	}
+    [[nodiscard]]
+    constexpr bool matches([[maybe_unused]] const CallInfoT& call) const noexcept
+    {
+        return matchResult;
+    }
 
-	mimicpp::StringT description{};
+    mimicpp::StringT description{};
 
-	[[nodiscard]]
-	mimicpp::StringT describe() const
-	{
-		return description;
-	}
+    [[nodiscard]]
+    mimicpp::StringT describe() const
+    {
+        return description;
+    }
 
-	static constexpr void consume([[maybe_unused]] const CallInfoT& call) noexcept
-	{
-	}
+    static constexpr void consume([[maybe_unused]] const CallInfoT& call) noexcept
+    {
+    }
 };
 
 template <typename Signature>
 class FinalizerMock
 {
 public:
-	using CallInfoT = mimicpp::call::info_for_signature_t<Signature>;
-	using ReturnT = mimicpp::signature_return_type_t<Signature>;
+    using CallInfoT = mimicpp::call::info_for_signature_t<Signature>;
+    using ReturnT = mimicpp::signature_return_type_t<Signature>;
 
-	MAKE_MOCK1(finalize_call, ReturnT (const CallInfoT&));
+    MAKE_MOCK1(finalize_call, ReturnT(const CallInfoT&));
 };
 
 template <typename Signature, typename Policy, typename Projection>
 // enable, when trompeloeil fully supports movable mocks
-//requires mimicpp::expectation_policy_for<
+// requires mimicpp::expectation_policy_for<
 //	std::remove_cvref_t<std::invoke_result_t<Projection, Policy>>,
 //	Signature>
 class PolicyFacade
 {
 public:
-	using CallT = mimicpp::call::info_for_signature_t<Signature>;
+    using CallT = mimicpp::call::info_for_signature_t<Signature>;
 
-	Policy policy;
-	Projection projection{};
+    Policy policy;
+    Projection projection{};
 
-	[[nodiscard]]
-	constexpr bool is_satisfied() const noexcept
-	{
-		return std::invoke(projection, policy)
-			.is_satisfied();
-	}
+    [[nodiscard]]
+    constexpr bool is_satisfied() const noexcept
+    {
+        return std::invoke(projection, policy)
+            .is_satisfied();
+    }
 
-	[[nodiscard]]
-	constexpr bool matches(const CallT& call) const noexcept
-	{
-		return std::invoke(projection, policy)
-			.matches(call);
-	}
+    [[nodiscard]]
+    constexpr bool matches(const CallT& call) const noexcept
+    {
+        return std::invoke(projection, policy)
+            .matches(call);
+    }
 
-	[[nodiscard]]
-	mimicpp::StringT describe() const
-	{
-		return std::invoke(projection, policy)
-			.describe();
-	}
+    [[nodiscard]]
+    mimicpp::StringT describe() const
+    {
+        return std::invoke(projection, policy)
+            .describe();
+    }
 
-	constexpr void consume(const CallT& call) noexcept
-	{
-		std::invoke(projection, policy)
-			.consume(call);
-	}
+    constexpr void consume(const CallT& call) noexcept
+    {
+        std::invoke(projection, policy)
+            .consume(call);
+    }
 };
 
 template <typename Signature>
 class FinalizerFake
 {
 public:
-	using CallInfoT = mimicpp::call::info_for_signature_t<Signature>;
-	using ReturnT = mimicpp::signature_return_type_t<Signature>;
+    using CallInfoT = mimicpp::call::info_for_signature_t<Signature>;
+    using ReturnT = mimicpp::signature_return_type_t<Signature>;
 
-	class Exception
-	{
-	};
+    class Exception
+    {
+    };
 
-	static ReturnT finalize_call([[maybe_unused]] const CallInfoT& call)
-	{
-		throw Exception{};
-	}
+    static ReturnT finalize_call([[maybe_unused]] const CallInfoT& call)
+    {
+        throw Exception{};
+    }
 };
 
 template <typename Signature>
 class PolicyMock
 {
 public:
-	using CallInfoT = mimicpp::call::info_for_signature_t<Signature>;
+    using CallInfoT = mimicpp::call::info_for_signature_t<Signature>;
 
-	static constexpr bool trompeloeil_movable_mock = true;
+    static constexpr bool trompeloeil_movable_mock = true;
 
-	MAKE_CONST_MOCK0(is_satisfied, bool (), noexcept);
-	MAKE_CONST_MOCK1(matches, bool(const CallInfoT&), noexcept);
-	MAKE_CONST_MOCK0(describe, mimicpp::StringT());
-	MAKE_MOCK1(consume, void (const CallInfoT&), noexcept);
+    MAKE_CONST_MOCK0(is_satisfied, bool(), noexcept);
+    MAKE_CONST_MOCK1(matches, bool(const CallInfoT&), noexcept);
+    MAKE_CONST_MOCK0(describe, mimicpp::StringT());
+    MAKE_MOCK1(consume, void(const CallInfoT&), noexcept);
 };
 
 template <typename Signature, typename Policy, typename Projection>
 // enable, when trompeloeil fully supports movable mocks
-//requires mimicpp::finalize_policy_for<
+// requires mimicpp::finalize_policy_for<
 //	std::remove_cvref_t<std::invoke_result_t<Projection, Policy>>,
 //	Signature>
 class FinalizerFacade
 {
 public:
-	using CallInfoT = mimicpp::call::info_for_signature_t<Signature>;
-	using ReturnT = mimicpp::signature_return_type_t<Signature>;
+    using CallInfoT = mimicpp::call::info_for_signature_t<Signature>;
+    using ReturnT = mimicpp::signature_return_type_t<Signature>;
 
-	Policy policy{};
-	Projection projection{};
+    Policy policy{};
+    Projection projection{};
 
-	[[nodiscard]]
-	constexpr ReturnT finalize_call(const CallInfoT& call)
-	{
-		return std::invoke(projection, policy)
-			.finalize_call(call);
-	}
+    [[nodiscard]]
+    constexpr ReturnT finalize_call(const CallInfoT& call)
+    {
+        return std::invoke(projection, policy)
+            .finalize_call(call);
+    }
 };
 
 class ControlPolicyFake
 {
 public:
-	bool isSatisfied{};
+    bool isSatisfied{};
 
-	[[nodiscard]]
-	constexpr bool is_satisfied() const noexcept
-	{
-		return isSatisfied;
-	}
+    [[nodiscard]]
+    constexpr bool is_satisfied() const noexcept
+    {
+        return isSatisfied;
+    }
 
-	mimicpp::control_state_t stateData{};
+    mimicpp::control_state_t stateData{};
 
-	[[nodiscard]]
-	mimicpp::control_state_t state() const
-	{
-		return stateData;
-	}
+    [[nodiscard]]
+    mimicpp::control_state_t state() const
+    {
+        return stateData;
+    }
 
-	static constexpr void consume() noexcept
-	{
-	}
+    static constexpr void consume() noexcept
+    {
+    }
 };
 
 class ControlPolicyMock
 {
 public:
-	MAKE_CONST_MOCK0(is_satisfied, bool(), noexcept);
-	MAKE_CONST_MOCK0(describe_state, std::optional<mimicpp::StringT>());
-	MAKE_CONST_MOCK0(state, mimicpp::control_state_t());
-	MAKE_MOCK0(consume, void());
+    MAKE_CONST_MOCK0(is_satisfied, bool(), noexcept);
+    MAKE_CONST_MOCK0(describe_state, std::optional<mimicpp::StringT>());
+    MAKE_CONST_MOCK0(state, mimicpp::control_state_t());
+    MAKE_MOCK0(consume, void());
 };
 
 template <typename Policy, typename Projection>
 class ControlPolicyFacade
 {
 public:
-	Policy policy{};
-	Projection projection{};
+    Policy policy{};
+    Projection projection{};
 
-	[[nodiscard]]
-	constexpr bool is_satisfied() const noexcept
-	{
-		return std::invoke(projection, policy)
-			.is_satisfied();
-	}
+    [[nodiscard]]
+    constexpr bool is_satisfied() const noexcept
+    {
+        return std::invoke(projection, policy)
+            .is_satisfied();
+    }
 
-	[[nodiscard]]
-	mimicpp::control_state_t state() const
-	{
-		return std::invoke(projection, policy)
-			.state();
-	}
+    [[nodiscard]]
+    mimicpp::control_state_t state() const
+    {
+        return std::invoke(projection, policy)
+            .state();
+    }
 
-	constexpr void consume() noexcept
-	{
-		return std::invoke(projection, policy)
-			.consume();
-	}
+    constexpr void consume() noexcept
+    {
+        return std::invoke(projection, policy)
+            .consume();
+    }
 };
 
 template <typename T>
 class MatcherMock
 {
 public:
-	MAKE_CONST_MOCK1(matches, bool(T));
-	MAKE_CONST_MOCK0(describe, mimicpp::StringT());
+    MAKE_CONST_MOCK1(matches, bool(T));
+    MAKE_CONST_MOCK0(describe, mimicpp::StringT());
 };
 
 template <typename Matcher, typename Projection>
 class [[maybe_unused]] MatcherFacade
 {
 public:
-	[[nodiscard]]
-	MatcherFacade(Matcher matcher, Projection projection)
-		: m_Matcher{std::move(matcher)},
-		m_Projection{std::move(projection)}
-	{
-	}
+    [[nodiscard]]
+    MatcherFacade(Matcher matcher, Projection projection)
+        : m_Matcher{std::move(matcher)},
+          m_Projection{std::move(projection)}
+    {
+    }
 
-	template <typename T>
-	[[nodiscard]]
-	constexpr bool matches(T&& target) const
-	{
-		return std::invoke(m_Projection, m_Matcher)
-			.matches(std::forward<T>(target));
-	}
+    template <typename T>
+    [[nodiscard]]
+    constexpr bool matches(T&& target) const
+    {
+        return std::invoke(m_Projection, m_Matcher)
+            .matches(std::forward<T>(target));
+    }
 
-	[[nodiscard]]
-	constexpr mimicpp::StringT describe() const
-	{
-		return std::invoke(m_Projection, m_Matcher)
-			.describe();
-	}
+    [[nodiscard]]
+    constexpr mimicpp::StringT describe() const
+    {
+        return std::invoke(m_Projection, m_Matcher)
+            .describe();
+    }
 
 private:
-	Matcher m_Matcher;
-	Projection m_Projection;
+    Matcher m_Matcher;
+    Projection m_Projection;
 };
 
 template <typename Return, typename... Params>
 class InvocableMockBase;
 
 template <typename Return, typename... Params>
-	requires (0u == sizeof...(Params))
+    requires(0u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK0(Invoke, Return(Params...));
-	MAKE_CONST_MOCK0(Invoke, Return(Params...));
+    MAKE_MOCK0(Invoke, Return(Params...));
+    MAKE_CONST_MOCK0(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (1u == sizeof...(Params))
+    requires(1u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK1(Invoke, Return(Params...));
-	MAKE_CONST_MOCK1(Invoke, Return(Params...));
+    MAKE_MOCK1(Invoke, Return(Params...));
+    MAKE_CONST_MOCK1(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (2u == sizeof...(Params))
+    requires(2u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK2(Invoke, Return(Params...));
-	MAKE_CONST_MOCK2(Invoke, Return(Params...));
+    MAKE_MOCK2(Invoke, Return(Params...));
+    MAKE_CONST_MOCK2(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (3u == sizeof...(Params))
+    requires(3u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK3(Invoke, Return(Params...));
-	MAKE_CONST_MOCK3(Invoke, Return(Params...));
+    MAKE_MOCK3(Invoke, Return(Params...));
+    MAKE_CONST_MOCK3(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (4u == sizeof...(Params))
+    requires(4u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK4(Invoke, Return(Params...));
-	MAKE_CONST_MOCK4(Invoke, Return(Params...));
+    MAKE_MOCK4(Invoke, Return(Params...));
+    MAKE_CONST_MOCK4(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (5u == sizeof...(Params))
+    requires(5u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK5(Invoke, Return(Params...));
-	MAKE_CONST_MOCK5(Invoke, Return(Params...));
+    MAKE_MOCK5(Invoke, Return(Params...));
+    MAKE_CONST_MOCK5(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (6u == sizeof...(Params))
+    requires(6u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK6(Invoke, Return(Params...));
-	MAKE_CONST_MOCK6(Invoke, Return(Params...));
+    MAKE_MOCK6(Invoke, Return(Params...));
+    MAKE_CONST_MOCK6(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (7u == sizeof...(Params))
+    requires(7u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK7(Invoke, Return(Params...));
-	MAKE_CONST_MOCK7(Invoke, Return(Params...));
+    MAKE_MOCK7(Invoke, Return(Params...));
+    MAKE_CONST_MOCK7(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (8u == sizeof...(Params))
+    requires(8u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK8(Invoke, Return(Params...));
-	MAKE_CONST_MOCK8(Invoke, Return(Params...));
+    MAKE_MOCK8(Invoke, Return(Params...));
+    MAKE_CONST_MOCK8(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (9u == sizeof...(Params))
+    requires(9u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK9(Invoke, Return(Params...));
-	MAKE_CONST_MOCK9(Invoke, Return(Params...));
+    MAKE_MOCK9(Invoke, Return(Params...));
+    MAKE_CONST_MOCK9(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (10u == sizeof...(Params))
+    requires(10u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK10(Invoke, Return(Params...));
-	MAKE_CONST_MOCK10(Invoke, Return(Params...));
+    MAKE_MOCK10(Invoke, Return(Params...));
+    MAKE_CONST_MOCK10(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (11u == sizeof...(Params))
+    requires(11u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK11(Invoke, Return(Params...));
-	MAKE_CONST_MOCK11(Invoke, Return(Params...));
+    MAKE_MOCK11(Invoke, Return(Params...));
+    MAKE_CONST_MOCK11(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (12u == sizeof...(Params))
+    requires(12u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK12(Invoke, Return(Params...));
-	MAKE_CONST_MOCK12(Invoke, Return(Params...));
+    MAKE_MOCK12(Invoke, Return(Params...));
+    MAKE_CONST_MOCK12(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (13u == sizeof...(Params))
+    requires(13u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK13(Invoke, Return(Params...));
-	MAKE_CONST_MOCK13(Invoke, Return(Params...));
+    MAKE_MOCK13(Invoke, Return(Params...));
+    MAKE_CONST_MOCK13(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (14u == sizeof...(Params))
+    requires(14u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK14(Invoke, Return(Params...));
-	MAKE_CONST_MOCK14(Invoke, Return(Params...));
+    MAKE_MOCK14(Invoke, Return(Params...));
+    MAKE_CONST_MOCK14(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
-	requires (15u == sizeof...(Params))
+    requires(15u == sizeof...(Params))
 class InvocableMockBase<Return, Params...>
 {
 public:
-	MAKE_MOCK15(Invoke, Return(Params...));
-	MAKE_CONST_MOCK15(Invoke, Return(Params...));
+    MAKE_MOCK15(Invoke, Return(Params...));
+    MAKE_CONST_MOCK15(Invoke, Return(Params...));
 };
 
 template <typename Return, typename... Params>
 class InvocableMock
-	: public InvocableMockBase<Return, Params...>
+    : public InvocableMockBase<Return, Params...>
 {
 public:
-	using ReturnT = Return;
-	using ParamListT = std::tuple<Params...>;
-	using InvocableMockBase<Return, Params...>::Invoke;
+    using ReturnT = Return;
+    using ParamListT = std::tuple<Params...>;
+    using InvocableMockBase<Return, Params...>::Invoke;
 
-	constexpr ReturnT operator ()(Params... params)
-	{
-		return Invoke(std::forward<Params>(params)...);
-	}
+    constexpr ReturnT operator()(Params... params)
+    {
+        return Invoke(std::forward<Params>(params)...);
+    }
 
-	constexpr ReturnT operator ()(Params... params) const
-	{
-		return Invoke(std::forward<Params>(params)...);
-	}
+    constexpr ReturnT operator()(Params... params) const
+    {
+        return Invoke(std::forward<Params>(params)...);
+    }
 };
 
 template <std::equality_comparable Value>
 class VariantEqualsMatcher final
-	: public Catch::Matchers::MatcherGenericBase
+    : public Catch::Matchers::MatcherGenericBase
 {
 public:
-	[[nodiscard]]
-	explicit constexpr VariantEqualsMatcher(Value value)
-		: m_Value{std::move(value)}
-	{
-	}
+    [[nodiscard]]
+    explicit constexpr VariantEqualsMatcher(Value value)
+        : m_Value{std::move(value)}
+    {
+    }
 
-	template <typename... Alternatives>
-	[[nodiscard]]
-	constexpr bool match(const std::variant<Alternatives...>& other) const
-		requires requires { { std::holds_alternative<Value>(other) } -> std::convertible_to<bool>; }
-	{
-		return std::holds_alternative<Value>(other)
-				&& m_Value == std::get<Value>(other);
-	}
+    template <typename... Alternatives>
+    [[nodiscard]]
+    constexpr bool match(const std::variant<Alternatives...>& other) const
+        requires requires { { std::holds_alternative<Value>(other) } -> std::convertible_to<bool>; }
+    {
+        return std::holds_alternative<Value>(other)
+            && m_Value == std::get<Value>(other);
+    }
 
-	std::string describe() const override
-	{
-		return "Variant state equals: " + Catch::Detail::stringify(m_Value);
-	}
+    std::string describe() const override
+    {
+        return "Variant state equals: " + Catch::Detail::stringify(m_Value);
+    }
 
 private:
-	Value m_Value;
+    Value m_Value;
 };
 
 template <typename Value>
 [[nodiscard]]
 constexpr auto variant_equals(Value&& value)
 {
-	return VariantEqualsMatcher<std::remove_cvref_t<Value>>{
-		std::forward<Value>(value)
-	};
+    return VariantEqualsMatcher<std::remove_cvref_t<Value>>{
+        std::forward<Value>(value)};
 }
 
 class FakeSequenceStrategy
 {
 public:
-	[[nodiscard, maybe_unused]]
-	constexpr int operator ()(const auto id, [[maybe_unused]] const int cursor) const noexcept
-	{
-		return static_cast<int>(id);
-	}
+    [[nodiscard, maybe_unused]]
+    constexpr int operator()(const auto id, [[maybe_unused]] const int cursor) const noexcept
+    {
+        return static_cast<int>(id);
+    }
 };
