c87e754289194bce57555a4e49af8426430de030
DNKpp <dnkpp2011@gmail.com>
2024-12-11 17:09:42 +0100

    chore: update formatting for Doctest-Adapter related files

--- include/mimic++/adapters/Doctest.hpp
+++ include/mimic++/adapters/Doctest.hpp
@@ -11,53 +11,52 @@
 #include "mimic++/Reporter.hpp"
 
 #if __has_include(<doctest/doctest.h>)
-#include <doctest/doctest.h>
+    #include <doctest/doctest.h>
 #else
-	#error "Unable to find Doctest includes."
+    #error "Unable to find Doctest includes."
 #endif
 
 namespace mimicpp::detail::doctest
 {
-	using namespace ::doctest;
+    using namespace ::doctest;
 
-	[[noreturn]]
-	inline void send_fail(const StringViewT msg)
-	{
-		DOCTEST_FAIL(msg);
-		unreachable();
-	}
+    [[noreturn]]
+    inline void send_fail(const StringViewT msg)
+    {
+        DOCTEST_FAIL(msg);
+        unreachable();
+    }
 
-	inline void send_success(const StringViewT msg)
-	{
-		DOCTEST_REQUIRE_MESSAGE(true, msg);
-	}
+    inline void send_success(const StringViewT msg)
+    {
+        DOCTEST_REQUIRE_MESSAGE(true, msg);
+    }
 
-	inline void send_warning(const StringViewT msg)
-	{
-		DOCTEST_MESSAGE(msg);
-	}
+    inline void send_warning(const StringViewT msg)
+    {
+        DOCTEST_MESSAGE(msg);
+    }
 }
 
 namespace mimicpp
 {
-	/**
-	 * \brief Reporter for the integration into Doctest.
-	 * \ingroup REPORTING_ADAPTERS
-	 * \details This reporter enables the integration of ``mimic++`` into ``Doctest`` and prefixes the headers
-	 * of ``Doctest`` with ``doctest/``.
-	 *
-	 * This reporter installs itself by simply including this header file into any source file of the test executable.
-	 */
-	using DoctestReporterT = BasicReporter<
-		&detail::doctest::send_success,
-		&detail::doctest::send_warning,
-		&detail::doctest::send_fail
-	>;
+    /**
+     * \brief Reporter for the integration into Doctest.
+     * \ingroup REPORTING_ADAPTERS
+     * \details This reporter enables the integration of ``mimic++`` into ``Doctest`` and prefixes the headers
+     * of ``Doctest`` with ``doctest/``.
+     *
+     * This reporter installs itself by simply including this header file into any source file of the test executable.
+     */
+    using DoctestReporterT = BasicReporter<
+        &detail::doctest::send_success,
+        &detail::doctest::send_warning,
+        &detail::doctest::send_fail>;
 }
 
 namespace mimicpp::detail::doctest
 {
-	inline const ReporterInstaller<DoctestReporterT> installer{};
+    inline const ReporterInstaller<DoctestReporterT> installer{};
 }
 
 #endif
--- test/adapter-tests/doctest/main.cpp
+++ test/adapter-tests/doctest/main.cpp
@@ -8,99 +8,99 @@
 
 namespace
 {
-	inline std::atomic_int g_SuccessCounter{0};
-	inline std::atomic_int g_WarningCounter{0};
+    inline std::atomic_int g_SuccessCounter{0};
+    inline std::atomic_int g_WarningCounter{0};
 
-	class SuccessListener final
-		: public doctest::IReporter
-	{
-	public:
-		explicit SuccessListener([[maybe_unused]] const doctest::ContextOptions& in)
-		{
-		}
+    class SuccessListener final
+        : public doctest::IReporter
+    {
+    public:
+        explicit SuccessListener([[maybe_unused]] const doctest::ContextOptions& in)
+        {
+        }
 
-		void report_query([[maybe_unused]] const doctest::QueryData& in) override
-		{
-		}
+        void report_query([[maybe_unused]] const doctest::QueryData& in) override
+        {
+        }
 
-		void test_run_start() override
-		{
-		}
+        void test_run_start() override
+        {
+        }
 
-		void test_run_end([[maybe_unused]] const doctest::TestRunStats& in) override
-		{
-		}
+        void test_run_end([[maybe_unused]] const doctest::TestRunStats& in) override
+        {
+        }
 
-		void test_case_start([[maybe_unused]] const doctest::TestCaseData& in) override
-		{
-		}
+        void test_case_start([[maybe_unused]] const doctest::TestCaseData& in) override
+        {
+        }
 
-		void test_case_reenter([[maybe_unused]] const doctest::TestCaseData& in) override
-		{
-		}
+        void test_case_reenter([[maybe_unused]] const doctest::TestCaseData& in) override
+        {
+        }
 
-		void test_case_end([[maybe_unused]] const doctest::CurrentTestCaseStats& in) override
-		{
-		}
+        void test_case_end([[maybe_unused]] const doctest::CurrentTestCaseStats& in) override
+        {
+        }
 
-		void test_case_exception([[maybe_unused]] const doctest::TestCaseException& in) override
-		{
-		}
+        void test_case_exception([[maybe_unused]] const doctest::TestCaseException& in) override
+        {
+        }
 
-		void subcase_start([[maybe_unused]] const doctest::SubcaseSignature& in) override
-		{
-		}
+        void subcase_start([[maybe_unused]] const doctest::SubcaseSignature& in) override
+        {
+        }
 
-		void subcase_end() override
-		{
-		}
+        void subcase_end() override
+        {
+        }
 
-		void log_assert(const doctest::AssertData& in) override
-		{
-			if (!in.m_failed)
-			{
-				++g_SuccessCounter;
-			}
-		}
+        void log_assert(const doctest::AssertData& in) override
+        {
+            if (!in.m_failed)
+            {
+                ++g_SuccessCounter;
+            }
+        }
 
-		void log_message(const doctest::MessageData& in) override
-		{
-			if (in.m_string == "Warning")
-			{
-				++g_WarningCounter;
-			}
-		}
+        void log_message(const doctest::MessageData& in) override
+        {
+            if (in.m_string == "Warning")
+            {
+                ++g_WarningCounter;
+            }
+        }
 
-		void test_case_skipped([[maybe_unused]] const doctest::TestCaseData& in) override
-		{
-		}
-	};
+        void test_case_skipped([[maybe_unused]] const doctest::TestCaseData& in) override
+        {
+        }
+    };
 }
 
 REGISTER_LISTENER("SuccessListener", 1, SuccessListener);
 
 TEST_SUITE("adapter::doctest")
 {
-	TEST_CASE("doctest::send_success notifies Doctest for success.")
-	{
-		g_SuccessCounter = 0;
+    TEST_CASE("doctest::send_success notifies Doctest for success.")
+    {
+        g_SuccessCounter = 0;
 
-		mimicpp::detail::doctest::send_success("Success");
+        mimicpp::detail::doctest::send_success("Success");
 
-		REQUIRE(g_SuccessCounter == 1);
-	}
+        REQUIRE(g_SuccessCounter == 1);
+    }
 
-	TEST_CASE("doctest::send_warning notifies Doctest.")
-	{
-		mimicpp::detail::doctest::send_warning("Warning");
+    TEST_CASE("doctest::send_warning notifies Doctest.")
+    {
+        mimicpp::detail::doctest::send_warning("Warning");
 
-		REQUIRE(g_WarningCounter == 1);
-	}
+        REQUIRE(g_WarningCounter == 1);
+    }
 
-	TEST_CASE(
-		"doctest::send_fail notifies Doctest for failures and aborts."
-		* doctest::should_fail{})
-	{
-		mimicpp::detail::doctest::send_fail("Fail");
-	}
+    TEST_CASE(
+        "doctest::send_fail notifies Doctest for failures and aborts."
+        * doctest::should_fail{})
+    {
+        mimicpp::detail::doctest::send_fail("Fail");
+    }
 }
