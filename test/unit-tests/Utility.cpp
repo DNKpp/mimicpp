b4e7685f0e0c73c6d6d07d692fb92d736e91d30a
DNKpp <dnkpp2011@gmail.com>
2024-12-18 11:25:27 +0100

    test: extend detail::expand_tuple tests

--- test/unit-tests/Utility.cpp
+++ test/unit-tests/Utility.cpp
@@ -104,25 +104,24 @@ TEST_CASE(
 {
     SECTION("When tuple is empty")
     {
+        std::tuple input{};
+
         SECTION("And n == 0")
         {
-            const std::tuple result = detail::expand_tuple<int, 0>(
-                std::tuple{});
+            const std::tuple result = detail::expand_tuple<int, 0>(std::move(input));
             STATIC_REQUIRE(std::same_as<const std::tuple<>, decltype(result)>);
         }
 
         SECTION("And n == 1")
         {
-            const std::tuple result = detail::expand_tuple<int, 1>(
-                std::tuple{});
+            const std::tuple result = detail::expand_tuple<int, 1>(std::move(input));
             STATIC_REQUIRE(std::same_as<const std::tuple<int>, decltype(result)>);
             REQUIRE(std::tuple{0} == result);
         }
 
         SECTION("And n == 2")
         {
-            const std::tuple result = detail::expand_tuple<int, 2>(
-                std::tuple{});
+            const std::tuple result = detail::expand_tuple<int, 2>(std::move(input));
             STATIC_REQUIRE(std::same_as<const std::tuple<int, int>, decltype(result)>);
             REQUIRE(std::tuple{0, 0} == result);
         }
@@ -130,25 +129,24 @@ TEST_CASE(
 
     SECTION("When tuple has size 1")
     {
+        std::tuple input{42.};
+
         SECTION("And n == 1")
         {
-            const std::tuple result = detail::expand_tuple<int, 1>(
-                std::tuple{42.});
+            const std::tuple result = detail::expand_tuple<int, 1>(std::move(input));
             STATIC_REQUIRE(std::same_as<const std::tuple<double>, decltype(result)>);
         }
 
         SECTION("And n == 2")
         {
-            const std::tuple result = detail::expand_tuple<int, 2>(
-                std::tuple{42.});
+            const std::tuple result = detail::expand_tuple<int, 2>(std::move(input));
             STATIC_REQUIRE(std::same_as<const std::tuple<double, int>, decltype(result)>);
             REQUIRE(std::tuple{42., 0} == result);
         }
 
         SECTION("And n == 3")
         {
-            constexpr std::tuple result = detail::expand_tuple<int, 3>(
-                std::tuple{42.});
+            const std::tuple result = detail::expand_tuple<int, 3>(std::move(input));
             STATIC_REQUIRE(std::same_as<const std::tuple<double, int, int>, decltype(result)>);
             REQUIRE(std::tuple{42., 0, 0} == result);
         }
@@ -156,28 +154,67 @@ TEST_CASE(
 
     SECTION("When tuple has size 2")
     {
+        std::tuple input{1337., 42.f};
+
         SECTION("And n == 2")
         {
-            const std::tuple result = detail::expand_tuple<int, 2>(
-                std::tuple{1337., 42.f});
+            const std::tuple result = detail::expand_tuple<int, 2>(std::move(input));
             STATIC_REQUIRE(std::same_as<const std::tuple<double, float>, decltype(result)>);
             REQUIRE(std::tuple{1337., 42.f} == result);
         }
 
         SECTION("And n == 3")
         {
-            const std::tuple result = detail::expand_tuple<int, 3>(
-                std::tuple{1337., 42.f});
+            const std::tuple result = detail::expand_tuple<int, 3>(std::move(input));
             STATIC_REQUIRE(std::same_as<const std::tuple<double, float, int>, decltype(result)>);
             REQUIRE(std::tuple{1337., 42.f, 0} == result);
         }
 
         SECTION("And n == 4")
         {
-            const std::tuple result = detail::expand_tuple<int, 4>(
-                std::tuple{1337., 42.f});
+            const std::tuple result = detail::expand_tuple<int, 4>(std::move(input));
             STATIC_REQUIRE(std::same_as<const std::tuple<double, float, int, int>, decltype(result)>);
             REQUIRE(std::tuple{1337., 42.f, 0, 0} == result);
+        }
+    }
+
+    SECTION("It keeps the existing elemens as they are.")
+    {
+        std::string arg{"Hello, World"};
+
+        SECTION("When argument is const.")
+        {
+            const std::tuple result = detail::expand_tuple<int, 1>(std::tuple<const std::string>(arg));
+            STATIC_REQUIRE(std::same_as<const std::tuple<const std::string>, decltype(result)>);
+            REQUIRE(arg == std::get<0>(result));
+        }
+
+        SECTION("When argument is lvalue-ref.")
+        {
+            const std::tuple result = detail::expand_tuple<int, 1>(std::tuple<std::string&>(arg));
+            STATIC_REQUIRE(std::same_as<const std::tuple<std::string&>, decltype(result)>);
+            REQUIRE(&arg == &std::get<0>(result));
+        }
+
+        SECTION("When argument is const lvalue-ref.")
+        {
+            const std::tuple result = detail::expand_tuple<int, 1>(std::tuple<const std::string&>(arg));
+            STATIC_REQUIRE(std::same_as<const std::tuple<const std::string&>, decltype(result)>);
+            REQUIRE(&arg == &std::get<0>(result));
+        }
+
+        SECTION("When argument is rvalue-ref.")
+        {
+            const std::tuple result = detail::expand_tuple<int, 1>(std::tuple<std::string&&>(std::move(arg)));
+            STATIC_REQUIRE(std::same_as<const std::tuple<std::string&&>, decltype(result)>);
+            REQUIRE(&arg == &std::get<0>(result));
+        }
+
+        SECTION("When argument is const rvalue-ref.")
+        {
+            const std::tuple result = detail::expand_tuple<int, 1>(std::tuple<const std::string&&>(std::move(arg)));
+            STATIC_REQUIRE(std::same_as<const std::tuple<const std::string&&>, decltype(result)>);
+            REQUIRE(&arg == &std::get<0>(result));
         }
     }
 }
