3e6533b8d74640d40b4ab3057e818287e2faa5af
DNKpp <dnkpp2011@gmail.com>
2024-12-19 18:57:50 +0100

    feat: add basic cpptrace::Backend

--- test/unit-tests/Stacktrace.cpp
+++ test/unit-tests/Stacktrace.cpp
@@ -9,6 +9,26 @@
 
 using namespace mimicpp;
 
+#ifdef MIMICPP_CONFIG_USE_CPPTRACE
+
+TEST_CASE(
+    "stacktrace_traits<BackendT>::current() generates a new cpptrace::stacktrace.",
+    "[cpptrace][stacktrace]")
+{
+    using BackendT = mimicpp::cpptrace::Backend;
+    using traits_t = stacktrace_traits<BackendT>;
+
+    const BackendT first = traits_t::current(0);
+    const BackendT second = traits_t::current(0);
+
+    REQUIRE_THAT(
+        first.data().frames | std::views::drop(1),
+        Catch::Matchers::RangeEquals(second.data().frames | std::views::drop(1)));
+    REQUIRE(first.data().frames.front() != second.data().frames.front());
+}
+
+#endif
+
 #ifdef MIMICPP_CONFIG_EXPERIMENTAL_STACKTRACE
 
 TEST_CASE(
