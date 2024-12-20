6645a9fcb436b46c53f262f24d770c55dd4da427
DNKpp <dnkpp2011@gmail.com>
2024-12-20 15:05:05 +0100

    chore: add Findtrompeloeil.cmake

---
+++ cmake/Findtrompeloeil.cmake
@@ -0,0 +1,8 @@
+#          Copyright Dominic (DNKpp) Koepke 2024 - 2024.
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          https://www.boost.org/LICENSE_1_0.txt)
+
+include(get_cpm)
+
+CPMAddPackage("gh:rollbear/trompeloeil@49")
