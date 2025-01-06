c1b4a31d741dc5caf723f2fc2d7d1c409930beb6
DNKpp <dnkpp2011@gmail.com>
2025-01-06 15:13:19 +0100

    feat: matches::approx_rel

--- include/mimic++/Matcher.hpp
+++ include/mimic++/Matcher.hpp
@@ -18,6 +18,7 @@
 #include <cmath>
 #include <concepts>
 #include <functional>
+#include <limits>
 #include <ranges>
 #include <tuple>
 #include <type_traits>
@@ -474,11 +475,11 @@ namespace mimicpp::matches
     }
 
     /**
-     * \brief Tests, whether the floating-point target is approximately equal to value.
+     * \brief Tests, whether the floating-point target is approximately equal to ``value``.
      * \param value The value to compare to.
      * \param epsilon The maximum absolute difference.
      * \throws std::runtime_error When ``value`` is ``NaN`` or ``infinity``.
-     * \throws std::runtime_error When ``epsilon`` is ``NaN``, ``infinity`` or not positive.
+     * \throws std::runtime_error When ``epsilon`` is ``NaN``, ``infinity`` or non-positive.
      * \return The newly created matcher.
      */
     [[nodiscard]]
@@ -497,6 +498,70 @@ namespace mimicpp::matches
             "is not approximately {} +- {}",
             std::tuple{value, epsilon}
         };
+    }
+
+    /**
+     * \brief Tests, whether the floating-point target is approximately equal to ``value``.
+     * \param value The value to compare against.
+     * \param relEpsilon The maximum relative difference.
+     * \return The newly created matcher.
+     * \throws std::runtime_error When ``value`` is ``NaN`` or ``infinity``.
+     * \throws std::runtime_error When ``relEpsilon`` is ``NaN``, ``infinity`` or non-positive.
+     *
+     * \details This functions compares both floating-point values with a scaled epsilon.
+     * In fact:
+     *
+     * ``|a-b| <= eps * max(|a|, |b|)``,
+     *
+     * where ``a`` and ``b`` are the operands and ``eps`` denotes the factor of the maximum input by which
+     * ``a`` and ``b`` may differ.
+     * \note This algorithm was published by [Donald Knuth](https://en.wikipedia.org/wiki/Donald_Knuth) in his book
+     * “The Art of Computer Programming, Volume II: Seminumerical Algorithms (Addison-Wesley, 1969)”.
+     */
+    [[nodiscard]]
+    constexpr auto approx_rel(
+        const std::floating_point auto value,
+        const std::floating_point auto relEpsilon)
+    {
+        detail::check_fp_value(value);
+        detail::check_fp_epsilon(relEpsilon);
+
+        return PredicateMatcher{
+            [](const std::floating_point auto target, const auto val, const auto rel) {
+                // a target equal to +-infinity would result in an inf epsilon, which is not very useful
+                if (std::isinf(target))
+                {
+                    return false;
+                }
+
+                const auto absDiff = std::abs(target - val);
+                const auto scaledEpsilon = rel * std::max(std::abs(target), std::abs(val));
+                return absDiff <= scaledEpsilon; },
+            "is approximately {} +- ({} * max(|lhs|, |rhs|))",
+            "is not approximately {} +- ({} * max(|lhs|, |rhs|))",
+            std::tuple{value, relEpsilon}
+        };
+    }
+
+    /**
+     * \brief Tests, whether the floating-point target is approximately equal to ``value``.
+     * \param value The value to compare against.
+     * \return The newly created matcher.
+     * \throws std::runtime_error When ``value`` is ``NaN`` or ``infinity``.
+     *
+     * \details This overload sets ``100 * std::numeric_limits<Float>::epsilon()`` as the relative epsilon value.
+     * This seems like a reasonable choice, which is also used by the ``catch2``'s ``WithinRel`` matcher.
+     * \see https://github.com/catchorg/Catch2/blob/devel/docs/comparing-floating-point-numbers.md#withinrel
+     * \details For detailed information about the underlying algorithm, have a look at the primary overload.
+     * \see approx_rel(std::floating_point auto, std::floating_point auto)
+     */
+    template <std::floating_point Float>
+    [[nodiscard]]
+    constexpr auto approx_rel(const Float value)
+    {
+        return matches::approx_rel(
+            value,
+            std::numeric_limits<Float>::epsilon() * Float{100});
     }
 
     /**
--- test/unit-tests/matchers/FloatingPointMatchers.cpp
+++ test/unit-tests/matchers/FloatingPointMatchers.cpp
@@ -151,3 +151,108 @@ TEMPLATE_TEST_CASE(
         matches::approx_abs(T{4.2}, epsilon),
         std::runtime_error);
 }
+
+TEMPLATE_TEST_CASE(
+    "matches::approx_rel determines a match when the floating-point value is within a scaled epsilon range to the"
+    " expected value",
+    "[matcher]",
+    float,
+    double,
+    long double)
+{
+    using T = TestType;
+    const auto [expected, value, target, relEpsilon] = GENERATE(
+        (table<bool, T, T, T>({
+            { true,                                                        T{0.},                                   T{0.}, T{1.e-7}},
+            { true,                                                        T{1.},                                   T{1.}, T{1.e-7}},
+            // very close to 1., but with rounding errors
+            { true, T{0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1},                                   T{1.}, T{1.e-7}},
+            { true,                                                        T{1.},                                   T{0.},    T{1.}},
+            { true,                                                       T{-1.},                                   T{0.},    T{1.}},
+            { true,                                                       T{1.1},                                   T{1.},  T{0.15}},
+            { true,                                                      T{-1.1},                                  T{-1.},  T{0.15}},
+            {false,                                                      T{-1.1},                                   T{1.},  T{0.15}},
+            {false,                                                       T{1.1},                                  T{-1.},  T{0.15}},
+            {false,                                                       T{1.1},                                  T{-1.},  T{0.15}},
+            {false,                                                       T{4.2},      std::numeric_limits<T>::infinity(),    T{.1}},
+            {false,                                                       T{4.2},     -std::numeric_limits<T>::infinity(),    T{.1}},
+            {false,                                                       T{4.2},     std::numeric_limits<T>::quiet_NaN(), T{1337.}},
+            {false,                                                       T{4.2}, std::numeric_limits<T>::signaling_NaN(), T{1337.}},
+    })));
+    INFO("Expected value: " << value << " relative-epsilon: " << relEpsilon << " target: " << target);
+
+    SECTION("When plain matcher is used.")
+    {
+        const auto matcher = matches::approx_rel(value, relEpsilon);
+
+        REQUIRE_THAT(
+            matcher.describe(),
+            Catch::Matchers::Matches(R"(is approximately -?\d+(.\d+)? \+- \(\d+(e-\d+|.\d+)? \* max\(\|lhs\|, \|rhs\|\)\))"));
+
+        REQUIRE(expected == matcher.matches(target));
+    }
+
+    SECTION("When negated matcher is used.")
+    {
+        const auto matcher = !matches::approx_rel(value, relEpsilon);
+
+        REQUIRE_THAT(
+            matcher.describe(),
+            Catch::Matchers::Matches(R"(is not approximately -?\d+(.\d+)? \+- \(\d+(e-\d+|.\d+)? \* max\(\|lhs\|, \|rhs\|\)\))"));
+
+        REQUIRE(expected != matcher.matches(target));
+    }
+}
+
+TEMPLATE_TEST_CASE(
+    "matches::approx_rel throws std::runtime_error, when invalid value is given.",
+    "[matcher]",
+    float,
+    double,
+    long double)
+{
+    using T = TestType;
+
+    const auto value = GENERATE(
+        std::numeric_limits<T>::quiet_NaN(),
+        -std::numeric_limits<T>::quiet_NaN(),
+        std::numeric_limits<T>::signaling_NaN(),
+        -std::numeric_limits<T>::signaling_NaN(),
+        std::numeric_limits<T>::infinity(),
+        -std::numeric_limits<T>::infinity());
+    INFO("Value: " << value);
+
+    REQUIRE_THROWS_AS(
+        matches::approx_rel(value, T{4.2}),
+        std::runtime_error);
+    REQUIRE_THROWS_AS(
+        matches::approx_rel(value),
+        std::runtime_error);
+}
+
+TEMPLATE_TEST_CASE(
+    "matches::approx_rel throws std::runtime_error, when invalid epsilon is given.",
+    "[matcher]",
+    float,
+    double,
+    long double)
+{
+    using T = TestType;
+
+    const auto epsilon = GENERATE(
+        std::numeric_limits<T>::quiet_NaN(),
+        -std::numeric_limits<T>::quiet_NaN(),
+        std::numeric_limits<T>::signaling_NaN(),
+        -std::numeric_limits<T>::signaling_NaN(),
+        std::numeric_limits<T>::infinity(),
+        -std::numeric_limits<T>::infinity(),
+        T{0},
+        -T{0},
+        -std::numeric_limits<T>::epsilon(),
+        -4.2);
+    INFO("Epsilon: " << epsilon);
+
+    REQUIRE_THROWS_AS(
+        matches::approx_rel(T{4.2}, epsilon),
+        std::runtime_error);
+}
