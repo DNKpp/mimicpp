70c25c51aed7b102c97ee96fd988b008d479dd31
DNKpp <dnkpp2011@gmail.com>
2025-01-31 15:32:24 +0100

    feat: util::partition_by

---
+++ include/mimic++/utilities/Algorithm.hpp
@@ -0,0 +1,77 @@
+//          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MIMICPP_UTILITIES_ALGORITHM_HPP
+#define MIMICPP_UTILITIES_ALGORITHM_HPP
+
+#pragma once
+
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <ranges>
+#include <utility>
+
+namespace mimicpp::util
+{
+    /**
+     * \brief Partitions the target range by the predicate results on the control range.
+     * \tparam Target The target range.
+     * \tparam Control The control range.
+     * \tparam Projection The projection type.
+     * \tparam Predicate The predicate type.
+     * \param targetRange The range to be partitioned.
+     * \param controlRange The range, which controls the partition.
+     * \param predicate The predicate for the projected control-elements.
+     * \param projection The projection into the control-elements.
+     * \return A subrange to the elements for which the predicate returned ``false``.
+     *
+     * \details This algorithm partitions the target range by evaluating the associated elements of the control range.
+     * This means, that if the predicate evaluates the control-element to ``false``, the associated target-element moves
+     * into the second half; otherwise it stays at its position.
+     * The association is simply index based => ``target[i]`` is associated with ``control[i]``.
+     *
+     * \note Due to ``std::vector<bool>::iterator`` (and other Proxy-Iterators) not satisfying ``std::permutable``,
+     * this requirement is dropped.
+     * Nevertheless, the range must semantically still be permutable.
+     * \see https://stackoverflow.com/questions/63412623/should-c20-stdrangessort-not-need-to-support-stdvectorbool
+     */
+    template <
+        std::ranges::random_access_range Target,
+        std::ranges::random_access_range Control,
+        typename Projection = std::identity,
+        std::indirect_unary_predicate<
+            std::projected<std::ranges::iterator_t<Control>, Projection>> Predicate>
+        requires std::ranges::sized_range<Target>
+              && std::ranges::sized_range<Control>
+    /*&& std::permutable<std::ranges::iterator_t<Target>>
+    && std::permutable<std::ranges::iterator_t<Control>>*/
+    [[nodiscard]]
+    constexpr std::ranges::borrowed_subrange_t<Target> partition_by(
+        Target&& targetRange,
+        Control&& controlRange,
+        Predicate predicate,
+        Projection projection = {})
+    {
+        assert(std::ranges::size(targetRange) == std::ranges::size(controlRange) && "Size mismatch.");
+
+        auto const targetBegin = std::ranges::begin(targetRange);
+        auto const controlBegin = std::ranges::begin(controlRange);
+        auto midpoint = std::ranges::end(targetRange);
+        auto end = std::ranges::end(controlRange);
+        for (auto iter = std::ranges::find_if_not(controlRange, std::ref(predicate), std::ref(projection));
+             iter != end;
+             iter = std::ranges::find_if_not(iter, end, std::ref(predicate), std::ref(projection)))
+        {
+            auto const index = std::ranges::distance(controlBegin, iter);
+            std::ranges::iter_swap(iter, --end);
+            std::ranges::iter_swap(std::ranges::next(targetBegin, index), --midpoint);
+        }
+
+        return {std::move(midpoint), std::ranges::end(targetRange)};
+    }
+}
+
+#endif
---
+++ test/unit-tests/utilities/Algorithm.cpp
@@ -0,0 +1,115 @@
+//          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "mimic++/utilities/Algorithm.hpp"
+
+using namespace mimicpp;
+
+TEST_CASE(
+    "util::partition_by on empty ranges has no effect.",
+    "[util][util::partition_by]")
+{
+    std::vector<std::string> target{};
+    std::vector<int> control{};
+
+    std::ranges::subrange const result = util::partition_by(target, control, std::bind_front(std::equal_to{}, 42));
+    REQUIRE_THAT(
+        result,
+        Catch::Matchers::IsEmpty());
+    REQUIRE_THAT(
+        target,
+        Catch::Matchers::IsEmpty());
+    REQUIRE_THAT(
+        control,
+        Catch::Matchers::IsEmpty());
+}
+
+TEST_CASE(
+    "util::partition_by supports ranges with just one element.",
+    "[util][util::partition_by]")
+{
+    std::vector<std::string> target{"Hello, World!"};
+    std::vector control{42};
+
+    SECTION("When element satisfies the predicate, it's not part of the result range.")
+    {
+        std::ranges::subrange const result = util::partition_by(target, control, std::bind_front(std::equal_to{}, 42));
+        REQUIRE_THAT(
+            result,
+            Catch::Matchers::IsEmpty());
+    }
+
+    SECTION("When element does not satisfy the predicate, it's part of the result range.")
+    {
+        std::ranges::subrange const result = util::partition_by(target, control, std::bind_front(std::not_equal_to{}, 42));
+        REQUIRE_THAT(
+            result,
+            Catch::Matchers::RangeEquals(std::vector{"Hello, World!"}));
+    }
+
+    REQUIRE_THAT(
+        target,
+        Catch::Matchers::RangeEquals(std::vector{"Hello, World!"}));
+    REQUIRE_THAT(
+        control,
+        Catch::Matchers::RangeEquals(std::vector{42}));
+}
+
+TEST_CASE(
+    "util::partition_by supports ranges with multiple elements.",
+    "[util][util::partition_by]")
+{
+    std::vector<std::string> target{"Test1", "Test2", "Test3"};
+    std::vector control{1, 2, 3};
+
+    SECTION("When none of the elements satisfy the predicate, they are all listed in the result range.")
+    {
+        std::ranges::subrange const result = util::partition_by(target, control, std::bind_front(std::less{}, 10));
+        REQUIRE_THAT(
+            result,
+            Catch::Matchers::UnorderedRangeEquals(std::vector{"Test1", "Test2", "Test3"}));
+    }
+
+    SECTION("When all elements satisfy the predicate, none of them is listed in the result range.")
+    {
+        std::ranges::subrange const result = util::partition_by(target, control, std::bind_front(std::greater{}, 10));
+        REQUIRE_THAT(
+            result,
+            Catch::Matchers::IsEmpty());
+        REQUIRE_THAT(
+            target,
+            Catch::Matchers::RangeEquals(std::vector{"Test1", "Test2", "Test3"}));
+        REQUIRE_THAT(
+            control,
+            Catch::Matchers::RangeEquals(std::vector{1, 2, 3}));
+    }
+
+    SECTION("When some of the elements do not satisfy the predicate, these are listed in the result range.")
+    {
+        std::ranges::subrange const result = util::partition_by(target, control, [](int const val) { return 0 == val % 2; });
+        REQUIRE_THAT(
+            result,
+            Catch::Matchers::UnorderedRangeEquals(std::vector{"Test1", "Test3"}));
+        REQUIRE_THAT(
+            target | std::views::drop(1),
+            Catch::Matchers::RangeEquals(result));
+        REQUIRE_THAT(
+            target | std::views::take(1),
+            Catch::Matchers::RangeEquals(std::vector{"Test2"}));
+        REQUIRE_THAT(
+            control | std::views::drop(1),
+            Catch::Matchers::UnorderedRangeEquals(std::vector{1, 3}));
+        REQUIRE_THAT(
+            control | std::views::take(1),
+            Catch::Matchers::RangeEquals(std::vector{2}));
+    }
+
+    REQUIRE_THAT(
+        target,
+        Catch::Matchers::UnorderedRangeEquals(std::vector{"Test1", "Test2", "Test3"}));
+    REQUIRE_THAT(
+        control,
+        Catch::Matchers::UnorderedRangeEquals(std::vector{1, 2, 3}));
+}
