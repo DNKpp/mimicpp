da469c7bc7d72a0156e674554f04359c6a8a3f26
DNKpp <dnkpp2011@gmail.com>
2025-02-04 09:45:46 +0100

    fix: correct includes in examples/CustomPrinter.cpp

--- examples/CustomPrinter.cpp
+++ examples/CustomPrinter.cpp
@@ -3,7 +3,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
-#include "mimic++/Printer.hpp"
+#include "mimic++/Printing.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
