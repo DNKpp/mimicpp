b3f42f50210592da4b3c43310987bd93403c836d
dnkpp <dnkpp2011@gmail.com>
2025-02-22 15:16:34 +0100

    chore: rework lambda handling on linux

--- include/mimic++/printing/type/PostProcessing.hpp
+++ include/mimic++/printing/type/PostProcessing.hpp
@@ -53,6 +53,43 @@ namespace mimicpp::printing::type::detail
 
     template <print_iterator OutIter>
     [[nodiscard]]
+    std::tuple<OutIter, std::size_t> prettify_lambda_scope(
+        OutIter out,
+        RegexT const& suffixRegex,
+        StringViewT const prefix,
+        StringViewT const fullName)
+    {
+        assert(fullName.data() <= prefix.data() && "Prefix must be part of fullName.");
+        assert(prefix.data() + prefix.size() <= fullName.data() + fullName.size() && "Prefix must be part of fullName.");
+
+        StringViewT rest{prefix.data() + prefix.size(), fullName.data() + fullName.size()};
+        auto const closingIter = util::find_closing_token(rest, '(', ')');
+        assert(closingIter != rest.cend() && "No corresponding closing-token found.");
+
+        SVMatchT suffixMatches{};
+        rest = StringViewT{closingIter, rest.cend()};
+        std::regex_search(rest.cbegin(), rest.cend(), suffixMatches, suffixRegex);
+        assert(!suffixMatches.empty() && "No function suffix found.");
+        assert(suffixMatches.size() == 2 && "Regex out-of-sync.");
+        StringViewT const suffix{suffixMatches[0].first, suffixMatches[0].second};
+
+        out = std::ranges::copy(StringViewT{"lambda#"}, std::move(out)).out;
+        if (0 != suffixMatches[1].length())
+        {
+            out = std::ranges::copy(suffixMatches[1].first, suffixMatches[1].second, std::move(out)).out;
+        }
+        else // no number set?, just default to 1
+        {
+            out = std::ranges::copy(StringViewT{"1"}, std::move(out)).out;
+        }
+
+        return {
+            std::ranges::copy(StringViewT{"::"}, std::move(out)).out,
+            static_cast<std::size_t>(std::ranges::distance(prefix.data(), suffix.data() + suffix.size()))};
+    }
+
+    template <print_iterator OutIter>
+    [[nodiscard]]
     std::tuple<OutIter, std::size_t> prettify_function_scope(
         OutIter out,
         RegexT const& functionSuffixRegex,
@@ -88,12 +125,66 @@ namespace mimicpp::printing::type::detail
     {
         assert(scope.data() == fullName.data() && "Scope and fullName are not aligned.");
 
+        SVMatchT matches{};
+
+        constexpr StringViewT anonymousNamespaceToken{"(anonymous namespace)::"};
+        if (scope == anonymousNamespaceToken)
+        {
+            return std::tuple{
+                format::format_to(std::move(out), "(anon ns)::"),
+                scope.size() - anonymousNamespaceToken.size(),
+                anonymousNamespaceToken.size()};
+        }
+
+        // libc++ sometimes also uses `'lambda'()` for lambdas
+        #if MIMICPP_DETAIL_USES_LIBCXX
+        if (constexpr StringViewT libcxxLambdaScopePrefix{"'lambda'("};
+            scope.starts_with(libcxxLambdaScopePrefix))
+        {
+            // use a fake group for the lambda no
+            static RegexT lambdaEndRegex{R"(\)()::)"};
+
+            std::size_t count{};
+            std::tie(out, count) = prettify_lambda_scope(
+                std::move(out),
+                lambdaEndRegex,
+                StringViewT{fullName.cbegin(), fullName.cbegin() + libcxxLambdaScopePrefix.size()},
+                fullName);
+            return std::tuple{std::move(out), 0u, count};
+        }
+        // while libstdc++ sometimes uses `{lambda()#N}`
+        #else
+        if (constexpr StringViewT lambdaScopePrefix{"{lambda("};
+            scope.starts_with(lambdaScopePrefix))
+        {
+            static RegexT lambdaEndRegex{R"(\)#(\d+)\}::)"};
+
+            std::size_t count{};
+            std::tie(out, count) = prettify_lambda_scope(
+                std::move(out),
+                lambdaEndRegex,
+                StringViewT{fullName.cbegin(), fullName.cbegin() + lambdaScopePrefix.size()},
+                fullName);
+            return std::tuple{std::move(out), 0u, count};
+        }
+        #endif
+
+        // clang sometimes also uses `$_N` for lambdas
+        #if MIMICPP_DETAIL_IS_CLANG
+        static const RegexT lambda2Scope{R"(\$_(\d+)::)"};
+        if (std::regex_match(scope.cbegin(), scope.cend(), matches, lambda2Scope))
+        {
+            return std::tuple{
+                prettify_lambda_scope(std::move(out), matches),
+                std::ranges::distance(scope.cbegin(), matches[0].first),
+                matches[0].length()};
+        }
+        #endif
+
         static RegexT const functionScopePrefix{
             R"(^(?:\w+\s+)?)"        // return type (optional)
             R"((operator.+?|\w+)\()" // function-name + (
         };
-
-        SVMatchT matches{};
         if (std::regex_search(scope.cbegin(), scope.cend(), matches, functionScopePrefix))
         {
             static RegexT const functionSuffixRegex{
@@ -110,48 +201,6 @@ namespace mimicpp::printing::type::detail
                 matches,
                 fullName);
             return std::tuple{std::move(out), 0u, count};
-        }
-
-        constexpr StringViewT anonymousNamespaceToken{"(anonymous namespace)::"};
-        if (scope == anonymousNamespaceToken)
-        {
-            return std::tuple{
-                format::format_to(std::move(out), "(anon ns)::"),
-                scope.size() - anonymousNamespaceToken.size(),
-                anonymousNamespaceToken.size()};
-        }
-
-        // clang sometimes also uses `$_N` for lambdas
-        #if MIMICPP_DETAIL_IS_CLANG
-        static const RegexT lambda2Scope{R"(\$_(\d+)::)"};
-        if (std::regex_match(scope.cbegin(), scope.cend(), matches, lambda2Scope))
-        {
-            return std::tuple{
-                prettify_lambda_scope(std::move(out), matches),
-                std::ranges::distance(scope.cbegin(), matches[0].first),
-                matches[0].length()};
-        }
-        #endif
-
-        // libc++ sometimes also uses `'lambda'()` for lambdas
-        #if MIMICPP_DETAIL_USES_LIBCXX
-        constexpr StringViewT libcxxLambdaToken{"'lambda'()::"};
-        if (scope == libcxxLambdaToken)
-        {
-            return std::tuple{
-                format::format_to(std::move(out), "lambda#1::"),
-                scope.size() - libcxxLambdaToken.size(),
-                libcxxLambdaToken.size()};
-        }
-        #endif
-
-        static RegexT const lambdaScope{R"(\{lambda\(\)#(\d+)\}::)"};
-        if (std::regex_match(scope.cbegin(), scope.cend(), matches, lambdaScope))
-        {
-            return std::tuple{
-                prettify_lambda_scope(std::move(out), matches),
-                std::ranges::distance(scope.cbegin(), matches[0].first),
-                matches[0].length()};
         }
 
         // Probably a regular c++-scope
@@ -193,8 +242,6 @@ namespace mimicpp::printing::type::detail
     {
         static const RegexT unifyClosingAngleBrackets{R"(\s+>)"};
         name = std::regex_replace(name, unifyClosingAngleBrackets, ">");
-
-        name = unify_lambda_scopes(std::move(name));
 
         static const RegexT stdImplNamespace{
         #if MIMICPP_DETAIL_USES_LIBCXX
