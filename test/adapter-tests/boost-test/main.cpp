04dc31f6565311fe871df8222cd50a4cdc10dbf6
dnkpp <dnkpp2011@gmail.com>
2025-03-04 17:22:27 +0100

    chore: mimicpp-adapter-tests-boost-test utilizes test-basics

--- test/adapter-tests/boost-test/main.cpp
+++ test/adapter-tests/boost-test/main.cpp
@@ -3,6 +3,8 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
+#include <TestAssert.hpp>
+
 #include "../../unit-tests/SuppressionMacros.hpp"
 #include "mimic++/Mock.hpp"
 
