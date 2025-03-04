b1a1293df9a6f315c1e7eab0a872a717d5f46a61
dnkpp <dnkpp2011@gmail.com>
2025-03-04 12:57:37 +0100

    cleanup: remove obsolete helper function util::regex_replace_all

--- include/mimic++/utilities/Regex.hpp
+++ include/mimic++/utilities/Regex.hpp
@@ -21,37 +21,4 @@ namespace mimicpp
     using SVMatchT = std::match_results<StringViewT::const_iterator>;
 }
 
-namespace mimicpp::util
-{
-    template <typename Char, typename CharTraits, typename Allocator, typename RegexTraits>
-    [[nodiscard]]
-    constexpr std::basic_string<Char, CharTraits, Allocator> regex_replace_all(
-        std::basic_string<Char, CharTraits, Allocator> str,
-        std::basic_regex<Char, RegexTraits> const& regex,
-        std::basic_string_view<Char, CharTraits> const fmt)
-    {
-        using match_result_t = std::match_results<typename std::basic_string<Char, CharTraits, Allocator>::const_iterator>;
-
-        // just advance by one, so that we can find matches which involves the replaced fmt
-        // like: `> > >` with regex `> >` and fmt = `>>`
-        //      => `>> >` => `>>>`
-        // When fmt is empty, we need a step size of 0.
-        auto const advanceStep = static_cast<std::ptrdiff_t>(!fmt.empty());
-
-        auto processedIter = str.cbegin();
-        match_result_t match{};
-        while (std::regex_search(processedIter, str.cend(), match, regex))
-        {
-            auto const matchBegin = match[0].first;
-            auto const matchEnd = match[0].second;
-
-            auto const nextIndex = std::distance(str.cbegin(), matchBegin + advanceStep);
-            str.replace(matchBegin, matchEnd, fmt);
-            processedIter = str.cbegin() + nextIndex;
-        }
-
-        return str;
-    }
-}
-
 #endif
--- test/unit-tests/utilities/Regex.cpp
+++ test/unit-tests/utilities/Regex.cpp
@@ -7,39 +7,3 @@
 
 using namespace mimicpp;
 
-TEST_CASE(
-    "util::regex_replace_all replaces all regex occurrences with the format-string.",
-    "[util]")
-{
-    SECTION("When an empty string is given, nothing has to be done.")
-    {
-        auto const result = util::regex_replace_all(std::string{}, std::regex{"\\d"}, std::string_view{});
-        REQUIRE_THAT(
-            result,
-            Catch::Matchers::Equals(""));
-    }
-
-    SECTION("When an empty fmt is given, the tokens are just removed.")
-    {
-        std::string const input{"00Test12t1T123"};
-        auto const result = util::regex_replace_all(
-            input,
-            std::regex{"\\d"},
-            std::string_view{});
-        REQUIRE_THAT(
-            result,
-            Catch::Matchers::Equals("TesttT"));
-    }
-
-    SECTION("The fmt replaces all (even on the fly introduced) tokens.")
-    {
-        std::string const input{" >> > > > "};
-        auto const result = util::regex_replace_all(
-            input,
-            std::regex{R"(\>\s+\>)"},
-            std::string_view{">>"});
-        REQUIRE_THAT(
-            result,
-            Catch::Matchers::Equals(" >>>>> "));
-    }
-}
