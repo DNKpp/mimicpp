4d867eab13f63dcbd08803f35155e9f35f97f292
dnkpp <dnkpp2011@gmail.com>
2025-03-04 14:54:58 +0100

    deps: add eremy-rifkin/libassert v2.1.5

---
+++ cmake/Findlibassert.cmake
@@ -0,0 +1,8 @@
+#          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          https://www.boost.org/LICENSE_1_0.txt)
+
+include(get_cpm)
+
+CPMAddPackage("gh:jeremy-rifkin/libassert@2.1.5")
