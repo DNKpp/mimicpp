eaa80a9a30b1e1ef3b8c2885991dbb4ce5df24b7
dnkpp <dnkpp2011@gmail.com>
2025-03-17 14:51:41 +0100

    fix

--- cmake/EnableSanitizers.cmake
+++ cmake/EnableSanitizers.cmake
@@ -9,11 +9,17 @@ function(enable_sanitizers TARGET_NAME)
 	if (SANITIZE_ADDRESS)
 		# workaround linker errors on msvc
 		# see: https://learn.microsoft.com/en-us/answers/questions/864574/enabling-address-sanitizer-results-in-error-lnk203
-		target_compile_definitions(
-			${TARGET_NAME}
+		target_compile_definitions(${TARGET_NAME}
 			PRIVATE
-			$<$<CXX_COMPILER_ID:MSVC>:_DISABLE_VECTOR_ANNOTATION>
-			$<$<CXX_COMPILER_ID:MSVC>:_DISABLE_STRING_ANNOTATION>
+			"$<$<CXX_COMPILER_ID:MSVC>:_DISABLE_VECTOR_ANNOTATION;_DISABLE_STRING_ANNOTATION>"
+		)
+
+		target_compile_options(${TARGET_NAME}
+			PRIVATE
+			# see: https://gcc.gnu.org/onlinedocs/gcc/Instrumentation-Options.html#index-fsanitize_003dbuiltin
+			"$<$<CXX_COMPILER_ID:GNU>:-g;-fno-omit-frame-pointer;-fno-optimize-sibling-calls;-fno-ipa-icf>"
+			# see: https://clang.llvm.org/docs/AddressSanitizer.html#id3
+			"$<$<CXX_COMPILER_ID:Clang>:-g;-fno-omit-frame-pointer;-fno-optimize-sibling-calls>"
 		)
 	endif()
 
