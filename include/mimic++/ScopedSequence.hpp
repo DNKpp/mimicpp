0ccf82ed541cdfe8ae27ae29730b3c891082711f
dnkpp <dnkpp2011@gmail.com>
2025-03-21 21:14:48 +0100

    feat: ScopedSequence

---
+++ include/mimic++/ScopedSequence.hpp
@@ -0,0 +1,180 @@
+//          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MIMICPP_SCOPED_SEQUENCE_HPP
+#define MIMICPP_SCOPED_SEQUENCE_HPP
+
+#pragma once
+
+#include "mimic++/Expectation.hpp"
+#include "mimic++/ExpectationBuilder.hpp"
+#include "mimic++/Sequence.hpp"
+#include "mimic++/config/Config.hpp"
+#include "mimic++/utilities/SourceLocation.hpp"
+
+#include <functional>
+#include <utility>
+#include <vector>
+
+namespace mimicpp::sequence::detail
+{
+    template <typename Sequence>
+    class ExpectationBuilderFinalizer
+    {
+    public:
+        ExpectationBuilderFinalizer(ExpectationBuilderFinalizer const&) = delete;
+        ExpectationBuilderFinalizer& operator=(ExpectationBuilderFinalizer const&) = delete;
+        ExpectationBuilderFinalizer(ExpectationBuilderFinalizer&&) = delete;
+        ExpectationBuilderFinalizer& operator=(ExpectationBuilderFinalizer&&) = delete;
+
+        ~ExpectationBuilderFinalizer() = default;
+
+        template <bool timesConfigured, typename... Args>
+        [[nodiscard]]
+        explicit(false) constexpr ExpectationBuilderFinalizer(
+            BasicExpectationBuilder<timesConfigured, Args...>&& builder,
+            util::SourceLocation loc = {})
+            : m_Builder{&builder},
+              m_SourceLocation{std::move(loc)},
+              m_FinalizeFn{
+                  +[](void* storage, util::SourceLocation const& loc, Sequence& sequence) {
+                      auto* builderPtr = static_cast<BasicExpectationBuilder<timesConfigured, Args...>*>(storage);
+                      return ScopedExpectation{
+                          std::move(*builderPtr) and expect::in_sequence(sequence),
+                          *loc};
+                  }}
+        {
+        }
+
+        [[nodiscard]]
+        ScopedExpectation finalize(Sequence& sequence)
+        {
+            MIMICPP_ASSERT(m_Builder, "Builder is nullptr.");
+            MIMICPP_ASSERT(m_FinalizeFn, "FinalizeFn is nullptr.");
+
+            return std::invoke(
+                std::exchange(m_FinalizeFn, nullptr),
+                std::exchange(m_Builder, nullptr),
+                m_SourceLocation,
+                sequence);
+        }
+
+    private:
+        void* m_Builder;
+        util::SourceLocation m_SourceLocation;
+
+        using FinalizeFn = ScopedExpectation (*)(void*, util::SourceLocation const&, Sequence&);
+        FinalizeFn m_FinalizeFn;
+    };
+}
+
+namespace mimicpp
+{
+    /**
+     * \brief A sequence type that verifies its owned expectations during destruction.
+     * \ingroup EXPECTATION_SEQUENCE
+     * \tparam Strategy The sequence strategy employed.
+     *
+     * \details This type offers a convenient mechanism for chaining multiple expectations into a sequence,
+     * and it monitors whether they are fulfilled upon destruction.
+     *
+     * In essence, it behaves similarly to manually attaching expectations via `expect::in_sequence` to a sequence.
+     * The primary distinction is that if an expectation is not satisfied,
+     * this type will report the first unfulfilled expectation.
+     * In contrast, when expectations are manually constructed as `ScopedExpectation`,
+     * the last created expectation (due to the stack’s order) would typically be reported.
+     *
+     * Nevertheless, all standard sequence rules also apply.
+     */
+    template <auto Strategy>
+    class BasicScopedSequence
+        : public sequence::detail::BasicSequenceInterface<sequence::Id, Strategy>
+    {
+    public:
+        /**
+         * \brief Deleted copy-constructor.
+         */
+        [[nodiscard]]
+        BasicScopedSequence(BasicScopedSequence const&) = delete;
+
+        /**
+         * \brief Deleted copy-assignment operator.
+         */
+        BasicScopedSequence& operator=(BasicScopedSequence const&) = delete;
+
+        /**
+         * \brief Defaulted, but possibly throwing, destructor.
+         */
+        ~BasicScopedSequence() noexcept(false) = default;
+
+        /**
+         * \brief Default default-constructor.
+         */
+        [[nodiscard]]
+        BasicScopedSequence() = default;
+
+        /**
+         * \brief Defaulted move-constructor.
+         */
+        [[nodiscard]]
+        BasicScopedSequence(BasicScopedSequence&&) = default;
+
+        /**
+         * \brief Defaulted move-assignment operator.
+         * \return A mutable reference to the current instance.
+         */
+        BasicScopedSequence& operator=(BasicScopedSequence&&) = default;
+
+        /**
+         * \brief Attaches a newly constructed expectation.
+         * \param builder The expectation-builder that will be finalized.
+         * \return A mutable reference to the current instance.
+         * \details This function augments the provided expectation-builder with an additional sequence-policy for this
+         * sequence and finalizes its construction.
+         * Additionally, the sequence takes over the ownership of the constructed `ScopedExpectation`.
+         */
+        BasicScopedSequence& operator+=(sequence::detail::ExpectationBuilderFinalizer<BasicScopedSequence>&& builder)
+        {
+            m_Expectations.emplace_back(builder.finalize(*this));
+
+            return *this;
+        }
+
+        /**
+         * \brief Retrieves the collection of explicitly owned expectations.
+         * \return A constant reference to the collection of attached expectations.
+         * \note This function returns only those expectations that were explicitly attached using the `operator +=`,
+         * and not every expectation that is currently queued in the sequence (i.e. by manually using `expect::in_sequence`).
+         */
+        [[nodiscard]]
+        std::vector<ScopedExpectation> const& expectations() const noexcept
+        {
+            return m_Expectations;
+        }
+
+    private:
+        std::vector<ScopedExpectation> m_Expectations{};
+    };
+
+    /**
+     * \brief The scoped-sequence type with greedy strategy.
+     * \ingroup EXPECTATION_SEQUENCE
+     */
+    using GreedyScopedSequence = BasicScopedSequence<sequence::detail::GreedyStrategy{}>;
+
+    /**
+     * \brief The scoped-sequence type with lazy strategy.
+     * \ingroup EXPECTATION_SEQUENCE
+     */
+    using LazyScopedSequence = BasicScopedSequence<sequence::detail::LazyStrategy{}>;
+
+    /**
+     * \brief The default scoped-sequence type (lazy strategy).
+     * \ingroup EXPECTATION_SEQUENCE
+     */
+    using ScopedSequence = LazyScopedSequence;
+}
+
+#endif
---
+++ test/unit-tests/ScopedSequence.cpp
@@ -0,0 +1,162 @@
+//          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "mimic++/ScopedSequence.hpp"
+#include "mimic++/Mock.hpp"
+
+#include "TestReporter.hpp"
+
+using namespace mimicpp;
+
+namespace
+{
+    using TestScopedSequence = BasicScopedSequence<sequence::detail::LazyStrategy{}>;
+}
+
+TEST_CASE(
+    "ScopedSequence is a non-copyable but movable type.",
+    "[sequence]")
+{
+    STATIC_REQUIRE(!std::is_copy_constructible_v<TestScopedSequence>);
+    STATIC_REQUIRE(!std::is_copy_assignable_v<TestScopedSequence>);
+
+    STATIC_REQUIRE(!std::is_nothrow_destructible_v<TestScopedSequence>);
+
+    STATIC_REQUIRE(std::is_default_constructible_v<TestScopedSequence>);
+    STATIC_REQUIRE(std::is_move_constructible_v<TestScopedSequence>);
+    STATIC_REQUIRE(std::is_move_assignable_v<TestScopedSequence>);
+}
+
+TEST_CASE(
+    "ScopedSequence checks attached expectations during destruction.",
+    "[sequence]")
+{
+    ScopedReporter reporter{};
+    Mock<void()> mock{};
+
+    SECTION("When no expectations are attached, nothing is checked.")
+    {
+        TestScopedSequence sequence{};
+    }
+
+    SECTION("When single expectation is attached.")
+    {
+        TestScopedSequence sequence{};
+        sequence += mock.expect_call();
+
+        mock();
+    }
+
+    SECTION("When multiple expectations are attached.")
+    {
+        TestScopedSequence sequence{};
+        sequence += mock.expect_call();
+        sequence += mock.expect_call();
+
+        mock();
+
+        sequence += mock.expect_call();
+
+        mock();
+        mock();
+    }
+
+    CHECK_THAT(
+        reporter.unfulfilled_expectations(),
+        Catch::Matchers::IsEmpty());
+}
+
+TEST_CASE(
+    "ScopedSequence reports unfulfilled expectations during destruction.",
+    "[sequence]")
+{
+    ScopedReporter reporter{};
+    Mock<void()> mock{};
+
+    SECTION("When a single expectation is attached.")
+    {
+        std::vector<util::SourceLocation> locations{};
+
+        {
+            TestScopedSequence sequence{};
+            sequence += mock.expect_call();
+
+            std::ranges::transform(
+                sequence.expectations(),
+                std::back_inserter(locations),
+                &ScopedExpectation::from);
+        }
+
+        REQUIRE_THAT(
+            reporter.unfulfilled_expectations(),
+            Catch::Matchers::SizeIs(1u));
+        auto const& report = reporter.unfulfilled_expectations().front();
+        CHECK(locations.front() == report.from);
+    }
+
+    SECTION("When multiple expectations are attached, the first unfulfilled is reported.")
+    {
+        std::optional<TestScopedSequence> sequence{std::in_place};
+        *sequence += mock.expect_call();
+        *sequence += mock.expect_call();
+        *sequence += mock.expect_call();
+
+        std::vector<util::SourceLocation> locations{};
+        std::ranges::transform(
+            sequence->expectations(),
+            std::back_inserter(locations),
+            &ScopedExpectation::from);
+        REQUIRE_THAT(
+            locations,
+            Catch::Matchers::SizeIs(3));
+
+        auto const calls = GENERATE(0u, 1u, 2u);
+        for ([[maybe_unused]] auto const i : std::views::iota(0u, calls))
+        {
+            mock();
+        }
+
+        sequence.reset();
+
+        REQUIRE_THAT(
+            reporter.unfulfilled_expectations(),
+            Catch::Matchers::SizeIs(locations.size() - calls));
+        auto const& report = reporter.unfulfilled_expectations().front();
+        CHECK(locations[calls] == report.from);
+    }
+}
+
+TEST_CASE(
+    "Expectations can manually attached to BasicScopedSequences.",
+    "[sequence]")
+{
+    Mock<void()> mock{};
+
+    TestScopedSequence sequence{};
+    sequence += mock.expect_call();
+
+    ScopedExpectation expectation = mock.expect_call()
+                                and expect::in_sequence(sequence);
+
+    ScopedExpectation freestandingExpectation = mock.expect_call();
+    REQUIRE_THAT(
+        sequence.expectations(),
+        Catch::Matchers::SizeIs(1u));
+
+    mock();
+    CHECK(freestandingExpectation.is_satisfied());
+    CHECK(!expectation.is_satisfied());
+    CHECK(!sequence.expectations().front().is_satisfied());
+
+    mock();
+    CHECK(freestandingExpectation.is_satisfied());
+    CHECK(!expectation.is_satisfied());
+    CHECK(sequence.expectations().front().is_satisfied());
+
+    mock();
+    CHECK(freestandingExpectation.is_satisfied());
+    CHECK(expectation.is_satisfied());
+    CHECK(sequence.expectations().front().is_satisfied());
+}
