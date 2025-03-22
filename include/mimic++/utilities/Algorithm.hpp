4ddd08afb698ad9f344fc5d314fec9ce67db40d7
dnkpp <dnkpp2011@gmail.com>
2025-03-22 20:54:42 +0100

    feat: util::concat_array

--- include/mimic++/utilities/Algorithm.hpp
+++ include/mimic++/utilities/Algorithm.hpp
@@ -11,11 +11,13 @@
 #include "mimic++/config/Config.hpp"
 
 #include <algorithm>
+#include <array>
 #include <concepts>
 #include <functional>
 #include <iterator>
 #include <ranges>
 #include <string_view>
+#include <tuple>
 #include <utility>
 
 namespace mimicpp::util
@@ -196,6 +198,57 @@ namespace mimicpp::util
             });
 
         return {lower, end};
+    }
+
+    /**
+     * \brief Concatenates the given arrays by copying all elements into a new array.
+     * \tparam T The element type.
+     * \tparam firstN The size of the first array.
+     * \tparam secondN The size of the second array.
+     * \param first The first array.
+     * \param second The second array.
+     * \return A newly constructed arrays with copied elements.
+     */
+    template <std::copyable T, std::size_t firstN, std::size_t secondN>
+    [[nodiscard]]
+    constexpr std::array<T, firstN + secondN> concat_arrays(std::array<T, firstN> const& first, std::array<T, secondN> const& second)
+    {
+        return std::invoke(
+            [&]<std::size_t... firstIs, std::size_t... secondIs>(
+                [[maybe_unused]] std::index_sequence<firstIs...> const,
+                [[maybe_unused]] std::index_sequence<secondIs...> const) {
+                return std::array<T, firstN + secondN>{
+                    std::get<firstIs>(first)...,
+                    std::get<secondIs>(second)...};
+            },
+            std::make_index_sequence<firstN>{},
+            std::make_index_sequence<secondN>{});
+    }
+
+    /**
+     * \copybrief concat_arrays
+     * \tparam T The element type.
+     * \tparam firstN The size of the first array.
+     * \tparam Others Other array types which share the same element-type.
+     * \param first The first array.
+     * \param others The second array.
+     * \return A newly constructed arrays with copied elements.
+     */
+    template <std::copyable T, std::size_t firstN, typename... Others>
+        requires(... && std::same_as<T, std::ranges::range_value_t<Others>>)
+    [[nodiscard]]
+    constexpr std::array<T, (firstN + ... + std::tuple_size_v<Others>)> concat_arrays(std::array<T, firstN> const& first, Others const&... others)
+    {
+        if constexpr (0u == sizeof...(Others))
+        {
+            return first;
+        }
+        else
+        {
+            return concat_arrays(
+                first,
+                concat_arrays(others...));
+        }
     }
 }
 
