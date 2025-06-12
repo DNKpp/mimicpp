44df1ace1976a874241c1e27af096087cecced17
DNKpp <dnkpp2011@gmail.com>
2025-06-12 21:33:10 +0200

    test: add custom-source-location-tests

---
+++ test/custom-source-location-tests/CustomSourceLocation.cpp
@@ -0,0 +1,65 @@
+//          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "CustomSourceLocation.hpp"
+
+using namespace mimicpp;
+
+TEST_CASE(
+    "mimic++ prefers custom registrations, when present.",
+    "[util][util::source_location]")
+{
+    STATIC_REQUIRE(std::same_as<CustomBackend, util::source_location::InstalledBackend>);
+
+    using trompeloeil::_;
+    using trompeloeil::gt;
+
+    using traits = util::source_location::backend_traits<CustomBackend>;
+
+    std::shared_ptr const inner = std::make_shared<CustomBackend::Inner>();
+    REQUIRE_CALL(traits::currentMock, Invoke())
+        .LR_RETURN(CustomBackend{inner});
+    util::SourceLocation const loc{};
+
+    SECTION("Testing file-name.")
+    {
+        std::string const fileName = GENERATE("", "Test", " Hello, World! ");
+        REQUIRE_CALL(inner->fileNameMock, Invoke())
+            .LR_RETURN(fileName);
+
+        CHECK_THAT(
+            std::string{loc.file_name()},
+            Catch::Matchers::Equals(fileName));
+    }
+
+    SECTION("Testing function-name.")
+    {
+        std::string const functionName = GENERATE("", "Test", " Hello, World! ");
+        REQUIRE_CALL(inner->functionNameMock, Invoke())
+            .LR_RETURN(functionName);
+
+        CHECK_THAT(
+            std::string{loc.function_name()},
+            Catch::Matchers::Equals(functionName));
+    }
+
+    SECTION("Testing line.")
+    {
+        std::size_t const line = GENERATE(0u, 1u, 42u);
+        REQUIRE_CALL(inner->lineMock, Invoke())
+            .LR_RETURN(line);
+
+        REQUIRE(line == loc.line());
+    }
+
+    SECTION("Testing column.")
+    {
+        std::size_t const column = GENERATE(0u, 1u, 42u);
+        REQUIRE_CALL(inner->columnMock, Invoke())
+            .LR_RETURN(column);
+
+        REQUIRE(column == loc.column());
+    }
+}
