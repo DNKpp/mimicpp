f9a89dcb6554b46fb362063f971ed54de4c16094
DNKpp <dnkpp2011@gmail.com>
2025-10-04 22:14:17 +0200

    refactor: util::StaticString does not store the null-terminator

---
+++ test/unit-tests/utilities/StaticString.cpp
@@ -0,0 +1,48 @@
+//          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "mimic++/utilities/StaticString.hpp"
+
+using namespace mimicpp;
+
+TEST_CASE(
+    "util::StaticString can hold compile-time usable strings.",
+    "[util]")
+{
+    SECTION("Empty string is supported.")
+    {
+        constexpr util::StaticString text{""};
+
+        CHECK_THAT(
+            text,
+            Catch::Matchers::IsEmpty());
+        CHECK_THAT(
+            text,
+            Catch::Matchers::SizeIs(0u));
+        CHECK_THAT(
+            text,
+            Catch::Matchers::RangeEquals(std::vector<char>{}));
+    }
+
+    SECTION("When arbitrary string is given.")
+    {
+        constexpr util::StaticString text{"Hello, World!"};
+
+        CHECK_THAT(
+            (std::string{text.begin(), text.end()}),
+            Catch::Matchers::Equals("Hello, World!"));
+        CHECK_THAT(
+            text,
+            !Catch::Matchers::IsEmpty());
+        CHECK_THAT(
+            text,
+            Catch::Matchers::SizeIs(std::ranges::size("Hello, World!") - 1u));
+    }
+
+    SECTION("StaticString is fully usable in compile-time context.")
+    {
+        STATIC_CHECK(std::ranges::equal(util::StaticString{"Hello, World!"}, std::string_view{"Hello, World!"}));
+    }
+}
