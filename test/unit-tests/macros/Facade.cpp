39b2f7f22de7f79492191c4533145816f01cfb7b
DNKpp <dnkpp2011@gmail.com>
2025-10-12 12:47:44 +0200

    refactor: rename macros/FacadeHelper.hpp to macros/Facade.hpp

--- test/unit-tests/macros/Facade.cpp
+++ test/unit-tests/macros/Facade.cpp
@@ -3,7 +3,6 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
-#include "mimic++/macros/FacadeHelper.hpp"
 #include "mimic++/Facade.hpp"
 
 using namespace mimicpp;
