43f99d9d241f1a269714f675a5f9f81fe1c2610b
DNKpp <dnkpp2011@gmail.com>
2025-10-12 14:46:27 +0200

    refactor: split Facade.cpp into several source-files inside `facade` sub-dir

---
+++ test/unit-tests/facade/Traits.cpp
@@ -0,0 +1,94 @@
+//          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "TestReporter.hpp"
+#include "mimic++/Facade.hpp"
+#include "mimic++/ScopedSequence.hpp"
+
+using namespace mimicpp;
+
+namespace
+{
+    void CheckOmittedStacktraceEntry(util::SourceLocation const& before, reporting::CallReport const& report, util::SourceLocation const& after)
+    {
+#if MIMICPP_DETAIL_HAS_WORKING_STACKTRACE_BACKEND
+        CHECK_THAT(
+            report.stacktrace.source_file(0u),
+            Catch::Matchers::Equals(std::string{before.file_name()}));
+        // there is no straight-forward way to check the description
+        CHECK(before.line() < report.stacktrace.source_line(0u));
+        // strict < fails on some compilers
+        CHECK(report.stacktrace.source_line(0u) <= after.line());
+#else
+        REQUIRE(report.stacktrace.empty());
+#endif
+    }
+}
+
+TEST_CASE(
+    "facade::mock_as_member traits omits the facade functions stacktrace entry.",
+    "[facade]")
+{
+    struct Type
+    {
+        MIMICPP_MAKE_FACADE_EXT(
+            facade::mock_as_member,
+            foo_,
+            foo,
+            /*linkage*/,
+            void,
+            ());
+    };
+
+    ScopedReporter reporter{};
+
+    Type target{};
+    ScopedExpectation const exp = target.foo_.expect_call();
+    constexpr util::SourceLocation before{};
+    target.foo();
+    constexpr util::SourceLocation after{};
+
+    REQUIRE_THAT(
+        reporter.full_match_reports(),
+        Catch::Matchers::SizeIs(1u));
+
+    CheckOmittedStacktraceEntry(
+        before,
+        std::get<0>(reporter.full_match_reports().front()),
+        after);
+}
+
+TEST_CASE(
+    "facade::mock_as_member_with_this traits omits the facade functions stacktrace entry.",
+    "[facade]")
+{
+    struct Type
+    {
+        MIMICPP_MAKE_FACADE_EXT(
+            facade::mock_as_member_with_this<Type>,
+            foo_,
+            foo,
+            /*linkage*/,
+            void,
+            ());
+    };
+
+    ScopedReporter reporter{};
+
+    Type target{};
+    ScopedExpectation const exp = target.foo_.expect_call(&target);
+    constexpr util::SourceLocation before{};
+    target.foo();
+    constexpr util::SourceLocation after{};
+
+    REQUIRE_THAT(
+        reporter.full_match_reports(),
+        Catch::Matchers::SizeIs(1u));
+
+    CheckOmittedStacktraceEntry(
+        before,
+        std::get<0>(reporter.full_match_reports().front()),
+        after);
+}
