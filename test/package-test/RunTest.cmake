a2bf9669408c854d0a0abbfa2dfd3342667aa24f
DNKpp <dnkpp2011@gmail.com>
2025-10-17 23:42:51 +0200

    fix: make package-test more reliable

--- test/package-test/RunTest.cmake
+++ test/package-test/RunTest.cmake
@@ -5,6 +5,18 @@
 
 set(PACKAGE_PREFIX "${TEST_BINARY_DIR}/package")
 
+function(unwrap_args LIST OUT_LIST)
+    string(LENGTH ${LIST} STR_LENGTH)
+    math(EXPR STR_LENGTH "${STR_LENGTH} - 4")
+    string(SUBSTRING ${LIST} 2 ${STR_LENGTH} LIST)
+    set(${OUT_LIST} ${LIST} PARENT_SCOPE)
+endfunction()
+
+unwrap_args(${CONFIG_ARGS} CONFIG_ARGS)
+unwrap_args(${BUILD_ARGS} BUILD_ARGS)
+message(STATUS CONFIG_ARGS: "${CONFIG_ARGS}")
+message(STATUS BUILD_ARGS: "${BUILD_ARGS}")
+
 # Clear any previous attempt
 execute_process(COMMAND ${CMAKE_COMMAND} -E rm -rf "${TEST_BINARY_DIR}"
     COMMAND_ERROR_IS_FATAL ANY
@@ -34,25 +46,22 @@ execute_process(COMMAND ${CMAKE_COMMAND} -E rename "${PACKAGE_PREFIX}/origin" "$
 # Configure, build and run the test
 set(PACKAGE_TEST_BINARY_DIR "${TEST_BINARY_DIR}/package-consumer")
 execute_process(COMMAND ${CMAKE_COMMAND}
-    -S "${TEST_SOURCE_DIR}/package-consumer"
-    -B "${PACKAGE_TEST_BINARY_DIR}"
-    --fresh
-    --log-level=DEBUG
-    -D
-    CMAKE_PREFIX_PATH=${PACKAGE_PREFIX}/other/location
-    -D
-    CMAKE_MODULE_PATH=${PROJECT_SOURCE_DIR}/cmake
-    -D MIMICPP_WITH_FMT=${MIMICPP_WITH_FMT}
-    -D MIMICPP_WITH_UNICODE_STR_MATCHER=${MIMICPP_WITH_UNICODE_STR_MATCHER}
-    -D MIMICPP_WITH_STACKTRACE=${MIMICPP_WITH_STACKTRACE}
-    COMMAND_ERROR_IS_FATAL ANY
-    COMMAND_ECHO STDOUT
+        -S "${TEST_SOURCE_DIR}/package-consumer"
+        -B "${PACKAGE_TEST_BINARY_DIR}"
+        --fresh
+        --log-level=DEBUG
+        ${CONFIG_ARGS}
+        -D CMAKE_PREFIX_PATH=${PACKAGE_PREFIX}/other/location
+        -D CMAKE_MODULE_PATH=${PROJECT_SOURCE_DIR}/cmake
+        COMMAND_ERROR_IS_FATAL ANY
+        COMMAND_ECHO STDOUT
 )
-execute_process(COMMAND ${CMAKE_COMMAND} --build "${PACKAGE_TEST_BINARY_DIR}" -j
-    COMMAND_ERROR_IS_FATAL ANY
-    COMMAND_ECHO STDOUT
+execute_process(COMMAND ${CMAKE_COMMAND} --build "${PACKAGE_TEST_BINARY_DIR}" ${BUILD_ARGS} -j
+        COMMAND_ERROR_IS_FATAL ANY
+        COMMAND_ECHO STDOUT
 )
 execute_process(COMMAND ${CMAKE_CTEST_COMMAND} --test-dir "${PACKAGE_TEST_BINARY_DIR}" -C ${CMAKE_BUILD_TYPE}
-    COMMAND_ERROR_IS_FATAL ANY
-    COMMAND_ECHO STDOUT
+        COMMAND_ERROR_IS_FATAL ANY
+        COMMAND_ECHO STDOUT
 )
+
