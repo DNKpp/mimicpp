2ae11268573aac5256c262cc24d337b172a23eb2
DNKpp <dnkpp2011@gmail.com>
2026-03-27 00:55:00 +0100

    cleanup: remove unnecessary include

--- test/unit-tests/facade/Traits.cpp
+++ test/unit-tests/facade/Traits.cpp
@@ -1,11 +1,10 @@
-//          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+//          Copyright Dominic (DNKpp) Koepke 2024-2026.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
 #include "TestReporter.hpp"
 #include "mimic++/Facade.hpp"
-#include "mimic++/ScopedSequence.hpp"
 
 using namespace mimicpp;
 
