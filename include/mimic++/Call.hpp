e00935e35cb2f3ec031fdfaa18d1b04dd3a118bd
DNKpp <dnkpp2011@gmail.com>
2026-04-03 14:12:43 +0200

    feat: add call::ResultStorage helper type

--- include/mimic++/Call.hpp
+++ include/mimic++/Call.hpp
@@ -1,4 +1,4 @@
-//          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+//          Copyright Dominic (DNKpp) Koepke 2024-2026.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
@@ -50,6 +50,58 @@ namespace mimicpp::call
     struct info_for_signature<Return(Args...)>
     {
         using type = Info<Return, Args...>;
+    };
+
+    template <typename Result>
+    class ResultStorage
+    {
+    public:
+        [[nodiscard]]
+        explicit constexpr ResultStorage(Result&& value) noexcept(std::is_nothrow_move_constructible_v<Result>)
+            : m_Value{std::move(value)}
+        {
+        }
+
+        [[nodiscard]]
+        constexpr Result extract() noexcept(std::is_nothrow_move_constructible_v<Result>)
+        {
+            return std::move(m_Value);
+        }
+
+    private:
+        Result m_Value;
+    };
+
+    template <>
+    class ResultStorage<void>
+    {
+    public:
+        static constexpr void extract() noexcept
+        {
+        }
+    };
+
+    template <typename Result>
+        requires std::is_reference_v<Result>
+    class ResultStorage<Result>
+    {
+    public:
+        using ref = std::reference_wrapper<std::remove_reference_t<Result>>;
+
+        [[nodiscard]]
+        explicit constexpr ResultStorage(ref const ref) noexcept
+            : m_Ref{ref}
+        {
+        }
+
+        [[nodiscard]]
+        constexpr Result&& extract() noexcept
+        {
+            return std::forward<Result>(m_Ref.get());
+        }
+
+    private:
+        ref m_Ref;
     };
 }
 
