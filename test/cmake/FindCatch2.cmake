4b4d88d142acdbbdbf30e7315e38a3f2d0e7bae7
DNKpp <dnkpp2011@gmail.com>
2026-04-21 20:40:03 +0200

    deps: upgrade catch2 to v3.14.0

--- test/cmake/FindCatch2.cmake
+++ test/cmake/FindCatch2.cmake
@@ -1,4 +1,4 @@
-#          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+#          Copyright Dominic (DNKpp) Koepke 2024-2026.
 # Distributed under the Boost Software License, Version 1.0.
 #    (See accompanying file LICENSE_1_0.txt or copy at
 #          https://www.boost.org/LICENSE_1_0.txt)
@@ -8,7 +8,7 @@ include(get_cpm)
 # fixes reporting in clion
 set(CATCH_CONFIG_CONSOLE_WIDTH 800 CACHE STRING "")
 
-CPMAddPackage("gh:catchorg/Catch2@3.10.0")
+CPMAddPackage("gh:catchorg/Catch2@3.14.0")
 
 if (Catch2_ADDED)
 	include("${Catch2_SOURCE_DIR}/extras/Catch.cmake")
