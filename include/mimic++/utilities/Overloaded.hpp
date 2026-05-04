ea462b227a34f9629a623c95ae4e04ee706dcf48
DNKpp <dnkpp2011@gmail.com>
2026-05-04 20:01:05 +0200

    chore: add util::Overloaded helper type

---
+++ include/mimic++/utilities/Overloaded.hpp
@@ -0,0 +1,26 @@
+//          Copyright Dominic (DNKpp) Koepke 2026.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MIMICPP_UTILITIES_OVERLOADED_HPP
+#define MIMICPP_UTILITIES_OVERLOADED_HPP
+
+#pragma once
+
+namespace mimicpp::util
+{
+    /**
+     * \brief Famous overloaded helper type used for convenient variant visiting.
+     * \ingroup UTILITIES
+     * \see https://www.cppstories.com/2019/02/2lines3featuresoverload.html/
+     */
+    template <typename... Ts>
+    struct Overloaded
+        : public Ts...
+    {
+        using Ts::operator()...;
+    };
+}
+
+#endif
