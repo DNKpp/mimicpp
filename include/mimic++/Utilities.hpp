d3e70fd00f5a4a6bf9cbacede5fa4be6ca85bde6
DNKpp <dnkpp2011@gmail.com>
2026-05-06 03:03:57 +0200

    feat: add util::CopyableBox

--- include/mimic++/Utilities.hpp
+++ include/mimic++/Utilities.hpp
@@ -1,4 +1,4 @@
-//          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+//          Copyright Dominic (DNKpp) Koepke 2024-2026.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
@@ -11,6 +11,7 @@
 #include "mimic++/utilities/C++23Backports.hpp"
 #include "mimic++/utilities/C++26Backports.hpp"
 #include "mimic++/utilities/Concepts.hpp"
+#include "mimic++/utilities/CopyableBox.hpp"
 #include "mimic++/utilities/PassKey.hpp"
 #include "mimic++/utilities/PriorityTag.hpp"
 #include "mimic++/utilities/SourceLocation.hpp"
---
+++ include/mimic++/utilities/CopyableBox.hpp
@@ -0,0 +1,118 @@
+//          Copyright Dominic (DNKpp) Koepke 2026.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MIMICPP_UTILITIES_COPYABLE_BOX_HPP
+#define MIMICPP_UTILITIES_COPYABLE_BOX_HPP
+
+#pragma once
+
+#include "mimic++/Fwd.hpp"
+#include "mimic++/config/Config.hpp"
+
+#ifndef MIMICPP_DETAIL_IS_MODULE
+    #include <concepts>
+    #include <memory>
+    #include <type_traits>
+#endif
+
+MIMICPP_DETAIL_MODULE_EXPORT namespace mimicpp::util
+{
+    /**
+     * \brief Simple copyable value type, which owns a heap-allocated value.
+     * \ingroup UTILITIES
+     * \details
+     * Unlike the common `std::unique_ptr`, this type is directly copyable.
+     * Besides that, this type is fully `constexpr` (unlike `std::unique_ptr` before C++23).
+     */
+    template <typename T>
+    class CopyableBox
+    {
+    public:
+        constexpr ~CopyableBox() noexcept
+        {
+            delete m_Data;
+        }
+
+        [[nodiscard]]
+        constexpr CopyableBox(CopyableBox const& other)
+            : m_Data{other.m_Data ? new T{*other} : nullptr}
+        {
+        }
+
+        [[nodiscard]]
+        constexpr CopyableBox(CopyableBox&& other) noexcept
+            : m_Data{std::exchange(other.m_Data, nullptr)}
+        {
+        }
+
+        constexpr CopyableBox& operator=(CopyableBox other)
+        {
+            std::ranges::swap(m_Data, other.m_Data);
+            return *this;
+        }
+
+        [[nodiscard]]
+        constexpr CopyableBox()
+            : m_Data{new T{}}
+        {
+        }
+
+        [[nodiscard]]
+        explicit(false) constexpr CopyableBox(T const& value)
+            : m_Data{new T{value}}
+        {
+        }
+
+        [[nodiscard]]
+        explicit(false) constexpr CopyableBox(T&& value)
+            : m_Data{new T{std::move(value)}}
+        {
+        }
+
+        [[nodiscard]]
+        constexpr T& operator*() noexcept
+        {
+            MIMICPP_ASSERT(m_Data, "Data is null.");
+            return *m_Data;
+        }
+
+        [[nodiscard]]
+        constexpr T const& operator*() const noexcept
+        {
+            MIMICPP_ASSERT(m_Data, "Data is null.");
+            return *m_Data;
+        }
+
+        [[nodiscard]]
+        constexpr T* operator->() noexcept
+        {
+            return m_Data;
+        }
+
+        [[nodiscard]]
+        constexpr T const* operator->() const noexcept
+        {
+            return m_Data;
+        }
+
+        [[nodiscard]]
+        explicit constexpr operator bool() const noexcept
+        {
+            return m_Data != nullptr;
+        }
+
+        [[nodiscard]]
+        friend constexpr bool operator==(CopyableBox const& lhs, CopyableBox const& rhs)
+            requires std::equality_comparable<T>
+        {
+            return *lhs == *rhs;
+        }
+
+    private:
+        T* m_Data{};
+    };
+}
+
+#endif
