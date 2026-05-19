ba917cee1bc8f6d1447c1a1da3940a98850d8065
DNKpp <dnkpp2011@gmail.com>
2026-05-19 20:35:41 +0200

    test: add negative tests for unparsable type declarations

---
+++ test/unit-tests/printing/parser/Unparsable.cpp
@@ -0,0 +1,23 @@
+//          Copyright Dominic (DNKpp) Koepke 2026.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "mimic++/printing/type/Parser.hpp"
+
+using namespace mimicpp;
+
+TEST_CASE(
+    "parsing::parse_type returns std::nullopt if input cannot be parsed.",
+    "[print][print::type]")
+{
+    auto const input = GENERATE(
+        "const int const",
+        "volatile int const volatile",
+        "int const int",
+        "int* int",
+        "int<int>",
+        "int[int]");
+
+    CHECK_FALSE(printing::type::parse_type(input));
+}
