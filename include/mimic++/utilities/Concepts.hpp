61b84302b6f09f865d76e6432e306569c359c214
Dominic Koepke <koepke@aunovis.de>
2026-09-03 13:39:22 +0200

    feat: add util::unqualified concept

--- include/mimic++/utilities/Concepts.hpp
+++ include/mimic++/utilities/Concepts.hpp
@@ -1,4 +1,4 @@
-//          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+//          Copyright Dominic (DNKpp) Koepke 2024 - 2026.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
@@ -82,6 +82,12 @@ MIMICPP_DETAIL_MODULE_EXPORT namespace mimicpp::util
             { u == t } -> boolean_testable;
             { u != t } -> boolean_testable;
         };
+
+    /**
+     * \brief Determines, whether `T` is unqualified.
+     */
+    template <typename T>
+    concept unqualified = std::same_as<T, std::remove_cvref_t<T>>;
 
     /**
      * \}
